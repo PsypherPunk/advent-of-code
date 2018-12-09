e4f374209f0736bd8654a29df84890813f56b867
PsypherPunk <psypherpunk@gmail.com>
2018-12-09 21:01:32 +0000

    Added 2018 Day 6, Part One.

---
+++ 2018/06/Two.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coordinates = File
+                .ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"))
+                .Select(c => c.Trim().Split(new[] { ", " }, StringSplitOptions.None))
+                .Select(c => c.Select(i => Convert.ToInt32(i)).ToArray())
+                .Select(c => (x: c[0], y: c[1]))
+                .ToArray();
+
+            var width = coordinates.Max(c => c.x);
+            var height = coordinates.Max(c => c.y);
+
+            var safeCoords = 0;
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    var distances = coordinates
+                        .Select((c, i) => (index: i, dist: Math.Abs(c.x - x) + Math.Abs(c.y - y)))
+                        .OrderBy(c => c.dist)
+                        .ToArray();
+
+                    if (distances.Sum(c => c.dist) < 10000)
+                    {
+                        safeCoords++;
+                    }
+                }
+            }
+            Console.WriteLine(safeCoords);
+        }
+    }
+}

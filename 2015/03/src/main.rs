14962a203725a00e4c99d8f149ec7036307171a7
PsypherPunk <psypherpunk@gmail.com>
2019-11-29 19:03:57 +0000

    Added 2015 Day 3, Part Two.

--- 2015/03/src/main.rs
+++ 2015/03/src/main.rs
@@ -30,30 +30,77 @@ pub fn format_number(number: usize) -> String {
     reverse.join(",")
 }
 
-fn count_houses(input: String) -> usize {
+fn count_houses(input: &String) -> usize {
     let mut stops = vec![(0, 0)];
-    for line in input.lines() {
-        for ch in line.chars() {
-            let (x, y) = stops[0];
-            stops.insert(
-                0,
-                match ch {
-                    '^' => (x, y + 1),
-                    '>' => (x + 1, y),
-                    'v' => (x, y - 1),
-                    '<' => (x - 1, y),
-                    _ => panic!("Invalid character: {}", ch),
-                },
-            );
-        }
+    for ch in input.chars() {
+        let (x, y) = stops[0];
+        stops.insert(
+            0,
+            match ch {
+                '^' => (x, y + 1),
+                '>' => (x + 1, y),
+                'v' => (x, y - 1),
+                '<' => (x - 1, y),
+                _ => panic!("Invalid character: {}", ch),
+            },
+        );
     }
     let houses: HashSet<(i32, i32)> = HashSet::from_iter(stops);
     houses.len()
 }
 
+fn count_houses_with_robo_santa(input: &String) -> usize {
+    let mut santa_stops = vec![(0, 0)];
+    let mut robo_santa_stops = vec![(0, 0)];
+
+    let (even, odd): (Vec<_>, Vec<_>) = input.chars().enumerate().partition(|(i, _)| (i % 2) == 0);
+
+    let santa_instructions = even.iter().map(|(_, i)| i).collect::<Vec<&char>>();
+    let robo_santa_instructions = odd.iter().map(|(_, i)| i).collect::<Vec<&char>>();
+
+    for instruction in santa_instructions {
+        let (x, y) = santa_stops[0];
+        santa_stops.insert(
+            0,
+            match instruction {
+                '^' => (x, y + 1),
+                '>' => (x + 1, y),
+                'v' => (x, y - 1),
+                '<' => (x - 1, y),
+                _ => panic!("Invalid character: {}", instruction),
+            },
+        );
+    }
+
+    for instruction in robo_santa_instructions {
+        let (x, y) = robo_santa_stops[0];
+        robo_santa_stops.insert(
+            0,
+            match instruction {
+                '^' => (x, y + 1),
+                '>' => (x + 1, y),
+                'v' => (x, y - 1),
+                '<' => (x - 1, y),
+                _ => panic!("Invalid character: {}", instruction),
+            },
+        );
+    }
+
+    santa_stops.append(&mut robo_santa_stops);
+    let houses: HashSet<(i32, i32)> = HashSet::from_iter(santa_stops);
+    houses.len()
+}
+
 fn main() {
     let input = read_input();
-    println!("{} houses receive at least one present.", format_number(count_houses(input)));
+    println!(
+        "{} houses receive at least one present.",
+        format_number(count_houses(&input))
+    );
+    println!(
+        "This year, {} houses receive at least one present.",
+        format_number(count_houses_with_robo_santa(&input))
+    );
 }
 
 #[cfg(test)]
@@ -69,5 +116,16 @@ mod tests {
     #[test]
     fn test_four() {
         assert_eq!(count_houses(String::from("^>v<")), 4);
+    }
+
+    #[test]
+    fn test_robo_three() {
+        assert_eq!(count_houses_with_robo_santa(String::from("^v")), 3);
+        assert_eq!(count_houses_with_robo_santa(String::from("^>v<")), 3);
+    }
+
+    #[test]
+    fn test_robo_eleven() {
+        assert_eq!(count_houses_with_robo_santa(String::from("^v^v^v^v^v")), 11);
     }
 }

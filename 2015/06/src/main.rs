daa095277f6c28eb053c73854c39aceedf4e507f
PsypherPunk <psypherpunk@gmail.com>
2019-12-02 21:29:20 +0000

    Added 2015 Day 6, Part Two.

--- 2015/06/src/main.rs
+++ 2015/06/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -18,7 +18,7 @@ fn read_input() -> String {
     }
 }
 
-fn get_lit_count(input: String) -> usize {
+fn get_lit_count(input: &String) -> usize {
     let mut grid: HashSet<(usize, usize)> = HashSet::new();
 
     let instruction =
@@ -63,10 +63,56 @@ fn get_lit_count(input: String) -> usize {
     grid.len()
 }
 
+fn get_brightness(input: String) -> isize {
+    let mut grid: HashMap<(usize, usize), isize> = HashMap::new();
+
+    let instruction =
+        Regex::new(r"^(turn on|toggle|turn off) (\d+),(\d+) through (\d+),(\d+)$").unwrap();
+
+    for line in input.lines() {
+        let captures = instruction.captures(line).unwrap();
+        let y_start = &captures[3].parse::<usize>().unwrap();
+        let y_end = &captures[5].parse::<usize>().unwrap();
+        let x_start = &captures[2].parse::<usize>().unwrap();
+        let x_end = &captures[4].parse::<usize>().unwrap();
+        match &captures[1] {
+            "turn on" => {
+                for y in *y_start..=*y_end {
+                    for x in *x_start..=*x_end {
+                        let brightness = grid.entry((x, y)).or_insert(0);
+                        *brightness += 1;
+                    }
+                }
+            }
+            "turn off" => {
+                for y in *y_start..=*y_end {
+                    for x in *x_start..=*x_end {
+                        let brightness = grid.entry((x, y)).or_insert(1);
+                        *brightness = (*brightness - 1).max(0);
+                    }
+                }
+            }
+            "toggle" => {
+                for y in *y_start..=*y_end {
+                    for x in *x_start..=*x_end {
+                        let brightness = grid.entry((x, y)).or_insert(0);
+                        *brightness += 2;
+                    }
+                }
+            }
+            _ => panic!("Unknown instruction: {}", &captures[1]),
+        };
+    }
+
+    grid.values().sum()
+}
+
 fn main() {
     let input = read_input();
-    let lit_count = get_lit_count(input);
+    let lit_count = get_lit_count(&input);
     println!("How many lights are lit? {}", lit_count);
+    let total_brightness = get_brightness(input);
+    println!("What is the total brightness? {}", total_brightness);
 }
 
 #[cfg(test)]
@@ -76,7 +122,7 @@ mod tests {
     #[test]
     fn test_turn_on_0_0_through_999_999() {
         assert_eq!(
-            get_lit_count(String::from("turn on 0,0 through 999,999")),
+            get_lit_count(&String::from("turn on 0,0 through 999,999")),
             1_000_000
         );
     }
@@ -84,7 +130,7 @@ mod tests {
     #[test]
     fn test_toggle_0_0_through_999_0() {
         assert_eq!(
-            get_lit_count(String::from("toggle 0,0 through 999,0")),
+            get_lit_count(&String::from("toggle 0,0 through 999,0")),
             1_000
         );
     }
@@ -92,8 +138,21 @@ mod tests {
     #[test]
     fn test_turn_off_499_499_through_500_500() {
         assert_eq!(
-            get_lit_count(String::from("turn off 499,499 through 500,500")),
+            get_lit_count(&String::from("turn off 499,499 through 500,500")),
             0
+        );
+    }
+
+    #[test]
+    fn test_turn_on_0_0_through_0_0() {
+        assert_eq!(get_brightness(String::from("turn on 0,0 through 0,0")), 1);
+    }
+
+    #[test]
+    fn test_toggle_0_0_through_999_999() {
+        assert_eq!(
+            get_brightness(String::from("toggle 0,0 through 999,999")),
+            2_000_000
         );
     }
 }

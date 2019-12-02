44f93d1615af26b4f4ccf4320b1fa9f486369554
PsypherPunk <psypherpunk@gmail.com>
2019-12-02 12:39:12 +0000

    Added 2019 Day 2, Part Two.

--- 2019/02/src/main.rs
+++ 2019/02/src/main.rs
@@ -15,7 +15,7 @@ fn read_input() -> String {
     }
 }
 
-fn parse_opcodes(initial_state: String) -> Vec<usize> {
+fn parse_opcodes(initial_state: &String) -> Vec<usize> {
     initial_state
         .trim()
         .split(",")
@@ -23,9 +23,7 @@ fn parse_opcodes(initial_state: String) -> Vec<usize> {
         .collect::<Vec<usize>>()
 }
 
-fn get_final_state(initial_state: String) -> String {
-    let mut opcodes = parse_opcodes(initial_state);
-
+fn get_final_state(mut opcodes: Vec<usize>) -> Vec<usize> {
     let mut position = 0;
 
     while position < opcodes.len() {
@@ -41,28 +39,36 @@ fn get_final_state(initial_state: String) -> String {
     }
 
     opcodes
-        .iter()
-        .map(|o| o.to_string())
-        .collect::<Vec<String>>()
-        .join(",")
+}
+
+fn find_noun_verb(input: String) -> Vec<usize> {
+    let initial_state = parse_opcodes(&input);
+
+    for noun in 0..99 {
+        for verb in 0..99 {
+            let mut opcodes = initial_state.clone();
+            opcodes[1] = noun;
+            opcodes[2] = verb;
+            let final_state = get_final_state(opcodes.clone());
+            if final_state[0] == 19690720 {
+                return opcodes.clone();
+            }
+        }
+    }
+    panic!("Could not find noun/verb.")
 }
 
 fn main() {
     let input = read_input();
-    let mut opcodes = parse_opcodes(input);
+    let mut opcodes = parse_opcodes(&input);
     opcodes[1] = 12;
     opcodes[2] = 2;
-    let initial_state = opcodes
-        .iter()
-        .map(|o| o.to_string())
-        .collect::<Vec<String>>()
-        .join(",");
-    let final_state = get_final_state(initial_state);
-    let final_state = final_state
-        .trim()
-        .split(",")
-        .collect::<Vec<&str>>();
+    let final_state = get_final_state(opcodes);
     println!("The value in position 0 is: {}", final_state[0]);
+
+    let final_state = find_noun_verb(input);
+    println!("{:?}", final_state);
+    println!("100 * noun + verb is {}", 100 * final_state[1] + final_state[2]);
 }
 
 #[cfg(test)]
@@ -71,21 +77,33 @@ mod tests {
 
     #[test]
     fn test_100099() {
-        assert_eq!(get_final_state(String::from("1,0,0,0,99")), "2,0,0,0,99");
+        assert_eq!(
+            get_final_state(parse_opcodes(&String::from("1,0,0,0,99"))),
+            vec![2, 0, 0, 0, 99]
+        );
     }
 
     #[test]
     fn test_230399() {
-        assert_eq!(get_final_state(String::from("2,3,0,3,99")), "2,3,0,6,99");
+        assert_eq!(
+            get_final_state(parse_opcodes(&String::from("2,3,0,3,99"))),
+            vec![2, 3, 0, 6, 99]
+        );
     }
 
     #[test]
     fn test_2445990() {
-        assert_eq!(get_final_state(String::from("2,4,4,5,99,0")), "2,4,4,5,99,9801");
+        assert_eq!(
+            get_final_state(parse_opcodes(&String::from("2,4,4,5,99,0"))),
+            vec![2, 4, 4, 5, 99, 9801]
+        );
     }
 
     #[test]
     fn test_11149956099() {
-        assert_eq!(get_final_state(String::from("1,1,1,4,99,5,6,0,99")), "30,1,1,4,2,5,6,0,99");
+        assert_eq!(
+            get_final_state(parse_opcodes(&String::from("1,1,1,4,99,5,6,0,99"))),
+            vec![30, 1, 1, 4, 2, 5, 6, 0, 99]
+        );
     }
 }

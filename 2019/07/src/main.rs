0f62735ff04b4ab6fe6989dd501e1d99f2e7831d
PsypherPunk <psypherpunk@gmail.com>
2019-12-09 13:23:01 +0000

    Added 2019 Day 7, Part Two.

--- 2019/07/src/main.rs
+++ 2019/07/src/main.rs
@@ -3,6 +3,158 @@ use std::io::prelude::*;
 
 use itertools::Itertools;
 
+#[derive(Clone)]
+struct Amplifier {
+    opcode: Vec<isize>,
+    position: usize,
+}
+
+impl Amplifier {
+    fn new(input: &String) -> Amplifier {
+        let opcode = parse_opcodes(input);
+        Amplifier {
+            opcode,
+            position: 0,
+        }
+    }
+
+    fn run(&mut self, mut phase_setting: Option<&i32>, signal: usize) -> Option<usize> {
+        while self.position < self.opcode.len() {
+            let opcode = self.opcode[self.position] % 100;
+            match opcode {
+                1 => {
+                    let p1 = match (self.opcode[self.position] / 100) % 10 {
+                        0 => self.opcode[self.opcode[self.position + 1] as usize],
+                        1 => self.opcode[self.position + 1],
+                        _ => panic!("Invalid mode at position {}", self.opcode[self.position]),
+                    };
+                    let p2 = match (self.opcode[self.position] / 1000) % 10 {
+                        0 => self.opcode[self.opcode[self.position + 2] as usize],
+                        1 => self.opcode[self.position + 2],
+                        _ => panic!("Invalid mode at position {}", self.opcode[self.position]),
+                    };
+                    let output = p1 + p2;
+                    let output_position = self.opcode[self.position + 3] as usize;
+                    self.opcode[output_position] = output;
+                    self.position += 4;
+                }
+                2 => {
+                    let p1 = match (self.opcode[self.position] / 100) % 10 {
+                        0 => self.opcode[self.opcode[self.position + 1] as usize],
+                        1 => self.opcode[self.position + 1],
+                        _ => panic!("Invalid mode at position {}", self.opcode[self.position]),
+                    };
+                    let p2 = match (self.opcode[self.position] / 1000) % 10 {
+                        0 => self.opcode[self.opcode[self.position + 2] as usize],
+                        1 => self.opcode[self.position + 2],
+                        _ => panic!("Invalid mode at position {}", self.opcode[self.position]),
+                    };
+                    let output = p1 * p2;
+                    let output_position = self.opcode[self.position + 3] as usize;
+                    self.opcode[output_position] = output;
+                    self.position += 4;
+                }
+                3 => {
+                    let output_position = self.opcode[self.position + 1] as usize;
+                    let i = match phase_setting.take() {
+                        Some(i) => *i as usize,
+                        None => signal,
+                    };
+                    self.opcode[output_position] = i as isize;
+                    self.position += 2;
+                }
+                4 => {
+                    let p1 = match (self.opcode[self.position] / 100) % 10 {
+                        0 => self.opcode[self.opcode[self.position + 1] as usize],
+                        1 => self.opcode[self.position + 1],
+                        _ => panic!("Invalid mode at position {}", self.opcode[self.position]),
+                    } as usize;
+                    self.position += 2;
+                    return Some(p1);
+                }
+                5 => {
+                    let p1 = match (self.opcode[self.position] / 100) % 10 {
+                        0 => self.opcode[self.opcode[self.position + 1] as usize],
+                        1 => self.opcode[self.position + 1],
+                        _ => panic!("Invalid mode at position {}", self.opcode[self.position]),
+                    };
+                    let p2 = match (self.opcode[self.position] / 1000) % 10 {
+                        0 => self.opcode[self.opcode[self.position + 2] as usize],
+                        1 => self.opcode[self.position + 2],
+                        _ => panic!("Invalid mode at position {}", self.opcode[self.position]),
+                    };
+                    if p1 != 0 {
+                        self.position = p2 as usize;
+                    } else {
+                        self.position += 3;
+                    }
+                }
+                6 => {
+                    let p1 = match (self.opcode[self.position] / 100) % 10 {
+                        0 => self.opcode[self.opcode[self.position + 1] as usize],
+                        1 => self.opcode[self.position + 1],
+                        _ => panic!("Invalid mode at position {}", self.opcode[self.position]),
+                    };
+                    let p2 = match (self.opcode[self.position] / 1000) % 10 {
+                        0 => self.opcode[self.opcode[self.position + 2] as usize],
+                        1 => self.opcode[self.position + 2],
+                        _ => panic!("Invalid mode at position {}", self.opcode[self.position]),
+                    };
+                    if p1 == 0 {
+                        self.position = p2 as usize;
+                    } else {
+                        self.position += 3;
+                    }
+                }
+                7 => {
+                    let p1 = match (self.opcode[self.position] / 100) % 10 {
+                        0 => self.opcode[self.opcode[self.position + 1] as usize],
+                        1 => self.opcode[self.position + 1],
+                        _ => panic!("Invalid mode at position {}", self.opcode[self.position]),
+                    };
+                    let p2 = match (self.opcode[self.position] / 1000) % 10 {
+                        0 => self.opcode[self.opcode[self.position + 2] as usize],
+                        1 => self.opcode[self.position + 2],
+                        _ => panic!("Invalid mode at position {}", self.opcode[self.position]),
+                    };
+                    let output_position = self.opcode[self.position + 3] as usize;
+                    if p1 < p2 {
+                        self.opcode[output_position] = 1;
+                    } else {
+                        self.opcode[output_position] = 0;
+                    }
+                    self.position += 4;
+                }
+                8 => {
+                    let p1 = match (self.opcode[self.position] / 100) % 10 {
+                        0 => self.opcode[self.opcode[self.position + 1] as usize],
+                        1 => self.opcode[self.position + 1],
+                        _ => panic!("Invalid mode at position {}", self.opcode[self.position]),
+                    };
+                    let p2 = match (self.opcode[self.position] / 1000) % 10 {
+                        0 => self.opcode[self.opcode[self.position + 2] as usize],
+                        1 => self.opcode[self.position + 2],
+                        _ => panic!("Invalid mode at position {}", self.opcode[self.position]),
+                    };
+                    let output_position = self.opcode[self.position + 3] as usize;
+                    if p1 == p2 {
+                        self.opcode[output_position] = 1;
+                    } else {
+                        self.opcode[output_position] = 0;
+                    }
+                    self.position += 4;
+                }
+                99 => break,
+                _ => panic!(
+                    "Invalid self.opcode at position {} for {:?}",
+                    self.position, self.opcode
+                ),
+            };
+        }
+        None
+    }
+}
+
 fn read_input() -> String {
     let filename = "input.txt";
     match File::open(filename) {
@@ -25,9 +177,8 @@ fn parse_opcodes(initial_state: &String) -> Vec<isize> {
         .collect::<Vec<isize>>()
 }
 
-fn get_output(mut opcodes: Vec<isize>, mut inputs: Vec<usize>) -> usize {
-    let mut position = 0;
-    let mut output: usize = 0;
+fn get_output(opcodes: &mut Vec<isize>, mut inputs: Vec<usize>, mut position: usize) -> usize {
+    let mut input;
 
     while position < opcodes.len() {
         let opcode = opcodes[position] % 100;
@@ -66,7 +217,10 @@ fn get_output(mut opcodes: Vec<isize>, mut inputs: Vec<usize>) -> usize {
             }
             3 => {
                 let output_position = opcodes[position + 1] as usize;
-                let input = inputs.pop().unwrap();
+                input = match inputs.pop() {
+                    Some(i) => i,
+                    None => panic!("No inputs!"),
+                };
                 opcodes[output_position] = input as isize;
                 position += 2;
             }
@@ -76,8 +230,8 @@ fn get_output(mut opcodes: Vec<isize>, mut inputs: Vec<usize>) -> usize {
                     1 => opcodes[position + 1],
                     _ => panic!("Invalid mode at position {}", opcodes[position]),
                 } as usize;
-                output = p1;
                 position += 2;
+                return p1;
             }
             5 => {
                 let p1 = match (opcodes[position] / 100) % 10 {
@@ -155,16 +309,15 @@ fn get_output(mut opcodes: Vec<isize>, mut inputs: Vec<usize>) -> usize {
             _ => panic!("Invalid opcode at position {} for {:?}", position, opcodes),
         };
     }
-
-    output
+    0
 }
 
-fn run_amplifier_sequence(opcodes: Vec<isize>) -> usize {
+fn run_amplifier_sequence(opcodes: &Vec<isize>) -> usize {
     let mut output = 0;
     let mut outputs = vec![];
     for combination in (0..=4).permutations(5) {
         for phase_setting in combination.iter() {
-            output = get_output(opcodes.clone(), vec![output, *phase_setting]);
+            output = get_output(opcodes.clone().as_mut(), vec![output, *phase_setting], 0);
         }
         outputs.push(output);
         output = 0;
@@ -172,13 +325,38 @@ fn run_amplifier_sequence(opcodes: Vec<isize>) -> usize {
     *outputs.iter().max().unwrap() as usize
 }
 
+fn run_feedback_loop(input: &String) -> usize {
+    let mut outputs: Vec<usize> = Vec::new();
+
+    for combination in (5..=9).permutations(5) {
+        let mut signal: usize = 0;
+        let mut phase_settings = combination.iter().clone();
+
+        let mut amplifiers = vec![Amplifier::new(input); 5];
+
+        for i in (0..amplifiers.len()).cycle() {
+            signal = match amplifiers[i].run(phase_settings.next(), signal) {
+                Some(s) => s,
+                None => break,
+            }
+        }
+        outputs.push(signal);
+    }
+    *outputs.iter().max().unwrap()
+}
+
 fn main() {
     let input = read_input();
     let opcodes = parse_opcodes(&input);
-    let highest_signal = run_amplifier_sequence(opcodes);
+    let highest_signal = run_amplifier_sequence(&opcodes);
     println!(
         "What is the highest signal that can be sent to the thrusters? {}",
         highest_signal
+    );
+    let feedback_signal = run_feedback_loop(&input);
+    println!(
+        "What is the highest signal that can be sent to the thrusters? {}",
+        feedback_signal,
     );
 }
 
@@ -190,7 +368,7 @@ mod tests {
     fn test_43210() {
         let intcode = String::from("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0");
         let opcodes = parse_opcodes(&intcode);
-        assert_eq!(run_amplifier_sequence(opcodes), 43210,);
+        assert_eq!(run_amplifier_sequence(&opcodes), 43210);
     }
 
     #[test]
@@ -199,13 +377,29 @@ mod tests {
             "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0",
         );
         let opcodes = parse_opcodes(&intcode);
-        assert_eq!(run_amplifier_sequence(opcodes), 54321,);
+        assert_eq!(run_amplifier_sequence(&opcodes), 54321);
     }
 
     #[test]
     fn test_65210() {
         let intcode = String::from("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0");
         let opcodes = parse_opcodes(&intcode);
-        assert_eq!(run_amplifier_sequence(opcodes), 65210,);
+        assert_eq!(run_amplifier_sequence(&opcodes), 65210);
+    }
+
+    #[test]
+    fn test_139629729() {
+        let intcode = String::from(
+            "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5",
+        );
+        assert_eq!(run_feedback_loop(&intcode), 139629729);
+    }
+
+    #[test]
+    fn test_18216() {
+        let intcode = String::from(
+            "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10",
+        );
+        assert_eq!(run_feedback_loop(&intcode), 18216);
     }
 }

9a65fbf8e77761641c9661be7b06a2dcf293e66e
PsypherPunk <psypherpunk@gmail.com>
2020-01-14 12:52:21 +0000

    Added 2019 Day 22, Part Two.
    Refactored Part One to work on positions (à la Part Two).

--- 2019/22/src/main.rs
+++ 2019/22/src/main.rs
@@ -1,81 +1,98 @@
 use std::fs;
 
+use mod_exp::mod_exp;
 use regex::Regex;
 
 struct Deck {
-    cards: Vec<usize>,
+    count: i128,
+}
+
+#[derive(Copy, Clone)]
+enum Technique {
+    DealIntoNewStack,
+    CutNCards(i128),
+    DealWithIncrement(i128),
 }
 
 impl Deck {
-    fn new(count: usize) -> Self {
-        let cards = (0..count).collect();
-        Deck { cards }
+    fn new(count: i128) -> Self {
+        Deck { count }
     }
 
-    fn get_position_for_card(&self, card: usize) -> usize {
-        self.cards.iter().position(|&c| c == card).unwrap()
+    fn shuffle(&self, input: &str, card: i128) -> i128 {
+        let techniques = self.get_techniques(input);
+
+        techniques.iter().fold(card, |position, &cmd| match cmd {
+            Technique::DealIntoNewStack => self.count - 1 - position,
+            Technique::CutNCards(n) => (((position - n) % self.count) + self.count) % self.count,
+            Technique::DealWithIncrement(n) => (position * n) % self.count,
+        })
     }
 
-    fn shuffle(&mut self, input: &str) {
+    /// Purloined wholeheartedly from here:
+    /// https://www.reddit.com/r/adventofcode/comments/ee0rqi/2019_day_22_solutions/fbnkaju/
+    fn shuffle_repeatedly(&self, input: &str, repetitions: i128, position: i128) -> i128 {
+        let techniques = self.get_techniques(input);
+
+        let (increment, offset) = techniques.iter().rev().fold((1, 0), |(i, o), &technique| {
+            let (i, o) = match technique {
+                Technique::DealIntoNewStack => (-i, -o - 1),
+                Technique::CutNCards(n) => (i, o + n),
+                Technique::DealWithIncrement(n) => {
+                    let n = mod_exp(n, self.count - 2, self.count);
+                    (i * n, o * n)
+                }
+            };
+            (i % self.count, o % self.count)
+        });
+
+        let term1 = position * mod_exp(increment, repetitions, self.count) % self.count;
+        let tmp = (mod_exp(increment, repetitions, self.count) - 1)
+            * mod_exp(increment - 1, self.count - 2, self.count)
+            % self.count;
+        let term2 = offset * tmp % self.count;
+        (term1 + term2) % self.count
+    }
+
+    fn get_techniques(&self, input: &str) -> Vec<Technique> {
         let deal_with_increment = Regex::new(r"deal with increment (\d+)").unwrap();
         let cut = Regex::new(r"cut (-?\d+)").unwrap();
 
-        for line in input.lines() {
-            match line {
-                line if line.eq("deal into new stack") => {
-                    self.deal_into_new_stack();
-                }
+        input
+            .lines()
+            .map(|line| match line {
+                line if line.eq("deal into new stack") => Technique::DealIntoNewStack,
                 line if deal_with_increment.is_match(line) => {
                     let increment = deal_with_increment.captures(line).unwrap()[1]
-                        .parse::<usize>()
+                        .parse::<i128>()
                         .unwrap();
-                    self.deal_with_increment_n(increment);
+                    Technique::DealWithIncrement(increment)
                 }
                 line if cut.is_match(line) => {
-                    let cut = cut.captures(line).unwrap()[1].parse::<isize>().unwrap();
-                    self.cut_n_cards(cut);
+                    let cut = cut.captures(line).unwrap()[1].parse::<i128>().unwrap();
+                    Technique::CutNCards(cut)
                 }
                 line => {
                     panic!("Unknown shuffle instruction: {}", line);
                 }
-            }
-        }
-    }
-
-    fn deal_into_new_stack(&mut self) {
-        self.cards.reverse();
-    }
-
-    fn cut_n_cards(&mut self, n: isize) {
-        let n = (self.cards.len() as isize + n) % self.cards.len() as isize;
-        let n = n as usize;
-        let mut cards = self.cards[n..].to_vec();
-        cards.extend_from_slice(&self.cards[0..n]);
-        self.cards = cards;
-    }
-
-    fn deal_with_increment_n(&mut self, n: usize) {
-        self.cards.reverse();
-        let mut cards = vec![0; self.cards.len()];
-        let mut position = 0;
-
-        while !self.cards.is_empty() {
-            cards[position] = self.cards.pop().unwrap();
-            position = (position + n) % cards.len();
-        }
-
-        self.cards = cards;
+            })
+            .collect()
     }
 }
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
-    let mut deck = Deck::new(10007);
-    deck.shuffle(&input.trim());
+    let deck = Deck::new(10007);
     println!(
         "…what is the position of card 2019? {}",
-        deck.get_position_for_card(2019)
+        deck.shuffle(&input, 2019),
+    );
+
+    let deck = Deck::new(119_315_717_514_047_i128);
+    println!(
+        "…what number is on the card that ends up in position 2020? {}",
+        deck.shuffle_repeatedly(&input, 101_741_582_076_661_i128, 2020),
     );
 }
 
@@ -84,86 +101,83 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_new() {
+    fn test_deal_into_new_stack() {
         let deck = Deck::new(10);
 
-        assert_eq!(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9], deck.cards);
-    }
-    #[test]
-    fn test_deal_into_new_stack() {
-        let mut deck = Deck::new(10);
-
-        deck.deal_into_new_stack();
-
-        assert_eq!(vec![9, 8, 7, 6, 5, 4, 3, 2, 1, 0], deck.cards);
+        // 9 8 7 6 5 4 3 2 1 0
+        assert_eq!(9, deck.shuffle("deal into new stack", 0));
+        assert_eq!(0, deck.shuffle("deal into new stack", 9));
     }
 
     #[test]
     fn test_cut_n_cards() {
-        let mut deck = Deck::new(10);
+        let deck = Deck::new(10);
 
-        deck.cut_n_cards(3);
-
-        assert_eq!(vec![3, 4, 5, 6, 7, 8, 9, 0, 1, 2], deck.cards);
+        // 3 4 5 6 7 8 9 0 1 2
+        assert_eq!(7, deck.shuffle("cut 3", 0));
+        assert_eq!(0, deck.shuffle("cut 3", 3));
     }
 
     #[test]
     fn test_cut_negative_n_cards() {
-        let mut deck = Deck::new(10);
+        let deck = Deck::new(10);
 
-        deck.cut_n_cards(-4);
-
-        assert_eq!(vec![6, 7, 8, 9, 0, 1, 2, 3, 4, 5], deck.cards);
+        // 6 7 8 9 0 1 2 3 4 5
+        assert_eq!(7, deck.shuffle("cut -4", 3));
+        assert_eq!(0, deck.shuffle("cut -4", 6));
     }
 
     #[test]
     fn test_deal_with_increment_n() {
-        let mut deck = Deck::new(10);
+        let deck = Deck::new(10);
 
-        deck.deal_with_increment_n(3);
-
-        assert_eq!(vec![0, 7, 4, 1, 8, 5, 2, 9, 6, 3], deck.cards);
+        // 0 7 4 1 8 5 2 9 6 3
+        assert_eq!(0, deck.shuffle("deal with increment 3", 0));
+        assert_eq!(9, deck.shuffle("deal with increment 3", 3));
     }
 
     #[test]
     fn test_0369258147() {
-        let mut deck = Deck::new(10);
+        let deck = Deck::new(10);
         let instructions = r#"deal with increment 7
 deal into new stack
 deal into new stack"#;
 
-        deck.shuffle(instructions);
-
-        assert_eq!(vec![0, 3, 6, 9, 2, 5, 8, 1, 4, 7], deck.cards);
+        // 0 3 6 9 2 5 8 1 4 7
+        assert_eq!(0, deck.shuffle(&instructions, 0));
+        assert_eq!(1, deck.shuffle(&instructions, 3));
+        assert_eq!(9, deck.shuffle(&instructions, 7));
     }
 
     #[test]
     fn test_3074185296() {
-        let mut deck = Deck::new(10);
+        let deck = Deck::new(10);
         let instructions = r#"cut 6
 deal with increment 7
 deal into new stack"#;
 
-        deck.shuffle(instructions);
-
-        assert_eq!(vec![3, 0, 7, 4, 1, 8, 5, 2, 9, 6], deck.cards);
+        // 3 0 7 4 1 8 5 2 9 6
+        assert_eq!(0, deck.shuffle(&instructions, 3));
+        assert_eq!(1, deck.shuffle(&instructions, 0));
+        assert_eq!(9, deck.shuffle(&instructions, 6));
     }
 
     #[test]
     fn test_6307418529() {
-        let mut deck = Deck::new(10);
+        let deck = Deck::new(10);
         let instructions = r#"deal with increment 7
 deal with increment 9
 cut -2"#;
 
-        deck.shuffle(instructions);
-
-        assert_eq!(vec![6, 3, 0, 7, 4, 1, 8, 5, 2, 9], deck.cards);
+        // 6 3 0 7 4 1 8 5 2 9
+        assert_eq!(0, deck.shuffle(&instructions, 6));
+        assert_eq!(1, deck.shuffle(&instructions, 3));
+        assert_eq!(9, deck.shuffle(&instructions, 9));
     }
 
     #[test]
     fn test_9258147036() {
-        let mut deck = Deck::new(10);
+        let deck = Deck::new(10);
         let instructions = r#"deal into new stack
 cut -2
 deal with increment 7
@@ -175,8 +189,9 @@ deal with increment 9
 deal with increment 3
 cut -1"#;
 
-        deck.shuffle(instructions);
-
-        assert_eq!(vec![9, 2, 5, 8, 1, 4, 7, 0, 3, 6,], deck.cards);
+        // 9 2 5 8 1 4 7 0 3 6
+        assert_eq!(0, deck.shuffle(&instructions, 9));
+        assert_eq!(1, deck.shuffle(&instructions, 2));
+        assert_eq!(9, deck.shuffle(&instructions, 6));
     }
 }

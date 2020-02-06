367a812468052c5b4ebb7eb36f87b38481cd6c73
PsypherPunk <psypherpunk@gmail.com>
2020-02-06 14:40:50 +0000

    Added 2015 Day 16, Part Two.

--- 2015/16/src/main.rs
+++ 2015/16/src/main.rs
@@ -1,5 +1,8 @@
 use std::fs;
 
+use regex::Regex;
+use std::collections::HashMap;
+
 static MFCSAM: &str = r#"children: 3
 cats: 7
 samoyeds: 2
@@ -11,41 +14,79 @@ trees: 3
 cars: 2
 perfumes: 1"#;
 
-fn get_sues_number(sue: &str) -> u16 {
-    sue
-        .split(':')
-        .collect::<Vec<&str>>()
-        .first()
-        .unwrap()
-        .split_whitespace()
-        .collect::<Vec<&str>>()
-        .last()
-        .unwrap()
-        .parse::<u16>()
-        .unwrap()
+fn get_mfcsam() -> HashMap<String, u8> {
+    MFCSAM
+        .lines()
+        .map(|line| {
+            let detection = line.split_whitespace().collect::<Vec<&str>>();
+            (
+                detection[0][..(detection[0].len() - 1)].to_string(),
+                detection[1].parse::<u8>().unwrap(),
+            )
+        })
+        .collect()
 }
 
-fn get_sue(input: &str) -> u16 {
-    let mfcsam = MFCSAM.trim().lines().collect::<Vec<&str>>();
+fn get_sues(input: &str) -> HashMap<String, HashMap<String, u8>> {
+    let re = Regex::new(r#"^Sue (\d+): (\w+): (\d+), (\w+): (\d+), (\w+): (\d+)$"#).unwrap();
 
-    let detected = input
+    input
         .trim()
         .lines()
-        .filter(|line| {
-            mfcsam
-                .iter()
-                .map(|detection| line.contains(detection))
-                .collect::<Vec<bool>>()
-                .iter()
-                .filter(|&detected| *detected)
-                .count()
-                == 3
+        .map(|line| {
+            let captures = re.captures(line).unwrap();
+            let mut facts = HashMap::new();
+            facts.insert(captures[2].to_string(), captures[3].parse::<u8>().unwrap());
+            facts.insert(captures[4].to_string(), captures[5].parse::<u8>().unwrap());
+            facts.insert(captures[6].to_string(), captures[7].parse::<u8>().unwrap());
+            (captures[1].to_string(), facts)
         })
-        .collect::<Vec<&str>>();
+        .collect()
+}
 
-    assert_eq!(1, detected.len());
+fn get_sue(input: &str) -> String {
+    let mfcsam = get_mfcsam();
+    let sues = get_sues(&input);
 
-    get_sues_number(detected.first().unwrap())
+    let detected = sues
+        .iter()
+        .find(|&(_, facts)| {
+            facts
+                .iter()
+                .map(|(k, v)| mfcsam.get(k).unwrap() == v)
+                .all(|x| x)
+        })
+        .unwrap();
+
+    detected.0.clone()
+}
+
+fn get_retroencabulated_sue(input: &str) -> String {
+    let mfcsam = get_mfcsam();
+    let sues = get_sues(&input);
+
+    let cats = String::from("cats");
+    let trees = String::from("trees");
+    let pomeranians = String::from("pomeranians");
+    let goldfish = String::from("goldfish");
+
+    let detected = sues
+        .iter()
+        .find(|&(_, facts)| {
+            facts
+                .iter()
+                .map(|(k, v)| match k {
+                    k if *k == cats => v > mfcsam.get(k).unwrap(),
+                    k if *k == trees => v > mfcsam.get(k).unwrap(),
+                    k if *k == pomeranians => v < mfcsam.get(k).unwrap(),
+                    k if *k == goldfish => v < mfcsam.get(k).unwrap(),
+                    _ => mfcsam.get(k).unwrap() == v,
+                })
+                .all(|x| x)
+        })
+        .unwrap();
+
+    detected.0.clone()
 }
 
 fn main() {
@@ -54,7 +95,12 @@ fn main() {
     println!(
         "What is the number of the Sue that got you the gift? {}",
         get_sue(&input),
-    )
+    );
+
+    println!(
+        "What is the number of the real Aunt Sue? {}",
+        get_retroencabulated_sue(&input),
+    );
 }
 
 #[cfg(test)]

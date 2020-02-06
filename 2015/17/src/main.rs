f484d1b6b7c52a940b8f5a549c3f1d1f626523f2
PsypherPunk <psypherpunk@gmail.com>
2020-02-06 21:05:30 +0000

    Added 2015 Day 17, Part Two.

--- 2015/17/src/main.rs
+++ 2015/17/src/main.rs
@@ -25,14 +25,34 @@ fn get_combinations_for_capacity(input: &str, capacity: u16) -> Vec<Vec<u16>> {
         .collect::<Vec<Vec<u16>>>()
 }
 
+fn get_smallest_combinations(combinations: &[Vec<u16>]) -> Vec<Vec<u16>> {
+    let lengths = combinations
+        .iter()
+        .map(|combination| combination.len())
+        .collect::<Vec<usize>>();
+
+    let smallest = *lengths.iter().min().unwrap();
+
+    combinations
+        .iter()
+        .filter(|combination| combination.len() == smallest)
+        .map(|combination| combination.iter().copied().collect::<Vec<u16>>())
+        .collect::<Vec<Vec<u16>>>()
+}
+
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
-
     let combinations = get_combinations_for_capacity(&input, 150);
+
     println!(
         "how many different combinations of containers can exactly fit all 150 liters of eggnog? {}",
         combinations.len(),
     );
+
+    println!(
+        "How many different ways can you fill that number of containers…? {}",
+        get_smallest_combinations(&combinations).len(),
+    )
 }
 
 #[cfg(test)]
@@ -40,13 +60,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_part1() {
         let input = r#"20
 15
 10
 5
 5"#;
+        let combinations = get_combinations_for_capacity(&input, 25);
 
-        assert_eq!(4, get_combinations(&input, 25))
+        assert_eq!(4, combinations.len());
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = r#"20
+15
+10
+5
+5"#;
+        let combinations = get_combinations_for_capacity(&input, 25);
+
+        assert_eq!(3, get_smallest_combinations(&combinations).len());
     }
 }

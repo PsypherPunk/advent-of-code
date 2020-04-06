6644cd1c891d589e0f7ab1dc516aa7f3a0dcd9e1
PsypherPunk <psypherpunk@gmail.com>
2020-04-06 16:31:17 +0100

    Added 2015 Day 24, Part One.

---
+++ 2015/24/src/main.rs
@@ -0,0 +1,80 @@
+use std::fs;
+
+use itertools::Itertools;
+
+fn get_packages(input: &str) -> Vec<usize> {
+    input
+        .trim()
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect()
+}
+
+fn get_smallest_groups(packages: &Vec<usize>, groups: usize) -> Option<Vec<Vec<usize>>> {
+    let compartment_weight = packages.iter().sum::<usize>() / groups;
+
+    for group_size in 1..packages.len() {
+        let optimal_groups = packages
+            .iter()
+            .cloned()
+            .combinations(group_size)
+            .filter(|package_group| package_group.iter().sum::<usize>() == compartment_weight)
+            .collect::<Vec<Vec<usize>>>();
+        if !optimal_groups.is_empty() {
+            return Some(optimal_groups);
+        }
+    }
+    None
+}
+
+fn get_quantum_entanglement(packages: &Vec<usize>) -> usize {
+    packages.iter().product()
+}
+
+fn get_passenger_compartment(input: &str) -> Vec<usize> {
+    let packages = get_packages(&input);
+    let mut smallest_groups = get_smallest_groups(&packages, 3).unwrap();
+
+    if smallest_groups.len() > 1 {
+        smallest_groups
+            .sort_by(|a, b| get_quantum_entanglement(a).cmp(&get_quantum_entanglement(b)));
+        smallest_groups.first().unwrap().to_vec()
+    } else {
+        smallest_groups.get(0).unwrap().to_vec()
+    }
+}
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+
+    let passenger_packages = get_passenger_compartment(&input);
+    println!(
+        "What is the quantum entanglement of the first group of packages in the ideal configuration? {}",
+        get_quantum_entanglement(&passenger_packages),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = r"1
+2
+3
+4
+5
+7
+8
+9
+10
+11";
+        let packages = get_packages(&input);
+
+        let smallest_groups = get_smallest_groups(&packages, 3).unwrap();
+
+        assert_eq!(1, smallest_groups.len());
+        assert_eq!(99, get_quantum_entanglement(&smallest_groups[0]));
+    }
+}

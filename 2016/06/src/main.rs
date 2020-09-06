993ff3e951154bcd066c7acbcf12fef5bdba6581
PsypherPunk <psypherpunk@gmail.com>
2020-09-06 13:32:01 +0100

    🌟 Added 2016 Day 6, Part Two.

--- 2016/06/src/main.rs
+++ 2016/06/src/main.rs
@@ -12,12 +12,12 @@ impl Signal {
         }
     }
 
-    fn get_message(&self) -> String {
+    fn get_error_corrected_message(&self) -> String {
         let mut counter = BTreeMap::new();
 
         self.signal.lines().for_each(|line| {
             line.chars().enumerate().for_each(|(pos, ch)| {
-                let pos_count = counter.entry(pos).or_insert(HashMap::new());
+                let pos_count = counter.entry(pos).or_insert_with(HashMap::new);
                 let ch_count = pos_count.entry(ch).or_insert(0);
                 *ch_count += 1;
             })
@@ -31,6 +31,26 @@ impl Signal {
             })
             .collect()
     }
+
+    fn get_modified_repetition_decoded_message(&self) -> String {
+        let mut counter = BTreeMap::new();
+
+        self.signal.lines().for_each(|line| {
+            line.chars().enumerate().for_each(|(pos, ch)| {
+                let pos_count = counter.entry(pos).or_insert_with(HashMap::new);
+                let ch_count = pos_count.entry(ch).or_insert(0);
+                *ch_count += 1;
+            })
+        });
+        counter
+            .into_iter()
+            .map(|(_, ch_count)| {
+                let mut chars = ch_count.into_iter().collect::<Vec<(char, usize)>>();
+                chars.sort_by(|a, b| a.1.cmp(&b.1));
+                chars.first().unwrap().0
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -40,17 +60,20 @@ fn main() {
 
     println!(
         "…what is the error-corrected version of the message being sent? {}",
-        signal.get_message(),
-    )
+        signal.get_error_corrected_message(),
+    );
+
+    println!(
+        "…what is the original message that Santa is trying to send? {}",
+        signal.get_modified_repetition_decoded_message(),
+    );
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_part_one() {
-        let input = r#"eedadn
+    const INPUT: &str = r#"eedadn
 drvtee
 eandsr
 raavrd
@@ -67,8 +90,18 @@ vrdear
 dvrsen
 enarar
 "#;
-        let signal = Signal::from_string(&input);
 
-        assert_eq!("easter", signal.get_message());
+    #[test]
+    fn test_part_one() {
+        let signal = Signal::from_string(&INPUT);
+
+        assert_eq!("easter", signal.get_error_corrected_message());
+    }
+
+    #[test]
+    fn test_part_two() {
+        let signal = Signal::from_string(&INPUT);
+
+        assert_eq!("advent", signal.get_modified_repetition_decoded_message());
     }
 }

f0b167a96ea3b1ac8c3ef5addf82ea6ac7d9bffa
PsypherPunk <psypherpunk@gmail.com>
2020-10-09 15:09:56 +0100

    🌟 Added 2016 Day 19, Part Two.

--- 2016/19/src/main.rs
+++ 2016/19/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::fs;
 
 fn get_winner(elf_count: &str) -> usize {
@@ -9,10 +10,32 @@ fn get_winner(elf_count: &str) -> usize {
     (2 * l) + 1
 }
 
+fn get_winner_opposite(elf_count: &str) -> usize {
+    let elf_count = elf_count.trim().parse::<usize>().unwrap();
+
+    let nearest_power_of_3 = 3_usize.pow((elf_count as f64).log(3.0).floor() as u32);
+    let l = elf_count - nearest_power_of_3;
+
+    if l == 0 {
+        nearest_power_of_3
+    } else {
+        match l.cmp(&nearest_power_of_3) {
+            Ordering::Less => l,
+            Ordering::Greater => nearest_power_of_3 + (l - nearest_power_of_3) * 2,
+            _ => panic!("Errrr…"),
+        }
+    }
+}
+
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!("…which Elf gets all the presents? {}", get_winner(&input));
+
+    println!(
+        "…which Elf now gets all the presents? {}",
+        get_winner_opposite(&input)
+    );
 }
 
 #[cfg(test)]
@@ -24,5 +47,12 @@ mod tests {
         let input = "5";
 
         assert_eq!(get_winner(&input), 3);
+    }
+
+    #[test]
+    fn test_5_opposite() {
+        let input = "5";
+
+        assert_eq!(get_winner_opposite(&input), 2);
     }
 }

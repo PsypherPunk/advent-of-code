9f3ef855af3a7660b50a1275495cf69b58535ab2
PsypherPunk <psypherpunk@gmail.com>
2020-10-10 19:24:46 +0100

    🌟 Added 2016 Day 20, Part Two.

--- 2016/20/src/main.rs
+++ 2016/20/src/main.rs
@@ -2,10 +2,9 @@ use std::cmp::{max, Ordering};
 use std::fs;
 
 type IpRange = (u32, u32);
-type Blocklist = Vec<IpRange>;
 
-fn get_blocklist(input: &str) -> Blocklist {
-    let mut ip_ranges: Blocklist = input
+fn get_blocklist(input: &str) -> Vec<IpRange> {
+    let mut ip_ranges: Vec<IpRange> = input
         .trim()
         .lines()
         .map(|line| {
@@ -20,6 +19,28 @@ fn get_blocklist(input: &str) -> Blocklist {
     ip_ranges.sort_by(|a, b| a.0.cmp(&b.0));
 
     ip_ranges
+}
+
+fn get_allowed_ip_count(blocklist: &[IpRange], limit: u32) -> u32 {
+    let mut allowed_ips = 0;
+
+    let mut current = 0;
+
+    for (lower, upper) in blocklist.iter() {
+        if let (Ordering::Less, Ordering::Less) = (current.cmp(&lower), current.cmp(&upper)) {
+            allowed_ips += (lower - current) - 1;
+        }
+        current = match upper.cmp(&limit) {
+            Ordering::Less => max(*upper, current),
+            Ordering::Equal => limit,
+            _ => current,
+        };
+    }
+    if current < limit {
+        allowed_ips += limit - current;
+    }
+
+    allowed_ips
 }
 
 fn get_lowest_ip_address(blocklist: &[IpRange]) -> u32 {
@@ -44,6 +65,11 @@ fn main() {
         "…what is the lowest-valued IP that is not blocked? {}",
         get_lowest_ip_address(&blocklist)
     );
+
+    println!(
+        "How many IPs are allowed by the blacklist? {}",
+        get_allowed_ip_count(&blocklist, 4294967295),
+    );
 }
 
 #[cfg(test)]
@@ -58,5 +84,17 @@ mod tests {
         let blocklist = get_blocklist(&input);
 
         assert_eq!(3, get_lowest_ip_address(&blocklist));
+    }
+
+    #[test]
+    fn test_allowed_ip_count() {
+        let input = r#"5-8
+0-2
+4-7"#;
+        let blocklist = get_blocklist(&input);
+
+        let allowed_ips = get_allowed_ip_count(&blocklist, 9);
+        dbg!(&allowed_ips);
+        assert_eq!(2, allowed_ips);
     }
 }

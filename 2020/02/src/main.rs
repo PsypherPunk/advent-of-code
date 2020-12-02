3553968b90262e3890b2d48a8954fceb93c50288
PsypherPunk <psypherpunk@gmail.com>
2020-12-02 08:54:52 +0000

    ⭐ Added 2020 Day 2, Part One.

---
+++ 2020/02/src/main.rs
@@ -0,0 +1,65 @@
+use std::fs;
+
+struct Policy {
+    min_count: usize,
+    max_count: usize,
+    letter: char,
+}
+
+fn get_policies_passwords(input: &str) -> Vec<(Policy, &str)> {
+    input
+        .trim()
+        .lines()
+        .map(|line| {
+            let parts = line.split_whitespace().collect::<Vec<&str>>();
+            let counts = parts[0].split('-').collect::<Vec<&str>>();
+            let letter = parts[1].chars().next().unwrap();
+            (
+                Policy {
+                    min_count: counts[0].parse().unwrap(),
+                    max_count: counts[1].parse().unwrap(),
+                    letter,
+                },
+                parts[2],
+            )
+        })
+        .collect()
+}
+
+fn is_valid_password(policy: &Policy, password: &str) -> bool {
+    let count = password.chars().filter(|c| *c == policy.letter).count();
+
+    count >= policy.min_count && count <= policy.max_count
+}
+
+fn get_valid_password_count(input: &str) -> usize {
+    let policies_passwords = get_policies_passwords(&input);
+
+    policies_passwords
+        .iter()
+        .filter(|(policy, password)| is_valid_password(policy, password))
+        .count()
+}
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+
+    println!(
+        "How many passwords are valid…? {}",
+        get_valid_password_count(&input),
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = r#"1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc""#;
+
+        assert_eq!(2, get_valid_password_count(&input));
+    }
+}

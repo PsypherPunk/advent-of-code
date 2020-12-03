43e62821167859694793b837e507142206c3fc9c
PsypherPunk <psypherpunk@gmail.com>
2020-12-03 08:47:45 +0000

    🌟 Added 2020 Day 3, Part Two.

--- 2020/03/src/main.rs
+++ 2020/03/src/main.rs
@@ -1,12 +1,15 @@
 use std::fs;
 
-fn get_tree_count(input: &str) -> usize {
+fn get_tree_count(input: &str, right: usize, down: usize) -> usize {
     input
         .trim()
         .lines()
         .enumerate()
+        .filter(|(num, _)| num % down == 0)
+        .map(|(_, line)| line)
+        .enumerate()
         .map(|(num, line)| {
-            let pos = (num * 3) % line.len();
+            let pos = (num * right) % line.len();
             match line.chars().nth(pos).unwrap() {
                 '.' => 0,
                 '#' => 1,
@@ -16,12 +19,24 @@ fn get_tree_count(input: &str) -> usize {
         .sum()
 }
 
+fn get_slopes_count(input: &str) -> usize {
+    [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+        .iter()
+        .map(|(right, down)| get_tree_count(&input, *right, *down))
+        .product()
+}
+
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!(
         "…how many trees would you encounter? {}",
-        get_tree_count(&input),
+        get_tree_count(&input, 3, 1),
+    );
+
+    println!(
+        "What do you get if you multiply together the number of trees encountered on each of the listed slopes? {}",
+        get_slopes_count(&input),
     );
 }
 
@@ -43,6 +58,23 @@ mod tests {
 #...##....#
 .#..#...#.#"#;
 
-        assert_eq!(7, get_tree_count(&input));
+        assert_eq!(7, get_tree_count(&input, 3, 1));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = r#"..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#"#;
+
+        assert_eq!(336, get_slopes_count(&input));
     }
 }

6cc4bc5b3911cd6407ce2d8ba7ce9c38c039b731
PsypherPunk <psypherpunk@gmail.com>
2020-12-12 10:36:57 +0000

    🌟 Added 2020 Day 11, Part Two.

--- 2020/11/src/main.rs
+++ 2020/11/src/main.rs
@@ -6,10 +6,16 @@ use ::day11::*;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
-    let seat_layout = SeatLayout::from_str(&input).unwrap();
+    let mut seat_layout = SeatLayout::from_str(&input).unwrap();
 
     println!(
         "How many seats end up occupied? {}",
         get_stable_layout(&seat_layout).get_occupied_seat_count(),
+    );
+
+    seat_layout.set_tolerance(5);
+    println!(
+        "…how many seats end up occupied? {}",
+        get_correct_stable_layout(&seat_layout).get_occupied_seat_count(),
     );
 }

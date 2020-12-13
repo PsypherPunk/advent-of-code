2344161064facc52692c3a062c3cb24a21ef7245
PsypherPunk <psypherpunk@gmail.com>
2020-12-13 10:09:34 +0000

    🌟 Added 2020 Day 13, Part Two.

--- 2020/13/src/lib.rs
+++ 2020/13/src/lib.rs
@@ -1,8 +1,10 @@
 use std::str::FromStr;
 
+use ring_algorithm::chinese_remainder_theorem;
+
 pub struct Notes {
-    earliest_timestamp: usize,
-    bus_ids: Vec<Option<usize>>,
+    earliest_timestamp: isize,
+    bus_ids: Vec<Option<isize>>,
 }
 
 impl FromStr for Notes {
@@ -30,7 +32,7 @@ impl FromStr for Notes {
 }
 
 impl Notes {
-    pub fn get_earliest_bus_wait(&self) -> (usize, usize) {
+    pub fn get_earliest_bus_wait(&self) -> (isize, isize) {
         self.bus_ids
             .iter()
             .filter(|bus_id| bus_id.is_some())
@@ -40,6 +42,24 @@ impl Notes {
             })
             .min_by(|(_, a), (_, b)| a.cmp(&b))
             .unwrap()
+    }
+
+    pub fn get_earliest_timestamp_with_offset(&self) -> isize {
+        let bus_ids = self
+            .bus_ids
+            .iter()
+            .filter(|bus_id| bus_id.is_some())
+            .map(|bus_id| bus_id.unwrap())
+            .collect::<Vec<isize>>();
+        let offsets = self
+            .bus_ids
+            .iter()
+            .enumerate()
+            .filter(|(_, bus_id)| bus_id.is_some())
+            .map(|(offset, _)| offset as isize)
+            .collect::<Vec<isize>>();
+
+        chinese_remainder_theorem(&offsets, &bus_ids).unwrap().abs()
     }
 }
 
@@ -55,8 +75,14 @@ mod tests {
         let notes = Notes::from_str(&INPUT).unwrap();
 
         let (bus_id, wait) = notes.get_earliest_bus_wait();
-        dbg!((bus_id, wait));
 
         assert_eq!(295, bus_id * wait);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let notes = Notes::from_str(&INPUT).unwrap();
+
+        assert_eq!(1068781, notes.get_earliest_timestamp_with_offset());
     }
 }

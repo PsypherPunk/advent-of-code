f036e754ea9c7e1e1092b0960c85a9a91df7c6e2
PsypherPunk <psypherpunk@gmail.com>
2020-12-13 09:53:21 +0000

    ⭐ Added 2020 Day 13, Part One.

---
+++ 2020/13/src/main.rs
@@ -0,0 +1,15 @@
+use std::fs;
+use std::str::FromStr;
+
+use ::day13::*;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+    let notes = Notes::from_str(&input).unwrap();
+
+    let (bus_id, wait) = notes.get_earliest_bus_wait();
+    println!(
+        "What is the ID…multiplied by the number of minutes…? {}",
+        bus_id * wait,
+    );
+}

667db4483e11b72668ca5c6eff1686e27c0f64d3
PsypherPunk <psypherpunk@gmail.com>
2020-12-17 17:42:45 +0000

    🌟 Added 2017 Day 2, Part Two.

--- 2017/02/src/lib.rs
+++ 2017/02/src/lib.rs
@@ -1,3 +1,31 @@
+pub fn get_evenly_divisible_values(input: &str) -> usize {
+    input
+        .trim()
+        .lines()
+        .map(|line| {
+            let mut numbers = line
+                .trim()
+                .split_whitespace()
+                .map(|number| number.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+            numbers.sort_unstable();
+
+            let (a, b) = numbers
+                .iter()
+                .enumerate()
+                .flat_map(|(i, a)| {
+                    numbers
+                        .iter()
+                        .skip(i + 1)
+                        .map(move |b| (*a, *b))
+                })
+                .find(|(a, b)| *b % *a == 0)
+                .unwrap();
+            b / a
+        })
+        .sum()
+}
+
 pub fn get_checksum(input: &str) -> usize {
     input
         .trim()
@@ -26,5 +54,14 @@ mod tests {
 2 4 6 8"#;
 
         assert_eq!(18, get_checksum(&input));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = r#"5 9 2 8
+9 4 7 3
+3 8 6 5"#;
+
+        assert_eq!(9, get_evenly_divisible_values(&input));
     }
 }
--- 2017/02/src/main.rs
+++ 2017/02/src/main.rs
@@ -5,5 +5,10 @@ use ::day02::*;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
-    println!("What is the checksum…? {}", get_checksum(&input),);
+    println!("What is the checksum…? {}", get_checksum(&input));
+
+    println!(
+        "What is the sum of each row's result…? {}",
+        get_evenly_divisible_values(&input),
+    );
 }

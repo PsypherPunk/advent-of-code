1045283174baf60987b0bd4e7b666e9a0ae3e558
PsypherPunk <psypherpunk@gmail.com>
2020-12-17 11:54:24 +0000

    🌟 Added 2020 Day 17, Part Two.

--- 2020/17/src/main.rs
+++ 2020/17/src/main.rs
@@ -6,8 +6,13 @@ use ::day17::*;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
-    let grid = get_grid_after_cycles(Grid::from_str(&input).unwrap(), 6);
+    let grid = get_grid_after_cycles(Grid::from_str(&input).unwrap(), 3, 6);
+    println!(
+        "How many cubes are left in the active state after the sixth cycle? {}",
+        grid.get_active_cube_count(),
+    );
 
+    let grid = get_grid_after_cycles(Grid::from_str(&input).unwrap(), 4, 6);
     println!(
         "How many cubes are left in the active state after the sixth cycle? {}",
         grid.get_active_cube_count(),

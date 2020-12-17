c39c24af76f4debd37fbd216f0fb49a54eec63f2
PsypherPunk <psypherpunk@gmail.com>
2020-12-17 11:54:24 +0000

    🌟 Added 2020 Day 17, Part Two.

--- 2020/17/src/lib.rs
+++ 2020/17/src/lib.rs
@@ -1,45 +1,40 @@
 use std::collections::HashMap;
 use std::str::FromStr;
 
-type Coordinate = (isize, isize, isize);
+type Coordinate = (isize, isize, isize, isize);
 
 trait Neighbours<T> {
-    fn get_neighbours(&self) -> Vec<T>;
+    fn get_neighbours(&self, dimensions: usize) -> Vec<T>;
 }
 
 impl Neighbours<Coordinate> for Coordinate {
-    fn get_neighbours(&self) -> Vec<Coordinate> {
+    fn get_neighbours(&self, dimensions: usize) -> Vec<Coordinate> {
         let x = self.0;
         let y = self.1;
         let z = self.2;
-        vec![
-            (x - 1, y - 1, z),
-            (x, y - 1, z),
-            (x + 1, y - 1, z),
-            (x + 1, y, z),
-            (x + 1, y + 1, z),
-            (x, y + 1, z),
-            (x - 1, y + 1, z),
-            (x - 1, y, z),
-            (x - 1, y - 1, z - 1),
-            (x, y - 1, z - 1),
-            (x + 1, y - 1, z - 1),
-            (x + 1, y, z - 1),
-            (x + 1, y + 1, z - 1),
-            (x, y + 1, z - 1),
-            (x - 1, y + 1, z - 1),
-            (x - 1, y, z - 1),
-            (x, y, z - 1),
-            (x - 1, y - 1, z + 1),
-            (x, y - 1, z + 1),
-            (x + 1, y - 1, z + 1),
-            (x + 1, y, z + 1),
-            (x + 1, y + 1, z + 1),
-            (x, y + 1, z + 1),
-            (x - 1, y + 1, z + 1),
-            (x - 1, y, z + 1),
-            (x, y, z + 1),
-        ]
+        let w = self.3;
+
+        (-1..=1)
+            .flat_map(|dx| {
+                (-1..=1)
+                    .flat_map(|dy| {
+                        (-1..=1)
+                            .flat_map(|dz| {
+                                let range = match dimensions {
+                                    3 => (0..=0),
+                                    4 => (-1..=1),
+                                    _ => panic!("Invalid dimension: {}", dimensions),
+                                };
+                                range
+                                    .map(|dw| (x + dx, y + dy, z + dz, w + dw))
+                                    .collect::<Vec<_>>()
+                            })
+                            .collect::<Vec<_>>()
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .filter(|neighbour| *neighbour != (x, y, z, w))
+            .collect()
     }
 }
 
@@ -75,7 +70,7 @@ impl FromStr for Grid {
             .flat_map(|(y, line)| {
                 line.chars()
                     .enumerate()
-                    .map(|(x, c)| ((x as isize, y as isize, 0), Cube::from(c)))
+                    .map(|(x, c)| ((x as isize, y as isize, 0, 0), Cube::from(c)))
                     .collect::<Vec<_>>()
             })
             .collect();
@@ -96,8 +91,8 @@ impl Grid {
         self.coordinates.values().filter(|cube| cube.active).count()
     }
 
-    pub fn cycle(&self) -> Grid {
-        let ((min_x, min_y, min_z), (max_x, max_y, max_z)) = self.get_min_max();
+    pub fn cycle(&self, dimensions: usize) -> Grid {
+        let ((min_x, min_y, min_z, min_w), (max_x, max_y, max_z, max_w)) = self.get_min_max();
 
         Grid {
             coordinates: ((min_x - 1)..=(max_x + 1))
@@ -105,32 +100,42 @@ impl Grid {
                     ((min_y - 1)..=(max_y + 1))
                         .flat_map(move |y| {
                             ((min_z - 1)..=(max_z + 1))
-                                .map(move |z| {
-                                    let cube = match self.coordinates.get(&(x, y, z)) {
-                                        Some(cube) => cube.clone(),
-                                        None => Cube::from('.'),
+                                .flat_map(move |z| {
+                                    let range = match dimensions {
+                                        3 => (0..=0),
+                                        4 => ((min_w - 1)..=(max_w + 1)),
+                                        _ => panic!("Invalid dimension: {}", dimensions),
                                     };
+                                    range
+                                        .map(move |w| {
+                                            let cube = match self.coordinates.get(&(x, y, z, w)) {
+                                                Some(cube) => cube.clone(),
+                                                None => Cube::from('.'),
+                                            };
 
-                                    let active_neighbours = (x, y, z)
-                                        .get_neighbours()
-                                        .iter()
-                                        .map(|neighbour| match self.coordinates.get(neighbour) {
-                                            Some(cube) if cube.active => 1,
-                                            _ => 0,
+                                            let active_neighbours = (x, y, z, w)
+                                                .get_neighbours(dimensions)
+                                                .iter()
+                                                .filter(|neighbour| {
+                                                    matches!(self.coordinates.get(neighbour), Some(cube) if cube.active)
+                                                })
+                                                .count();
+
+                                            let active = match cube {
+                                                cube if cube.active
+                                                    && (2..=3).contains(&active_neighbours) =>
+                                                {
+                                                    true
+                                                }
+                                                cube if !cube.active && active_neighbours == 3 => {
+                                                    true
+                                                }
+                                                _ => false,
+                                            };
+
+                                            ((x, y, z, w), Cube { active })
                                         })
-                                        .sum::<usize>();
-
-                                    let active = match cube {
-                                        cube if cube.active
-                                            && (2..=3).contains(&active_neighbours) =>
-                                        {
-                                            true
-                                        }
-                                        cube if !cube.active && active_neighbours == 3 => true,
-                                        _ => false,
-                                    };
-
-                                    ((x, y, z), Cube { active })
+                                        .collect::<Vec<_>>()
                                 })
                                 .collect::<Vec<_>>()
                         })
@@ -141,10 +146,10 @@ impl Grid {
     }
 }
 
-pub fn get_grid_after_cycles(grid: Grid, cycles: usize) -> Grid {
+pub fn get_grid_after_cycles(grid: Grid, dimensions: usize, cycles: usize) -> Grid {
     let mut grid = grid;
     for _ in 0..cycles {
-        grid = grid.cycle();
+        grid = grid.cycle(dimensions);
     }
 
     grid
@@ -154,14 +159,21 @@ pub fn get_grid_after_cycles(grid: Grid, cycles: usize) -> Grid {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_part_one() {
-        let input = r##".#.
+    const INPUT: &'static str = r##".#.
 ..#
 ###"##;
 
-        let grid = get_grid_after_cycles(Grid::from_str(&input).unwrap(), 6);
+    #[test]
+    fn test_part_one() {
+        let grid = get_grid_after_cycles(Grid::from_str(&INPUT).unwrap(), 3, 6);
 
         assert_eq!(112, grid.get_active_cube_count(),);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let grid = get_grid_after_cycles(Grid::from_str(&INPUT).unwrap(), 4, 6);
+
+        assert_eq!(848, grid.get_active_cube_count(),);
     }
 }

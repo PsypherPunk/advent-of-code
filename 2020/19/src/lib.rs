44f9eb46eadec4c730d23fb859309703b66d5fad
PsypherPunk <psypherpunk@gmail.com>
2020-12-19 20:29:36 +0000

    🌟 Added 2020 Day 19, Part Two.

--- 2020/19/src/lib.rs
+++ 2020/19/src/lib.rs
@@ -114,16 +114,16 @@ impl Rule {
             return Vec::new();
         }
         match self {
+            Rule::SingleCharacter(c) => match tail.starts_with(&[*c]) {
+                true => vec![&tail[1..]],
+                _ => vec![],
+            },
             Rule::Rule(n) => rules.get(n).unwrap().matches(rules, tail),
             Rule::AtLeastOne(sub_rule_a, sub_rule_b) => sub_rule_a
                 .matches(rules, tail)
                 .into_iter()
                 .chain(sub_rule_b.matches(rules, tail))
                 .collect(),
-            Rule::SingleCharacter(c) => match tail.starts_with(&[*c]) {
-                true => vec![&tail[1..]],
-                _ => vec![],
-            },
             Rule::MatchPair(sub_rule_a, sub_rule_b) => sub_rule_a
                 .matches(rules, tail)
                 .iter()
@@ -166,5 +166,62 @@ aaaabbb;"#;
         let satellite = SatelliteConnection::from_str(&input).unwrap();
 
         assert_eq!(2, satellite.get_valid_message_count());
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = r#"42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"#
+            .replace("8: 42", "8: 42 | 42 8")
+            .replace("11: 42 31", "11: 42 31 | 42 11 31");
+
+        let satellite = SatelliteConnection::from_str(&input).unwrap();
+
+        assert_eq!(12, satellite.get_valid_message_count());
     }
 }
--- 2020/19/src/main.rs
+++ 2020/19/src/main.rs
@@ -12,4 +12,15 @@ fn main() {
         "How many messages completely match rule 0? {}",
         satellite.get_valid_message_count(),
     );
+
+    let part_two_input = input
+        .replace("8: 42", "8: 42 | 42 8")
+        .replace("11: 42 31", "11: 42 31 | 42 11 31");
+
+    let satellite = SatelliteConnection::from_str(&part_two_input).unwrap();
+
+    println!(
+        "After updating rules 8 and 11, how many messages completely match rule 0? {}",
+        satellite.get_valid_message_count(),
+    );
 }

d263e34a9716e20afa6c42842debf4514ba8369b
PsypherPunk <psypherpunk@gmail.com>
2020-12-21 12:17:18 +0000

    🌟 Added 2020 Day 20, Part Two.

--- 2020/20/src/main.rs
+++ 2020/20/src/main.rs
@@ -6,10 +6,16 @@ use ::day20::*;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
-    let image = Image::from_str(&input).unwrap();
-
+    let mut image = Image::from_str(&input).unwrap();
     println!(
         "What do you get if you multiply together the IDs of the four corner tiles? {}",
-        image.get_borders().iter().product::<usize>(),
+        image.get_corners().iter().product::<usize>(),
+    );
+
+    let mut image = Image::from_str(&input).unwrap();
+    let tile = image.get_final_image();
+    println!(
+        "How many # are not part of a sea monster? {}",
+        tile.find_sea_monsters(),
     );
 }

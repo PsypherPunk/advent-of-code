a2c48612a75c5efc8911eec24b97829e38298745
PsypherPunk <psypherpunk@gmail.com>
2020-12-22 15:07:24 +0000

    🌟 Added 2020 Day 22, Part Two.

--- 2020/22/src/lib.rs
+++ 2020/22/src/lib.rs
@@ -1,10 +1,13 @@
 use std::cmp::Ordering;
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 use std::str::FromStr;
 
+type Player = VecDeque<usize>;
+
 pub struct Game {
-    player_1: VecDeque<usize>,
-    player_2: VecDeque<usize>,
+    player_1: Player,
+    player_2: Player,
+    hands: HashSet<usize>,
 }
 
 impl FromStr for Game {
@@ -19,6 +22,7 @@ impl FromStr for Game {
         Ok(Game {
             player_1: Game::get_deck(player_1),
             player_2: Game::get_deck(player_2),
+            hands: HashSet::new(),
         })
     }
 }
@@ -32,44 +36,103 @@ impl Game {
             .collect()
     }
 
-    fn get_highest_score(&self) -> usize {
-        let player_1_score = self
-            .player_1
+    fn get_player_score(&self, player: &Player) -> usize {
+        player
             .iter()
             .rev()
             .enumerate()
             .map(|(position, card)| (position + 1) * card)
-            .sum::<usize>();
+            .sum()
+    }
 
-        let player_2_score = self
-            .player_2
-            .iter()
-            .rev()
-            .enumerate()
-            .map(|(position, card)| (position + 1) * card)
-            .sum::<usize>();
+    fn get_checksum(&self) -> usize {
+        self.get_player_score(&self.player_1) * self.get_player_score(&self.player_2)
+    }
 
-        player_1_score.max(player_2_score)
+    fn player_one_wins(&self) -> bool {
+        match self.player_1.is_empty() {
+            true => false,
+            false => match self.player_2.is_empty() {
+                true => true,
+                false => true,
+            },
+        }
+    }
+
+    fn play_single_round(&mut self, player_1_card: usize, player_2_card: usize) {
+        match player_1_card.cmp(&player_2_card) {
+            Ordering::Greater => {
+                self.player_1.push_back(player_1_card);
+                self.player_1.push_back(player_2_card);
+            }
+            Ordering::Less => {
+                self.player_2.push_back(player_2_card);
+                self.player_2.push_back(player_1_card);
+            }
+            _ => panic!("Invalid round: {}, {}", player_1_card, player_2_card),
+        }
     }
 
     pub fn get_combat_winning_score(&mut self) -> usize {
         while !self.player_1.is_empty() && !self.player_2.is_empty() {
+            let checksum = self.get_checksum();
+            if self.hands.contains(&checksum) {
+                break;
+            }
+
             let player_1_card = self.player_1.pop_front().unwrap();
             let player_2_card = self.player_2.pop_front().unwrap();
-            match player_1_card.cmp(&player_2_card) {
-                Ordering::Greater => {
-                    self.player_1.push_back(player_1_card);
-                    self.player_1.push_back(player_2_card);
+            self.play_single_round(player_1_card, player_2_card);
+        }
+
+        match self.player_one_wins() {
+            true => self.get_player_score(&self.player_1),
+            false => self.get_player_score(&self.player_2),
+        }
+    }
+
+    fn get_recursive_combat_winner(&mut self) -> bool {
+        while !self.player_1.is_empty() && !self.player_2.is_empty() {
+            let checksum = self.get_checksum();
+            if self.hands.contains(&checksum) {
+                break;
+            }
+            self.hands.insert(checksum);
+
+            let player_1_card = self.player_1.pop_front().unwrap();
+            let player_2_card = self.player_2.pop_front().unwrap();
+
+            if player_1_card <= self.player_1.len() && player_2_card <= self.player_2.len() {
+                let mut game = Game {
+                    player_1: self.player_1.clone(),
+                    player_2: self.player_2.clone(),
+                    hands: HashSet::new(),
+                };
+                game.player_1.resize(player_1_card, 0);
+                game.player_2.resize(player_2_card, 0);
+                match game.get_recursive_combat_winner() {
+                    true => {
+                        self.player_1.push_back(player_1_card);
+                        self.player_1.push_back(player_2_card);
+                    }
+                    false => {
+                        self.player_2.push_back(player_2_card);
+                        self.player_2.push_back(player_1_card);
+                    }
                 }
-                Ordering::Less => {
-                    self.player_2.push_back(player_2_card);
-                    self.player_2.push_back(player_1_card);
-                }
-                _ => panic!("Invalid round: {}, {}", player_1_card, player_2_card),
+            } else {
+                self.play_single_round(player_1_card, player_2_card);
             }
         }
 
-        self.get_highest_score()
+        self.player_one_wins()
+    }
+
+    pub fn get_recursive_combat_winning_score(&mut self) -> usize {
+        match self.get_recursive_combat_winner() {
+            true => self.get_player_score(&self.player_1),
+            false => self.get_player_score(&self.player_2),
+        }
     }
 }
 
@@ -96,5 +159,12 @@ Player 2:
         let mut game = Game::from_str(&INPUT).unwrap();
 
         assert_eq!(306, game.get_combat_winning_score());
+    }
+
+    #[test]
+    fn test_part_two() {
+        let mut game = Game::from_str(&INPUT).unwrap();
+
+        assert_eq!(291, game.get_recursive_combat_winning_score());
     }
 }
--- 2020/22/src/main.rs
+++ 2020/22/src/main.rs
@@ -12,4 +12,11 @@ fn main() {
         "What is the winning player's score? {}",
         game.get_combat_winning_score(),
     );
+
+    let mut game = Game::from_str(&input).unwrap();
+
+    println!(
+        "What is the winning player's score? {}",
+        game.get_recursive_combat_winning_score(),
+    );
 }

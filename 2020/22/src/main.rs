c7b33e31b8202b43aca751d0970c8d0e94974fff
PsypherPunk <psypherpunk@gmail.com>
2020-12-22 14:38:31 +0000

    ⭐ Added 2020 Day 22, Part One.

---
+++ 2020/22/src/main.rs
@@ -0,0 +1,15 @@
+use std::fs;
+use std::str::FromStr;
+
+use ::day22::*;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+
+    let mut game = Game::from_str(&input).unwrap();
+
+    println!(
+        "What is the winning player's score? {}",
+        game.get_combat_winning_score(),
+    );
+}

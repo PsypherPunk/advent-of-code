eff94761a253cd14600b411e1a409664353dd8d6
PsypherPunk <psypherpunk@gmail.com>
2020-12-26 10:24:51 +0000

    🌟 Added 2017 Day 5, Part Two.

--- 2017/05/src/lib.rs
+++ 2017/05/src/lib.rs
@@ -21,20 +21,46 @@ pub fn get_steps_to_exit(mut offsets: Vec<isize>) -> usize {
     steps
 }
 
+pub fn get_stranger_steps_to_exit(mut offsets: Vec<isize>) -> usize {
+    let mut steps = 0;
+    let mut position: isize = 0;
+
+    while position >= 0 && (position as usize) < offsets.len() {
+        let jump = offsets[position as usize];
+
+        offsets[position as usize] += match offsets[position as usize] {
+            o if o >= 3 => -1,
+            _ => 1,
+        };
+
+        position += jump;
+        steps += 1;
+    }
+
+    steps
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test() {
-        let input = r#"0
+    const INPUT: &'static str = r#"0
 3
 0
 1
 -3"#;
 
-        let offsets = get_offsets(&input);
+    #[test]
+    fn test_part_one() {
+        let offsets = get_offsets(&INPUT);
 
         assert_eq!(5, get_steps_to_exit(offsets));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let offsets = get_offsets(&INPUT);
+
+        assert_eq!(10, get_stranger_steps_to_exit(offsets));
     }
 }
--- 2017/05/src/main.rs
+++ 2017/05/src/main.rs
@@ -6,9 +6,14 @@ fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     let offsets = get_offsets(&input);
-
     println!(
         "How many steps does it take to reach the exit? {}",
         get_steps_to_exit(offsets),
+    );
+
+    let offsets = get_offsets(&input);
+    println!(
+        "How many steps does it now take to reach the exit? {}",
+        get_stranger_steps_to_exit(offsets),
     );
 }

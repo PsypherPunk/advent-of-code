c0a74f8d75e34d2dd4b1ee9368703da34e36cbe9
PsypherPunk <psypherpunk@gmail.com>
2020-12-28 19:26:59 +0000

    ⭐ Added 2017 Day 7, Part One.

---
+++ 2017/07/src/main.rs
@@ -0,0 +1,15 @@
+use std::fs;
+use std::str::FromStr;
+
+use ::day07::*;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+
+    let tower = Tower::from_str(&input).unwrap();
+
+    println!(
+        "What is the name of the bottom program? {}",
+        tower.get_bottom_program(),
+    );
+}

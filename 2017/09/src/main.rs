8cbc804918eb0f093e9e0c244c3f5b909e2a17a7
PsypherPunk <psypherpunk@gmail.com>
2021-01-24 10:39:54 +0000

    🌟 Added 2017 Day 9, Part Two.

--- 2017/09/src/main.rs
+++ 2017/09/src/main.rs
@@ -5,5 +5,13 @@ use ::day09::*;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
-    println!("What is the total score…? {}", get_score_for_input(&input));
+    println!(
+        "What is the total score…? {}",
+        get_stream_for_input(&input).score,
+    );
+
+    println!(
+        "How many non-canceled characters are within the garbage…? {}",
+        get_stream_for_input(&input).garbage,
+    );
 }

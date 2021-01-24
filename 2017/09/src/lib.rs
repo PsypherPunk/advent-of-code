04d1af6ed848ba9996ac38f145cec31c7b04127c
PsypherPunk <psypherpunk@gmail.com>
2021-01-24 10:39:54 +0000

    🌟 Added 2017 Day 9, Part Two.

--- 2017/09/src/lib.rs
+++ 2017/09/src/lib.rs
@@ -1,8 +1,16 @@
-pub fn get_score_for_input(input: &str) -> usize {
+pub struct Stream {
+    pub score: usize,
+    pub garbage: usize,
+}
+
+pub fn get_stream_for_input(input: &str) -> Stream {
     let mut chars = input.chars().peekable();
 
     let mut depth = 0;
-    let mut score = 0;
+    let mut stream = Stream {
+        score: 0,
+        garbage: 0,
+    };
     let mut garbage = false;
 
     while chars.peek().is_some() {
@@ -11,28 +19,40 @@ pub fn get_score_for_input(input: &str) -> usize {
             '{' => {
                 if !garbage {
                     depth += 1;
+                } else {
+                    stream.garbage += 1;
                 }
             }
             '}' => {
                 if !garbage {
-                    score += depth;
+                    stream.score += depth;
                     depth -= 1;
+                } else {
+                    stream.garbage += 1;
                 }
             }
             '!' => {
                 let _ = chars.next().unwrap();
             }
             '<' => {
-                garbage = true;
+                if garbage {
+                    stream.garbage += 1;
+                } else {
+                    garbage = true;
+                }
             }
             '>' => {
                 garbage = false;
             }
-            _ => {}
+            _ => {
+                if garbage {
+                    stream.garbage += 1;
+                }
+            }
         }
     }
 
-    score
+    stream
 }
 
 #[cfg(test)]
@@ -41,13 +61,33 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        assert_eq!(get_score_for_input("{}"), 1);
-        assert_eq!(get_score_for_input("{{{}}}"), 6);
-        assert_eq!(get_score_for_input("{{},{}}"), 5);
-        assert_eq!(get_score_for_input("{{{},{},{{}}}}"), 16);
-        assert_eq!(get_score_for_input("{<a>,<a>,<a>,<a>}"), 1);
-        assert_eq!(get_score_for_input("{{<ab>},{<ab>},{<ab>},{<ab>}}"), 9);
-        assert_eq!(get_score_for_input("{{<!!>},{<!!>},{<!!>},{<!!>}}"), 9);
-        assert_eq!(get_score_for_input("{{<a!>},{<a!>},{<a!>},{<ab>}}"), 3);
+        assert_eq!(get_stream_for_input("{}").score, 1);
+        assert_eq!(get_stream_for_input("{{{}}}").score, 6);
+        assert_eq!(get_stream_for_input("{{},{}}").score, 5);
+        assert_eq!(get_stream_for_input("{{{},{},{{}}}}").score, 16);
+        assert_eq!(get_stream_for_input("{<a>,<a>,<a>,<a>}").score, 1);
+        assert_eq!(
+            get_stream_for_input("{{<ab>},{<ab>},{<ab>},{<ab>}}").score,
+            9
+        );
+        assert_eq!(
+            get_stream_for_input("{{<!!>},{<!!>},{<!!>},{<!!>}}").score,
+            9
+        );
+        assert_eq!(
+            get_stream_for_input("{{<a!>},{<a!>},{<a!>},{<ab>}}").score,
+            3
+        );
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(get_stream_for_input("<>").garbage, 0);
+        assert_eq!(get_stream_for_input("<random characters>").garbage, 17);
+        assert_eq!(get_stream_for_input("<<<<>").garbage, 3);
+        assert_eq!(get_stream_for_input("<{!>}>").garbage, 2);
+        assert_eq!(get_stream_for_input("<!!>").garbage, 0);
+        assert_eq!(get_stream_for_input("<!!!>>").garbage, 0);
+        assert_eq!(get_stream_for_input(r#"<{o"i!a,<{i<a>"#).garbage, 10);
     }
 }

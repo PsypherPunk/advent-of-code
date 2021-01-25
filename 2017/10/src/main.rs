399b7bc1536610542e2b580f1b78822c98a17930
PsypherPunk <psypherpunk@gmail.com>
2021-01-25 17:14:06 +0000

    🌟 Added 2017 Day 10, Part Two.

--- 2017/10/src/main.rs
+++ 2017/10/src/main.rs
@@ -12,4 +12,12 @@ fn main() {
         "…what is the result of multiplying the first two numbers in the list? {}",
         knot_hash.get_product_of_first_two_numbers(),
     );
+
+    let lengths = get_ascii_lengths(&input);
+    let mut knot_hash = KnotHash::new(0, 255);
+    knot_hash.generate_sparse_hash(&lengths);
+    println!(
+        "…what is the Knot Hash of your puzzle input? {}",
+        knot_hash.get_dense_hash(),
+    );
 }

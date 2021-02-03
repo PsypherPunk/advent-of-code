b607afc3c27cd18f9497eb1e5ec11833787bd6a4
PsypherPunk <psypherpunk@gmail.com>
2021-02-03 11:33:29 +0000

    🌟 Added 2017 Day 11, Part Two.

--- 2017/11/src/main.rs
+++ 2017/11/src/main.rs
@@ -5,8 +5,12 @@ use ::day11::*;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
+    let (position, max) = read_path(&input);
+
     println!(
         "…you need to determine the fewest number of steps required to reach him… {}",
-        get_steps(read_path(&input)),
+        get_steps(position),
     );
+
+    println!("How many steps away is the furthest…? {}", max);
 }

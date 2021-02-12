317f6adfc0592d7b759ec144487735443d070d82
PsypherPunk <psypherpunk@gmail.com>
2021-02-12 14:22:54 +0000

    🌟 Added 2017 Day 15, Part Two.

--- 2017/15/src/lib.rs
+++ 2017/15/src/lib.rs
@@ -5,6 +5,7 @@ const DIVISOR: usize = 2_147_483_647;
 struct Generator {
     factor: usize,
     previous: usize,
+    criterion: usize,
 }
 
 pub struct Generators {
@@ -24,10 +25,12 @@ impl FromStr for Generators {
             a: Generator {
                 factor: 16807,
                 previous: a_previous,
+                criterion: 4,
             },
             b: Generator {
                 factor: 48271,
                 previous: b_previous,
+                criterion: 8,
             },
         })
     }
@@ -38,8 +41,23 @@ impl Generators {
         let mut count = 0;
 
         for _ in 0..pairs {
-            let a_next = self.a.next() as u16;
-            let b_next = self.b.next() as u16;
+            let a_next = self.a.get_next() as u16;
+            let b_next = self.b.get_next() as u16;
+
+            if a_next == b_next {
+                count += 1;
+            }
+        }
+
+        count
+    }
+
+    pub fn get_picky_judgement(&mut self, pairs: usize) -> usize {
+        let mut count = 0;
+
+        for _ in 0..pairs {
+            let a_next = self.a.get_next_meeting_criteria() as u16;
+            let b_next = self.b.get_next_meeting_criteria() as u16;
 
             if a_next == b_next {
                 count += 1;
@@ -51,11 +69,20 @@ impl Generators {
 }
 
 impl Generator {
-    fn next(&mut self) -> usize {
+    fn get_next(&mut self) -> usize {
         let next = (self.previous * self.factor) % DIVISOR;
         self.previous = next;
 
         next
+    }
+
+    fn get_next_meeting_criteria(&mut self) -> usize {
+        loop {
+            let next = self.get_next();
+            if next % self.criterion == 0 {
+                return next;
+            }
+        }
     }
 }
 
@@ -77,22 +104,24 @@ mod tests {
         let mut a = Generator {
             factor: 16807,
             previous: 65,
+            criterion: 4,
         };
         let mut b = Generator {
             factor: 48271,
             previous: 8921,
+            criterion: 8,
         };
 
-        assert_eq!(1092455, a.next());
-        assert_eq!(1181022009, a.next());
-        assert_eq!(245556042, a.next());
-        assert_eq!(1744312007, a.next());
-        assert_eq!(1352636452, a.next());
-        assert_eq!(430625591, b.next());
-        assert_eq!(1233683848, b.next());
-        assert_eq!(1431495498, b.next());
-        assert_eq!(137874439, b.next());
-        assert_eq!(285222916, b.next());
+        assert_eq!(1092455, a.get_next());
+        assert_eq!(1181022009, a.get_next());
+        assert_eq!(245556042, a.get_next());
+        assert_eq!(1744312007, a.get_next());
+        assert_eq!(1352636452, a.get_next());
+        assert_eq!(430625591, b.get_next());
+        assert_eq!(1233683848, b.get_next());
+        assert_eq!(1431495498, b.get_next());
+        assert_eq!(137874439, b.get_next());
+        assert_eq!(285222916, b.get_next());
     }
 
     #[test]
@@ -101,12 +130,57 @@ mod tests {
             a: Generator {
                 factor: 16807,
                 previous: 65,
+                criterion: 4,
             },
             b: Generator {
                 factor: 48271,
                 previous: 8921,
+                criterion: 8,
             },
         };
         assert_eq!(1, generators.get_judgement(5));
+    }
+
+    #[test]
+    fn test_part_two_values() {
+        let mut a = Generator {
+            factor: 16807,
+            previous: 65,
+            criterion: 4,
+        };
+        let mut b = Generator {
+            factor: 48271,
+            previous: 8921,
+            criterion: 8,
+        };
+
+        assert_eq!(1352636452, a.get_next_meeting_criteria());
+        assert_eq!(1992081072, a.get_next_meeting_criteria());
+        assert_eq!(530830436, a.get_next_meeting_criteria());
+        assert_eq!(1980017072, a.get_next_meeting_criteria());
+        assert_eq!(740335192, a.get_next_meeting_criteria());
+        assert_eq!(1233683848, b.get_next_meeting_criteria());
+        assert_eq!(862516352, b.get_next_meeting_criteria());
+        assert_eq!(1159784568, b.get_next_meeting_criteria());
+        assert_eq!(1616057672, b.get_next_meeting_criteria());
+        assert_eq!(412269392, b.get_next_meeting_criteria());
+    }
+
+    #[test]
+    fn test_part_two_generate() {
+        let mut generators = Generators {
+            a: Generator {
+                factor: 16807,
+                previous: 65,
+                criterion: 4,
+            },
+            b: Generator {
+                factor: 48271,
+                previous: 8921,
+                criterion: 8,
+            },
+        };
+        assert_eq!(0, generators.get_picky_judgement(4));
+        assert_eq!(1, generators.get_picky_judgement(1056));
     }
 }
--- 2017/15/src/main.rs
+++ 2017/15/src/main.rs
@@ -7,9 +7,14 @@ fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     let mut generators = Generators::from_str(&input).unwrap();
-
     println!(
         "After 40 million pairs, what is the judge's final count? {}",
         generators.get_judgement(40_000_000),
+    );
+
+    let mut generators = Generators::from_str(&input).unwrap();
+    println!(
+        "…what is the judge's final count? {}",
+        generators.get_picky_judgement(5_000_000),
     );
 }

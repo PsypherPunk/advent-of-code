8031bf4ab3e372c666a2a57fb885b17b2776366a
PsypherPunk <psypherpunk@gmail.com>
2021-02-14 11:32:20 +0000

    🌟 Added 2017 Day 16, Part Two.

--- 2017/16/src/main.rs
+++ 2017/16/src/main.rs
@@ -9,4 +9,9 @@ fn main() {
         "In what order are the programs standing…? {}",
         perform_dance(&input),
     );
+
+    println!(
+        "In what order are the programs standing…? {}",
+        perform_dances(16, &input, 1_000_000_000),
+    );
 }

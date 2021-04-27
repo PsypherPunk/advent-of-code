37727c49eb2e9d529a4a5bd0b730bcd13f1b12fa
PsypherPunk <psypherpunk@gmail.com>
2021-04-27 13:08:02 +0100

    🌟 Added 2017 Day 18, Part Two.

--- 2017/18/src/main.rs
+++ 2017/18/src/main.rs
@@ -5,8 +5,18 @@ use ::day18::*;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
+    let mut zero = Program::new(&input, 0);
+    let mut one = Program::new(&input, 1);
+
+    Program::duet(&mut zero, &mut one);
+
     println!(
         "What is the value of the recovered frequency…? {}",
-        do_duet(&input),
+        zero.first_non_zero_recv,
+    );
+
+    println!(
+        "…how many times did program 1 send a value? {}",
+        one.sent_count,
     );
 }

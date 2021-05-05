d1cb6a70af8c826bc4f8c88462988b991455762b
PsypherPunk <psypherpunk@gmail.com>
2021-05-05 20:54:38 +0100

    🌟 Added 2017 Day 19, Part Two.

--- 2017/19/src/main.rs
+++ 2017/19/src/main.rs
@@ -6,10 +6,16 @@ use ::day19::*;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
-    let routing_diagram = RoutingDiagram::from_str(&input).unwrap();
+    let mut routing_diagram = RoutingDiagram::from_str(&input).unwrap();
+    routing_diagram.follow_diagram();
 
     println!(
         "What letters will it see…? {}",
         routing_diagram.get_letters(),
+    );
+
+    println!(
+        "How many steps…? {}",
+        routing_diagram.get_steps(),
     );
 }

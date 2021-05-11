9c2318ab63ccc907c749f4409b503eb738799ec6
PsypherPunk <psypherpunk@gmail.com>
2021-05-11 18:15:51 +0100

    🌟 Added 2017 Day 21, Part Two.

--- 2017/21/src/main.rs
+++ 2017/21/src/main.rs
@@ -7,13 +7,22 @@ fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     let rules = Rules::from_str(&input).unwrap();
+
     let mut image = Image::new();
     for _ in 0..5 {
         image = image.get_iteration(&rules);
     }
-
     println!(
         "How many pixels stay on after 5 iterations? {}",
+        image.get_on_count(),
+    );
+
+    let mut image = Image::new();
+    for _ in 0..18 {
+        image = image.get_iteration(&rules);
+    }
+    println!(
+        "How many pixels stay on after 18 iterations? {}",
         image.get_on_count(),
     );
 }

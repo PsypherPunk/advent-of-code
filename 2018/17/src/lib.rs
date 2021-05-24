13ef305843d5914d5b75d47a9415b9f05e56e07e
PsypherPunk <psypherpunk@gmail.com>
2021-05-24 22:26:25 +0100

    🌟 Added 2018 Day 17, Part Two.

--- 2018/17/src/lib.rs
+++ 2018/17/src/lib.rs
@@ -3,7 +3,7 @@ use std::ops::RangeInclusive;
 use std::str::FromStr;
 
 #[derive(Clone, PartialEq)]
-enum SquareMetre {
+enum Tile {
     Sand,
     Clay,
     WaterFlow,
@@ -11,7 +11,7 @@ enum SquareMetre {
 }
 
 pub struct Grid {
-    square_metres: HashMap<(usize, usize), SquareMetre>,
+    tiles: HashMap<(usize, usize), Tile>,
     min: (usize, usize),
 }
 
@@ -61,12 +61,12 @@ impl FromStr for Grid {
         let max_x = clay.iter().map(|c| *c.x.end()).max().unwrap();
         let max_y = clay.iter().map(|c| *c.y.end()).max().unwrap();
 
-        let square_metres = (0..=(max_y - min_y))
-            .flat_map(|y| (0..=(max_x - min_x)).map(move |x| ((x, y), SquareMetre::Sand)))
+        let tiles = (0..=(max_y - min_y))
+            .flat_map(|y| (0..=(max_x - min_x)).map(move |x| ((x, y), Tile::Sand)))
             .collect::<HashMap<_, _>>();
 
         let mut grid = Grid {
-            square_metres,
+            tiles,
             min: (min_x, min_y),
         };
 
@@ -76,8 +76,7 @@ impl FromStr for Grid {
                     .flat_map(move |y| c.x.clone().map(move |x| (x, y)))
             })
             .for_each(|(x, y)| {
-                grid.square_metres
-                    .insert((x - min_x + 1, y - min_y), SquareMetre::Clay);
+                grid.tiles.insert((x - min_x + 1, y - min_y), Tile::Clay);
             });
 
         Ok(grid)
@@ -85,60 +84,58 @@ impl FromStr for Grid {
 }
 
 impl Grid {
-    pub fn get_square_metres_water(&mut self) -> usize {
+    pub fn get_square_metres_water(&mut self) -> (usize, usize) {
         let left = 500 - self.min.0 + 1;
-        self.get_square_metre(left, 0, &Direction::Both);
+        self.get_tile(left, 0, &Direction::Both);
 
-        self.square_metres
+        self.tiles
             .values()
             .map(|square_meter| {
-                matches!(
-                    square_meter,
-                    SquareMetre::WaterFlow | SquareMetre::WaterRest
-                ) as usize
+                (
+                    matches!(square_meter, Tile::WaterFlow | Tile::WaterRest) as usize,
+                    matches!(square_meter, Tile::WaterRest) as usize,
+                )
             })
-            .sum()
+            .fold((0, 0), |(total_water, total_rest), (water, rest)| {
+                (total_water + water, total_rest + rest)
+            })
     }
 
-    fn get_square_metre(&mut self, x: usize, y: usize, direction: &Direction) -> Option<usize> {
-        if y == self.square_metres.len() {
+    fn get_tile(&mut self, x: usize, y: usize, direction: &Direction) -> Option<usize> {
+        if y == self.tiles.len() {
             return None;
         }
 
-        match self.square_metres.get(&(x, y))? {
-            SquareMetre::Clay | SquareMetre::WaterRest => Some(x),
-            SquareMetre::WaterFlow => None,
-            SquareMetre::Sand => {
-                self.square_metres.insert((x, y), SquareMetre::WaterFlow);
-                self.get_square_metre(x, y + 1, &Direction::Both)?;
+        match self.tiles.get(&(x, y))? {
+            Tile::Clay | Tile::WaterRest => Some(x),
+            Tile::WaterFlow => None,
+            Tile::Sand => {
+                self.tiles.insert((x, y), Tile::WaterFlow);
+                self.get_tile(x, y + 1, &Direction::Both)?;
                 match direction {
                     Direction::Both => {
                         match (
-                            self.get_square_metre(x - 1, y, &Direction::Left),
-                            self.get_square_metre(x + 1, y, &Direction::Right),
+                            self.get_tile(x - 1, y, &Direction::Left),
+                            self.get_tile(x + 1, y, &Direction::Right),
                         ) {
                             (Some(left), Some(right)) => {
-                                let mut row = self
-                                    .square_metres
+                                let water = self
+                                    .tiles
                                     .keys()
-                                    .filter(|(_, down)| *down == y)
-                                    .collect::<Vec<_>>();
-                                row.sort_by_key(|(x, _)| *x);
-                                let water = row
-                                    .into_iter()
-                                    .skip(left + 1)
-                                    .take(right - left - 1)
-                                    .map(|square_metre| (*square_metre, SquareMetre::WaterRest))
+                                    .filter(|(row, col)| {
+                                        *col == y && *row >= left + 1 && *row <= right - 1
+                                    })
+                                    .map(|tile| (*tile, Tile::WaterRest))
                                     .collect::<HashMap<_, _>>();
-                                self.square_metres.extend(water);
+                                self.tiles.extend(water);
 
                                 Some(x)
                             }
                             _ => None,
                         }
                     }
-                    Direction::Left => self.get_square_metre(x - 1, y, &Direction::Left),
-                    Direction::Right => self.get_square_metre(x + 1, y, &Direction::Right),
+                    Direction::Left => self.get_tile(x - 1, y, &Direction::Left),
+                    Direction::Right => self.get_tile(x + 1, y, &Direction::Right),
                 }
             }
         }
@@ -149,9 +146,7 @@ impl Grid {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_part_one() {
-        let input = r#"x=495, y=2..7
+    const INPUT: &str = r#"x=495, y=2..7
 y=7, x=495..501
 x=501, y=3..7
 x=498, y=2..4
@@ -160,8 +155,17 @@ x=498, y=10..13
 x=504, y=10..13
 y=13, x=498..504"#;
 
-        let mut grid = Grid::from_str(&input).unwrap();
+    #[test]
+    fn test_part_one() {
+        let mut grid = Grid::from_str(&INPUT).unwrap();
 
-        assert_eq!(57, grid.get_square_metres_water());
+        assert_eq!(57, grid.get_square_metres_water().0);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let mut grid = Grid::from_str(&INPUT).unwrap();
+
+        assert_eq!(29, grid.get_square_metres_water().1);
     }
 }

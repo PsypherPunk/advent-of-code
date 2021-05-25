3ec89035f21a9498788e1ea4c147ee5f992a6fe7
PsypherPunk <psypherpunk@gmail.com>
2021-05-25 23:38:10 +0100

    🔨 Updated 2018 Day 7, Part One.

---
+++ 2018/07/src/lib.rs
@@ -0,0 +1,75 @@
+use std::collections::HashMap;
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub struct Instructions {
+    dependents: HashMap<char, Vec<char>>,
+}
+
+impl FromStr for Instructions {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut dependents = HashMap::new();
+
+        s.trim().lines().for_each(|line| {
+            let chars = line.chars().collect::<Vec<_>>();
+
+            dependents.entry(chars[36]).or_insert_with(Vec::new);
+            dependents.entry(chars[5]).or_default().push(chars[36]);
+        });
+
+        Ok(Self { dependents })
+    }
+}
+
+impl Instructions {
+    /// Get the order of steps for the instructions.
+    ///
+    /// Find the step which does not occur in any other step's
+    /// dependents, then remove this causing its own dependents to
+    /// occur in one fewer set of dependents.
+    pub fn get_steps_order(&self) -> String {
+        let mut steps = self.dependents.clone();
+        let mut output = Vec::new();
+
+        while let Some(&step) = steps
+            .iter()
+            .map(|(step, _)| {
+                let count = steps
+                    .values()
+                    .map(|dependents| dependents.contains(step) as usize)
+                    .sum::<usize>();
+                (step, count)
+            })
+            .filter(|(_, count)| *count == 0)
+            .map(|(step, _)| step)
+            .min()
+        {
+            output.push(step);
+            steps.remove(&step);
+        }
+
+        output.into_iter().collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = r#"Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin."#;
+
+    #[test]
+    fn test_part_one() {
+        let instructions = Instructions::from_str(&INPUT).unwrap();
+
+        assert_eq!("CABDFE", instructions.get_steps_order());
+    }
+}

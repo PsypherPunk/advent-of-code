2594cebf95d978be11629d84a46adabef0d14bc7
PsypherPunk <psypherpunk@gmail.com>
2021-06-02 20:39:27 +0100

    🌟 Added 2018 Day 21, Part Two.

--- 2018/21/src/main.rs
+++ 2018/21/src/main.rs
@@ -8,7 +8,13 @@ fn main() {
 
     let mut cpu = Cpu::from_str(&input).unwrap();
     println!(
-        "What is the lowest…integer…that causes the program to halt…? {}",
-        cpu.get_register_zero_halt(),
+        "What is the lowest…integer…that causes the program to halt…the fewest instructions? {}",
+        cpu.get_register_zero_halt().unwrap(),
+    );
+
+    let mut cpu = Cpu::from_str(&input).unwrap();
+    println!(
+        "What is the lowest…integer…that causes the program to halt after…the most instructions? {}",
+        cpu.execute_part_two().unwrap(),
     );
 }

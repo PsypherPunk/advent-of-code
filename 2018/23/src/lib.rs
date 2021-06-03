4b5a0c916df0c7da44635bc6b3b4b2c078448883
PsypherPunk <psypherpunk@gmail.com>
2021-06-03 20:39:21 +0100

    🌟 Added 2018 Day 23, Part Two.

--- 2018/23/src/lib.rs
+++ 2018/23/src/lib.rs
@@ -1,19 +1,31 @@
-struct Position {
+#[derive(Clone, Copy, Debug)]
+pub struct Position {
     x: isize,
     y: isize,
     z: isize,
 }
 
+#[derive(Clone, Copy, Debug)]
 pub struct Nanobot {
     position: Position,
     radius: usize,
 }
 
+impl Default for Position {
+    fn default() -> Self {
+        Self { x: 0, y: 0, z: 0 }
+    }
+}
+
+impl Position {
+    pub fn get_manhattan_distance(&self, other: &Position) -> usize {
+        ((self.x - other.x).abs() + (self.y - other.y).abs() + (self.z - other.z).abs()) as usize
+    }
+}
+
 impl Nanobot {
-    fn get_manhattan_distance(&self, other: &Nanobot) -> usize {
-        ((self.position.x - other.position.x).abs()
-            + (self.position.y - other.position.y).abs()
-            + (self.position.z - other.position.z).abs()) as usize
+    fn overlaps(&self, other: &Nanobot) -> bool {
+        self.position.get_manhattan_distance(&other.position) <= self.radius + other.radius
     }
 }
 
@@ -52,8 +64,42 @@ pub fn get_strongest_nanobot(nanobots: &[Nanobot]) -> &Nanobot {
 pub fn get_nanobots_in_range(nanobot: &Nanobot, nanobots: &[Nanobot]) -> usize {
     nanobots
         .iter()
-        .filter(|other| nanobot.get_manhattan_distance(&other) <= nanobot.radius)
+        .filter(|other| nanobot.position.get_manhattan_distance(&other.position) <= nanobot.radius)
         .count()
+}
+
+pub fn get_teleportation_position_distance(nanobots: &[Nanobot]) -> usize {
+    let most_overlapping_nanobots = nanobots
+        .iter()
+        .enumerate()
+        .map(|(i, nanobot)| {
+            let mut overlapping: Vec<Nanobot> = Vec::new();
+
+            for other in nanobots
+                .iter()
+                .skip(i)
+                .filter(|other| other.overlaps(nanobot))
+            {
+                if overlapping.iter().all(|b2| b2.overlaps(other)) {
+                    overlapping.push(*other);
+                }
+            }
+
+            overlapping
+        })
+        .max_by_key(|overlapping_nanobots| overlapping_nanobots.len())
+        .unwrap();
+
+    most_overlapping_nanobots
+        .iter()
+        .map(|nanobot| {
+            nanobot
+                .position
+                .get_manhattan_distance(&Position::default())
+                .saturating_sub(nanobot.radius)
+        })
+        .max()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -94,7 +140,7 @@ pos=<1,3,1>, r=1"#;
         let strongest = get_strongest_nanobot(&nanobots);
         let distances = nanobots
             .iter()
-            .map(|nanobot| strongest.get_manhattan_distance(&nanobot))
+            .map(|nanobot| strongest.position.get_manhattan_distance(&nanobot.position))
             .collect::<Vec<_>>();
 
         assert_eq!(vec![0, 1, 4, 2, 5, 3, 3, 4, 5], distances);
@@ -107,5 +153,19 @@ pos=<1,3,1>, r=1"#;
         let strongest = get_strongest_nanobot(&nanobots);
 
         assert_eq!(7, get_nanobots_in_range(&strongest, &nanobots));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = r#"pos=<10,12,12>, r=2
+pos=<12,14,12>, r=2
+pos=<16,12,12>, r=4
+pos=<14,14,14>, r=6
+pos=<50,50,50>, r=200
+pos=<10,10,10>, r=5"#;
+
+        let nanobots = wrist_device::nanobots(&input).unwrap();
+
+        assert_eq!(36, get_teleportation_position_distance(&nanobots))
     }
 }

8f3618d35e2e686e5384fd2627e87a278dc01604
PsypherPunk <psypherpunk@gmail.com>
2021-06-03 12:30:31 +0100

    🌟 Added 2018 Day 22, Part Two.

--- 2018/22/src/lib.rs
+++ 2018/22/src/lib.rs
@@ -1,12 +1,38 @@
-use std::collections::HashMap;
+use std::cmp::{Ordering, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::str::FromStr;
 
-#[allow(dead_code)]
 #[derive(Clone, Copy, Debug, PartialEq)]
 enum Type {
     Rocky,
     Narrow,
     Wet,
+}
+
+#[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+enum Tool {
+    Torch,
+    ClimbingGear,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+struct Move {
+    region: Coordinate,
+    tool: Option<Tool>,
+    minutes: usize,
+    distance_to_target: usize,
+}
+
+impl PartialOrd for Move {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(&other))
+    }
+}
+
+impl Ord for Move {
+    fn cmp(&self, other: &Self) -> Ordering {
+        (self.minutes + self.distance_to_target).cmp(&(other.minutes + other.distance_to_target))
+    }
 }
 
 type Coordinate = (usize, usize);
@@ -40,7 +66,25 @@ impl FromStr for Cave {
 }
 
 impl Cave {
-    #[allow(dead_code)]
+    fn get_manhattan_distance(&self, a: &Coordinate, b: &Coordinate) -> usize {
+        ((a.0 as isize - b.0 as isize).abs() + (a.1 as isize - b.1 as isize).abs()) as usize
+    }
+
+    fn get_neighbouring_regions(&self, coordinate: &Coordinate) -> Vec<Coordinate> {
+        let (x, y) = coordinate;
+
+        vec![
+            (*x, y.saturating_sub(1)),
+            (x + 1, *y),
+            (*x, y + 1),
+            (x.saturating_sub(1), *y),
+        ]
+        .into_iter()
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect()
+    }
+
     fn get_type(&mut self, coordinate: &Coordinate) -> Type {
         match self.get_erosion_level(&coordinate) % 3 {
             0 => Type::Rocky,
@@ -77,10 +121,105 @@ impl Cave {
         (0..=self.target.1)
             .map(|y| {
                 (0..=self.target.0)
-                    .map(|x| self.get_erosion_level(&(x, y)) % 3)
+                    .map(|x| match self.get_type(&(x, y)) {
+                        Type::Rocky => 0,
+                        Type::Wet => 1,
+                        Type::Narrow => 2,
+                    })
                     .sum::<usize>()
             })
             .sum()
+    }
+
+    fn get_is_tool_ok_for_region(&mut self, tool: &Option<Tool>, region: &Coordinate) -> bool {
+        match self.get_type(region) {
+            Type::Rocky => {
+                matches!(tool, Some(Tool::ClimbingGear)) || matches!(tool, Some(Tool::Torch))
+            }
+            Type::Wet => matches!(tool, Some(Tool::ClimbingGear)) || matches!(tool, None),
+            Type::Narrow => matches!(tool, Some(Tool::Torch)) || matches!(tool, None),
+        }
+    }
+
+    fn get_tools_ok_for_region(&mut self, region: &Coordinate) -> Vec<Option<Tool>> {
+        match self.get_type(region) {
+            Type::Rocky => vec![Some(Tool::ClimbingGear), Some(Tool::Torch)],
+            Type::Wet => vec![Some(Tool::ClimbingGear), None],
+            Type::Narrow => vec![Some(Tool::Torch), None],
+        }
+    }
+
+    fn get_right_tool(
+        &mut self,
+        current: &Coordinate,
+        next: &Coordinate,
+    ) -> Result<Option<Tool>, String> {
+        let next_tools = self.get_tools_ok_for_region(next);
+
+        self.get_tools_ok_for_region(current)
+            .into_iter()
+            .find(|tool| next_tools.contains(tool))
+            .ok_or_else(|| "Unable to find the right tool".to_owned())
+    }
+
+    pub fn get_quickest_time_to_target(&mut self) -> Result<usize, String> {
+        let mut queue = BinaryHeap::new();
+        let mut discovered = HashSet::new();
+
+        queue.push(Reverse(Move {
+            region: (0, 0),
+            tool: Some(Tool::Torch),
+            minutes: 0,
+            distance_to_target: self.get_manhattan_distance(&(0, 0), &self.target),
+        }));
+
+        while !queue.is_empty() {
+            let current = queue.pop().unwrap().0;
+            discovered.insert((current.region, current.tool));
+
+            if current.region == self.target {
+                return match current.tool {
+                    Some(Tool::Torch) => Ok(current.minutes),
+                    _ => Ok(current.minutes + 7),
+                };
+            }
+
+            let next_moves = self
+                .get_neighbouring_regions(&current.region)
+                .iter()
+                .map(|neighbouring_region| {
+                    let current_region_type = self.get_type(&current.region);
+                    let neighbouring_region_type = self.get_type(neighbouring_region);
+                    if neighbouring_region_type == current_region_type
+                        || self.get_is_tool_ok_for_region(&current.tool, &neighbouring_region)
+                    {
+                        Move {
+                            region: *neighbouring_region,
+                            tool: current.tool,
+                            minutes: current.minutes + 1,
+                            distance_to_target: self
+                                .get_manhattan_distance(&neighbouring_region, &self.target),
+                        }
+                    } else {
+                        Move {
+                            region: *neighbouring_region,
+                            tool: self
+                                .get_right_tool(&current.region, neighbouring_region)
+                                .unwrap(),
+                            minutes: current.minutes + 8,
+                            distance_to_target: self
+                                .get_manhattan_distance(&neighbouring_region, &self.target),
+                        }
+                    }
+                })
+                .filter(|next| !discovered.contains(&(next.region, next.tool)))
+                .collect::<Vec<_>>();
+            for next_move in next_moves {
+                queue.push(Reverse(next_move));
+            }
+        }
+
+        Err("No path to target found.".to_owned())
     }
 }
 
@@ -112,5 +251,16 @@ mod tests {
         assert_eq!(510, cave.get_erosion_level(&(10, 10)));
         assert_eq!(Type::Rocky, cave.get_type(&(10, 10)));
         assert_eq!(114, cave.get_risk_level());
+    }
+
+    #[test]
+    fn test_part_two() {
+        let mut cave = Cave {
+            depth: 510,
+            erosion: HashMap::new(),
+            target: (10, 10),
+        };
+
+        assert_eq!(45, cave.get_quickest_time_to_target().unwrap());
     }
 }

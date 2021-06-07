21350bd4104ca3609002a4c879292c85678b09b9
PsypherPunk <psypherpunk@gmail.com>
2021-06-07 21:40:32 +0100

    🌟 Added 2018 Day 24, Part Two.

--- 2018/24/src/main.rs
+++ 2018/24/src/main.rs
@@ -5,10 +5,15 @@ use ::day24::*;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
-    let mut reindeer: Reindeer = reindeer::reindeer(&input).unwrap();
+    let mut reindeer = reindeer::reindeer(&input).unwrap();
 
     println!(
         "…how many units would the winning army have? {}",
-        reindeer.fight(),
+        reindeer.fight().unwrap(),
+    );
+
+    println!(
+        "How many units does the immune system have left…? {}",
+        get_immune_system_boost(&input),
     );
 }

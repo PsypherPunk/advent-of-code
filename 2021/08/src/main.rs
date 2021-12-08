2921d85d33fbbe411bcd67062ff04d2c18286062
PsypherPunk <psypherpunk@gmail.com>
2021-12-08 17:23:41 +0000

    feat: 🌟 add 2021, day 8, part 2

--- 2021/08/src/main.rs
+++ 2021/08/src/main.rs
@@ -9,4 +9,9 @@ fn main() {
         "In the output values, how many times do digits 1, 4, 7, or 8 appear? {}",
         get_part_one(&input),
     );
+
+    println!(
+        "What do you get if you add up all of the output values? {}",
+        get_part_two(&input),
+    );
 }

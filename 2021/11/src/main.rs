d25b34c60520ed1d4ca590ab299aa6ee6867b4a0
PsypherPunk <psypherpunk@gmail.com>
2021-12-11 21:10:05 +0000

    feat: 🐙 add optional animation for day 11

--- 2021/11/src/main.rs
+++ 2021/11/src/main.rs
@@ -1,17 +1,27 @@
 use std::fs;
 
+use argh::FromArgs;
+
 use ::day11::*;
 
+#[derive(FromArgs)]
+#[argh(description = "Advent of Code 2021, day 11")]
+struct Args {
+    #[argh(switch, short = 'd', description = "whether to display animation")]
+    display: bool,
+}
+
 fn main() {
+    let args: Args = argh::from_env();
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!(
         "How many total flashes are there after 100 steps? {}",
-        get_part_one(&input),
+        get_part_one(&input, args.display),
     );
 
     println!(
         "What is the first step during which all octopuses flash? {}",
-        get_part_two(&input),
+        get_part_two(&input, args.display),
     );
 }

72821ab1e8e294cc14a7ec1bbe8cfdf3eb907aed
PsypherPunk <psypherpunk@gmail.com>
2021-12-13 10:00:37 +0000

    feat: 🌟 add 2021, day 13, part 2

--- 2021/13/src/lib.rs
+++ 2021/13/src/lib.rs
@@ -45,8 +45,37 @@ pub fn get_part_one(input: &str) -> usize {
         .len()
 }
 
-pub fn get_part_two(input: &str) -> isize {
-    0
+pub fn get_part_two(input: &str) -> String {
+    let (dots, folds) = get_instructions(input);
+
+    let dots = dots
+        .iter()
+        .map(|&(x, y)| {
+            folds.iter().fold((x, y), |(x, y), fold| match fold {
+                Fold::X(position) => (position - (x - position).abs(), y),
+                Fold::Y(position) => (x, position - (y - position).abs()),
+            })
+        })
+        .collect::<HashSet<_>>();
+
+    let (width, height) = folds
+        .iter()
+        .fold((usize::MAX, usize::MAX), |(x, y), fold| match fold {
+            Fold::X(position) => (x.min(*position as usize), y),
+            Fold::Y(position) => (x, y.min(*position as usize)),
+        });
+
+    (0..height)
+        .map(|y| {
+            (0..width)
+                .map(|x| match dots.contains(&(x as isize, y as isize)) {
+                    true => '⬛',
+                    false => '⬜',
+                })
+                .collect::<String>()
+        })
+        .collect::<Vec<_>>()
+        .join("\n")
 }
 
 #[cfg(test)]
@@ -83,6 +112,17 @@ fold along x=5
 
     #[test]
     fn test_part_two() {
-        assert_eq!(2, get_part_two(INPUT));
+        let output = r#"#####
+#...#
+#...#
+#...#
+#####
+.....
+....."#
+            .replace('.', "⬜")
+            .replace('#', "⬛")
+            .to_owned();
+
+        assert_eq!(output, get_part_two(INPUT));
     }
 }
--- 2021/13/src/main.rs
+++ 2021/13/src/main.rs
@@ -5,7 +5,13 @@ use ::day13::*;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
-    println!("How many dots are visible after completing just the first fold instruction on your transparent paper? {}", get_part_one(&input),);
+    println!(
+        "How many dots are visible after completing just the first fold instruction on your transparent paper? {}",
+        get_part_one(&input),
+    );
 
-    println!("{}", get_part_two(&input),);
+    println!(
+        "What code do you use to activate the infrared thermal imaging camera system?\n{}",
+        get_part_two(&input),
+    );
 }

5656ede3ee0c7df9a90d888b2f4584cc652ff09f
PsypherPunk <psypherpunk@gmail.com>
2021-12-15 11:06:32 +0000

    feat: ⭐ add 2021, day 15, part 1

---
+++ 2021/15/src/main.rs
@@ -0,0 +1,14 @@
+use std::fs;
+
+use ::day15::*;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+
+    println!(
+        "What is the lowest total risk of any path from the top left to the bottom right? {}",
+        get_part_one(&input),
+    );
+
+    println!("{}", get_part_two(&input),);
+}

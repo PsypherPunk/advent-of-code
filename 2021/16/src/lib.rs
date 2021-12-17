b40a09e019c7e160e9f71920e5375c8b20a9bffd
PsypherPunk <psypherpunk@gmail.com>
2021-12-17 16:12:16 +0000

    feat: 🌟 add 2021, day 16, part 2

--- 2021/16/src/lib.rs
+++ 2021/16/src/lib.rs
@@ -6,6 +6,13 @@ struct Transmission {
     bits: Vec<u8>,
 }
 
+#[derive(Debug)]
+struct Packet {
+    version: u64,
+    values: Vec<u64>,
+    type_id: u64,
+}
+
 impl FromStr for Transmission {
     type Err = String;
 
@@ -43,10 +50,30 @@ impl Transmission {
     }
 }
 
-fn get_version(transmission: &mut Transmission) -> u64 {
-    let mut version = transmission.get_number_from_bits(3);
+impl Packet {
+    fn get_value(&self) -> u64 {
+        match self.type_id {
+            0 => self.values.iter().sum(),
+            1 => self.values.iter().product(),
+            2 => *self.values.iter().min().unwrap(),
+            3 => *self.values.iter().max().unwrap(),
+            5 => (self.values[0] > self.values[1]) as u64,
+            6 => (self.values[0] < self.values[1]) as u64,
+            7 => (self.values[0] == self.values[1]) as u64,
+            _ => panic!(),
+        }
+    }
+}
 
+fn get_packet(transmission: &mut Transmission) -> Packet {
+    let version = transmission.get_number_from_bits(3);
     let type_id = transmission.get_number_from_bits(3);
+
+    let mut packet = Packet {
+        version,
+        values: Vec::new(),
+        type_id,
+    };
 
     match type_id {
         4 => {
@@ -54,9 +81,10 @@ fn get_version(transmission: &mut Transmission) -> u64 {
             let mut buf = [0; 5];
             loop {
                 transmission.read_exact(&mut buf).unwrap();
-                n = n << 4 | buf[1..].iter().fold(0, |a, b| a << 1 | b);
+                n = n << 4 | buf[1..].iter().fold(0, |a, b| a << 1 | *b as u64);
                 if buf[0] == 0 {
-                    return version;
+                    packet.values.push(n);
+                    return packet;
                 }
             }
         }
@@ -67,13 +95,17 @@ fn get_version(transmission: &mut Transmission) -> u64 {
                     let packets_len = transmission.get_number_from_bits(15);
                     let start = transmission.position;
                     while transmission.position < start + packets_len as usize {
-                        version += get_version(transmission);
+                        let sub = get_packet(transmission);
+                        packet.version += sub.version;
+                        packet.values.extend(sub.values);
                     }
                 }
                 1 => {
                     let packet_count = transmission.get_number_from_bits(11);
                     for _ in 0..packet_count {
-                        version += get_version(transmission);
+                        let sub = get_packet(transmission);
+                        packet.version += sub.version;
+                        packet.values.extend(sub.values);
                     }
                 }
                 _ => panic!(),
@@ -81,13 +113,23 @@ fn get_version(transmission: &mut Transmission) -> u64 {
         }
     }
 
-    version
+    packet.values = vec![packet.get_value()];
+
+    packet
 }
 
-pub fn get_part_one(input: &str) -> usize {
+pub fn get_part_one(input: &str) -> u64 {
     let mut transmission = Transmission::from_str(input).unwrap();
 
-    get_version(&mut transmission) as usize
+    get_packet(&mut transmission).version
+}
+
+pub fn get_part_two(input: &str) -> u64 {
+    let mut transmission = Transmission::from_str(input).unwrap();
+
+    let packet = get_packet(&mut transmission);
+
+    packet.values[0]
 }
 
 #[cfg(test)]
@@ -105,7 +147,23 @@ mod tests {
     }, version_sum = {
         6, 16, 12, 23, 31,
     })]
-    fn test_part_one(transmission: &str, version_sum: usize) {
+    fn test_part_one(transmission: &str, version_sum: u64) {
         assert_eq!(version_sum, get_part_one(transmission));
+    }
+
+    #[parameterized(transmission = {
+        "C200B40A82",
+        "04005AC33890",
+        "880086C3E88112",
+        "CE00C43D881120",
+        "D8005AC2A8F0",
+        "F600BC2D8F",
+        "9C005AC2F8F0",
+        "9C0141080250320F1802104A08",
+    }, value = {
+        3, 54, 7, 9, 1, 0, 0, 1,
+    })]
+    fn test_part_two(transmission: &str, value: u64) {
+        assert_eq!(value, get_part_two(transmission));
     }
 }

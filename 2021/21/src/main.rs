0d8dcc8c68e003758b86638b52345237dca6a404
PsypherPunk <psypherpunk@gmail.com>
2021-12-21 18:37:46 +0000

    feat: ⭐ add 2021, day 21, part 1

---
+++ 2021/21/src/main.rs
@@ -0,0 +1,14 @@
+use std::fs;
+
+use ::day21::*;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+
+    println!(
+        "…what do you get if you multiply the score of the losing player by the number of times the die was rolled during the game? {}",
+        get_part_one(&input),
+    );
+
+    println!("{}", get_part_two(&input),);
+}

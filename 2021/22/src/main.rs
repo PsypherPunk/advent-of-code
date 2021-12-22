d61267882c78e1865d80ffd2bd0bbc49f03b0011
PsypherPunk <psypherpunk@gmail.com>
2021-12-22 19:56:50 +0000

    feat: 🌟 add 2021, day 22, part 2

--- 2021/22/src/main.rs
+++ 2021/22/src/main.rs
@@ -5,7 +5,7 @@ use ::day22::*;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
-    println!("…how many cubes are on? {}", get_part_one(&input),);
+    println!("…how many cubes are on? {}", get_part_one(&input));
 
-    println!("{}", get_part_two(&input),);
+    println!("…how many cubes are on? {}", get_part_two(&input));
 }

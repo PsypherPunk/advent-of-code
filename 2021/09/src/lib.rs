46b4090d5a0c50da934402eb44ee2e401f802b4f
PsypherPunk <psypherpunk@gmail.com>
2022-08-27 23:56:52 +0100

    fix: update 2021 day 9 to use Result

--- 2021/09/src/lib.rs
+++ 2021/09/src/lib.rs
@@ -9,47 +9,50 @@ fn get_neighbours(x: usize, y: usize) -> Vec<(usize, usize)> {
     ]
 }
 
-fn get_lava_tubes(input: &str) -> Vec<Vec<usize>> {
+fn get_lava_tubes(input: &str) -> Result<Vec<Vec<usize>>, String> {
     input
         .trim()
         .lines()
         .map(|line| {
             line.chars()
-                .map(|c| c.to_digit(10).unwrap() as usize)
-                .collect::<Vec<_>>()
+                .map(|c| c.to_digit(10).map(|digit| digit as usize))
+                .collect::<Option<Vec<_>>>()
         })
-        .collect()
+        .collect::<Option<Vec<_>>>()
+        .ok_or_else(|| "invalid digit".to_owned())
 }
 
 fn get_low_points(lava_tubes: &[Vec<usize>]) -> Vec<(usize, usize)> {
     let width = lava_tubes[0].len();
     let height = lava_tubes.len();
 
-    let mut low_points = Vec::new();
-    for y in 0..height {
-        for x in 0..width {
-            if get_neighbours(x, y)
+    (0..height)
+        .flat_map(|y| {
+            (0..width)
+                .map(move |x| {
+                    (x, y)
+                })
+        })
+        .filter(|&(x, y)| {
+            get_neighbours(x, y)
                 .iter()
                 .filter(|(nx, ny)| (*nx, *ny) != (x, y) && *nx < width && *ny < height)
                 .all(|&(nx, ny)| lava_tubes[ny][nx] > lava_tubes[y][x])
-            {
-                low_points.push((x, y));
-            }
-        }
-    }
-
-    low_points
+        })
+        .collect()
 }
 
-pub fn get_part_one(input: &str) -> usize {
-    let lava_tubes = get_lava_tubes(input);
+pub fn get_part_one(input: &str) -> Result<usize, String> {
+    let lava_tubes = get_lava_tubes(input)?;
     let low_points = get_low_points(&lava_tubes);
 
-    low_points.iter().map(|&(x, y)| lava_tubes[y][x] + 1).sum()
+    let sum = low_points.iter().map(|&(x, y)| lava_tubes[y][x] + 1).sum();
+
+    Ok(sum)
 }
 
-pub fn get_part_two(input: &str) -> usize {
-    let lava_tubes = get_lava_tubes(input);
+pub fn get_part_two(input: &str) -> Result<usize, String> {
+    let lava_tubes = get_lava_tubes(input)?;
     let low_points = get_low_points(&lava_tubes);
     let width = lava_tubes[0].len();
     let height = lava_tubes.len();
@@ -85,7 +88,9 @@ pub fn get_part_two(input: &str) -> usize {
     basin_sizes.sort_unstable();
     basin_sizes.reverse();
 
-    basin_sizes.iter().take(3).product()
+    let product = basin_sizes.iter().take(3).product();
+
+    Ok(product)
 }
 
 #[cfg(test)]
@@ -101,11 +106,11 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        assert_eq!(15, get_part_one(INPUT));
+        assert_eq!(15, get_part_one(INPUT).unwrap());
     }
 
     #[test]
     fn test_part_two() {
-        assert_eq!(1134, get_part_two(INPUT));
+        assert_eq!(1134, get_part_two(INPUT).unwrap());
     }
 }
--- 2021/09/src/main.rs
+++ 2021/09/src/main.rs
@@ -2,16 +2,18 @@ use std::fs;
 
 use ::day09::*;
 
-fn main() {
+fn main() -> Result<(), String> {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!(
         "What is the sum of the risk levels of all low points on your heightmap? {}",
-        get_part_one(&input),
+        get_part_one(&input)?,
     );
 
     println!(
         "What do you get if you multiply together the sizes of the three largest basins? {}",
-        get_part_two(&input),
+        get_part_two(&input)?,
     );
+
+    Ok(())
 }

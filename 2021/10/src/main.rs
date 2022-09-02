cfe7bc67c5948b9fe3d7719c68bac54381b1ad9c
PsypherPunk <psypherpunk@gmail.com>
2022-09-02 22:28:57 +0100

    fix: update 2021, day 10 to use Result

--- 2021/10/src/main.rs
+++ 2021/10/src/main.rs
@@ -2,13 +2,15 @@ use std::fs;
 
 use ::day10::*;
 
-fn main() {
+fn main() -> Result<(), String> {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!(
         "What is the total syntax error score for those errors? {}",
-        get_part_one(&input),
+        get_part_one(&input)?,
     );
 
-    println!("What is the middle score? {}", get_part_two(&input),);
+    println!("What is the middle score? {}", get_part_two(&input)?);
+
+    Ok(())
 }

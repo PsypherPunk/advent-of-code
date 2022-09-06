f465fd39d386198a9eeae3ff73ac5758c6c4c9b7
PsypherPunk <psypherpunk@gmail.com>
2022-09-06 16:13:37 +0100

    fix: update 2021, day 12 to use Result

--- 2021/12/src/main.rs
+++ 2021/12/src/main.rs
@@ -2,16 +2,18 @@ use std::fs;
 
 use ::day12::*;
 
-fn main() {
+fn main() -> Result<(), String> {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!(
         "How many paths through this cave system are there that visit small caves at most once? {}",
-        get_part_one(&input),
+        get_part_one(&input)?,
     );
 
     println!(
         "…how many paths through this cave system are there? {}",
-        get_part_two(&input),
+        get_part_two(&input)?,
     );
+
+    Ok(())
 }

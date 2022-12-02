33c61e428bcef05d83669609adfaccf7442dc204
Roger G. Coram <roger@thedatashed.co.uk>
2022-12-02 12:13:49 +0000

    refactor: move to fallible outcome
    …and _really_ over-work it.

--- 2022/02/src/main.rs
+++ 2022/02/src/main.rs
@@ -2,16 +2,18 @@ use std::fs;
 
 use ::day02::*;
 
-fn main() {
+fn main() -> Result<(), String> {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!(
         "What would your total score be if everything goes exactly according to your strategy guide? {}",
-        get_part_one(&input),
+        get_part_one(&input)?,
     );
 
     println!(
         "…what would your total score be if everything goes exactly according to your strategy guide? {}",
-        get_part_two(&input),
+        get_part_two(&input)?,
     );
+
+    Ok(())
 }

3d857d0b8b2ce1cce708da5754d89ede9668fe80
Roger G. Coram <roger@thedatashed.co.uk>
2022-12-02 12:13:49 +0000

    refactor: move to fallible outcome
    …and _really_ over-work it.

--- 2022/02/src/lib.rs
+++ 2022/02/src/lib.rs
@@ -1,7 +1,102 @@
+use std::cmp::Ordering;
+use std::str::FromStr;
+
+struct Round {
+    you: Shape,
+    opponent: Shape,
+}
+
+impl FromStr for Round {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (opponent, you) = s
+            .split_once(' ')
+            .ok_or_else(|| format!("invalid line: {}", s))?;
+
+        Ok(Self {
+            you: Shape::from_str(you)?,
+            opponent: Shape::from_str(opponent)?,
+        })
+    }
+}
+
+impl Round {
+    fn get_score(self) -> usize {
+        let outcome = match &self.you.partial_cmp(&self.opponent) {
+            Some(Ordering::Equal) => Outcome::Draw as usize,
+            Some(Ordering::Less) => Outcome::Lose as usize,
+            Some(Ordering::Greater) => Outcome::Win as usize,
+            None => unreachable!(),
+        };
+
+        outcome + self.you as usize
+    }
+}
+
+#[derive(PartialEq)]
 enum Shape {
     Rock = 1,
     Paper = 2,
     Scissors = 3,
+}
+
+impl FromStr for Shape {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" | "X" => Ok(Shape::Rock),
+            "B" | "Y" => Ok(Shape::Paper),
+            "C" | "Z" => Ok(Shape::Scissors),
+            _ => Err(format!("invalid shape: {}", s)),
+        }
+    }
+}
+
+impl PartialOrd for Shape {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match self {
+            Shape::Rock => match other {
+                Shape::Rock => Some(Ordering::Equal),
+                Shape::Paper => Some(Ordering::Less),
+                Shape::Scissors => Some(Ordering::Greater),
+            },
+            Shape::Paper => match other {
+                Shape::Rock => Some(Ordering::Greater),
+                Shape::Paper => Some(Ordering::Equal),
+                Shape::Scissors => Some(Ordering::Less),
+            },
+            Shape::Scissors => match other {
+                Shape::Rock => Some(Ordering::Less),
+                Shape::Paper => Some(Ordering::Greater),
+                Shape::Scissors => Some(Ordering::Equal),
+            },
+        }
+    }
+}
+
+impl Shape {
+    #[allow(clippy::wrong_self_convention)]
+    fn from_outcome(&self, outcome: &Outcome) -> Self {
+        match outcome {
+            Outcome::Win => match self {
+                Shape::Rock => Shape::Paper,
+                Shape::Paper => Shape::Scissors,
+                Shape::Scissors => Shape::Rock,
+            },
+            Outcome::Lose => match self {
+                Shape::Rock => Shape::Scissors,
+                Shape::Paper => Shape::Rock,
+                Shape::Scissors => Shape::Paper,
+            },
+            Outcome::Draw => match self {
+                Shape::Rock => Shape::Rock,
+                Shape::Paper => Shape::Paper,
+                Shape::Scissors => Shape::Scissors,
+            },
+        }
+    }
 }
 
 enum Outcome {
@@ -10,84 +105,58 @@ enum Outcome {
     Draw = 3,
 }
 
-pub fn get_part_one(input: &str) -> usize {
-    input
-        .trim()
-        .lines()
-        .map(|line| {
-            let (opponent, you) = line.split_once(' ').unwrap();
-            let opponent = match opponent {
-                "A" => Shape::Rock,
-                "B" => Shape::Paper,
-                "C" => Shape::Scissors,
-                _ => unreachable!(),
-            };
-            let you = match you {
-                "X" => Shape::Rock,
-                "Y" => Shape::Paper,
-                "Z" => Shape::Scissors,
-                _ => unreachable!(),
-            };
+impl FromStr for Outcome {
+    type Err = String;
 
-            match opponent {
-                Shape::Rock => match you {
-                    Shape::Rock => 3 + 1,
-                    Shape::Paper => 6 + 2,
-                    Shape::Scissors => 0 + 3,
-                },
-                Shape::Paper => match you {
-                    Shape::Rock => 0 + 1,
-                    Shape::Paper => 3 + 2,
-                    Shape::Scissors => 6 + 3,
-                },
-                Shape::Scissors => match you {
-                    Shape::Rock => 6 + 1,
-                    Shape::Paper => 0 + 2,
-                    Shape::Scissors => 3 + 3,
-                },
-            }
-        })
-        .sum()
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "X" => Ok(Outcome::Lose),
+            "Y" => Ok(Outcome::Draw),
+            "Z" => Ok(Outcome::Win),
+            _ => Err(format!("invalid outcome: {}", s)),
+        }
+    }
 }
 
-pub fn get_part_two(input: &str) -> usize {
-    input
+pub fn get_part_one(input: &str) -> Result<usize, String> {
+    let score = input
         .trim()
         .lines()
         .map(|line| {
-            let (opponent, you) = line.split_once(' ').unwrap();
-            let opponent = match opponent {
-                "A" => Shape::Rock,
-                "B" => Shape::Paper,
-                "C" => Shape::Scissors,
-                _ => unreachable!(),
-            };
-            let you = match you {
-                "X" => Outcome::Lose,
-                "Y" => Outcome::Draw,
-                "Z" => Outcome::Win,
-                _ => unreachable!(),
+            let round = Round::from_str(line)?;
+
+            Ok::<usize, String>(round.get_score())
+        })
+        .collect::<Result<Vec<usize>, _>>()?
+        .iter()
+        .sum();
+
+    Ok(score)
+}
+
+pub fn get_part_two(input: &str) -> Result<usize, String> {
+    let score = input
+        .trim()
+        .lines()
+        .map(|line| {
+            let (opponent, outcome) = line
+                .split_once(' ')
+                .ok_or_else(|| format!("invalid line: {}", line))?;
+
+            let (opponent, outcome) = (Shape::from_str(opponent)?, Outcome::from_str(outcome)?);
+
+            let round = Round {
+                you: opponent.from_outcome(&outcome),
+                opponent,
             };
 
-            match opponent {
-                Shape::Rock => match you {
-                    Outcome::Win => 6 + 2,
-                    Outcome::Lose => 0 + 3,
-                    Outcome::Draw => 3 + 1,
-                },
-                Shape::Paper => match you {
-                    Outcome::Win => 6 + 3,
-                    Outcome::Lose => 0 + 1,
-                    Outcome::Draw => 3 + 2,
-                },
-                Shape::Scissors => match you {
-                    Outcome::Win => 6 + 1,
-                    Outcome::Lose => 0 + 2,
-                    Outcome::Draw => 3 + 3,
-                },
-            }
+            Ok::<usize, String>(round.get_score())
         })
-        .sum()
+        .collect::<Result<Vec<usize>, _>>()?
+        .iter()
+        .sum();
+
+    Ok(score)
 }
 
 #[cfg(test)]
@@ -101,11 +170,11 @@ C Z
 
     #[test]
     fn test_part_one() {
-        assert_eq!(15, get_part_one(INPUT));
+        assert_eq!(Ok(15), get_part_one(INPUT));
     }
 
     #[test]
     fn test_part_two() {
-        assert_eq!(12, get_part_two(INPUT));
+        assert_eq!(Ok(12), get_part_two(INPUT));
     }
 }

5dd05d101f38481e0ddde92a60530b663f9cd318
PsypherPunk <psypherpunk@gmail.com>
2022-12-03 06:36:11 +0000

    feat: 🌟 add 2022, day 3

---
+++ 2022/03/src/main.rs
@@ -0,0 +1,17 @@
+use std::fs;
+
+use ::day03::*;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+
+    println!(
+        "What is the sum of the priorities of those item types? {}",
+        get_part_one(&input),
+    );
+
+    println!(
+        "What is the sum of the priorities of those item types? {}",
+        get_part_two(&input),
+    );
+}

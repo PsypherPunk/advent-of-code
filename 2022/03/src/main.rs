02bd4a99acd1ad3309ac314b8e55cfb239b16f6f
PsypherPunk <psypherpunk@gmail.com>
2022-12-03 09:23:15 +0000

    refactor: move to fallible outcome for day 3

--- 2022/03/src/main.rs
+++ 2022/03/src/main.rs
@@ -2,16 +2,18 @@ use std::fs;
 
 use ::day03::*;
 
-fn main() {
+fn main() -> Result<(), String> {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!(
         "What is the sum of the priorities of those item types? {}",
-        get_part_one(&input),
+        get_part_one(&input)?,
     );
 
     println!(
         "What is the sum of the priorities of those item types? {}",
-        get_part_two(&input),
+        get_part_two(&input)?,
     );
+
+    Ok(())
 }

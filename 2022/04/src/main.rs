aae01ff79834c8459155290abb6eba8e7ba213d4
PsypherPunk <psypherpunk@gmail.com>
2022-12-04 09:33:59 +0000

    refactor: move to fallible outcome for day 4

--- 2022/04/src/main.rs
+++ 2022/04/src/main.rs
@@ -2,16 +2,18 @@ use std::fs;
 
 use ::day04::*;
 
-fn main() {
+fn main() -> Result<(), String> {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!(
         "In how many assignment pairs does one range fully contain the other? {}",
-        get_part_one(&input),
+        get_part_one(&input)?,
     );
 
     println!(
         "In how many assignment pairs do the ranges overlap? {}",
-        get_part_two(&input),
+        get_part_two(&input)?,
     );
+
+    Ok(())
 }

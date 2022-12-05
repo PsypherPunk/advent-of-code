148d303f659128bed48edc326dd4a2c0a98281ce
Roger G. Coram <roger@thedatashed.co.uk>
2022-12-05 15:51:58 +0000

    refactor: move to fallible outcome for day 5

--- 2022/05/src/main.rs
+++ 2022/05/src/main.rs
@@ -2,16 +2,18 @@ use std::fs;
 
 use ::day05::*;
 
-fn main() {
+fn main() -> Result<(), AdventOfCodeError> {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!(
         "After the rearrangement procedure completes, what crate ends up on top of each stack? {}",
-        get_part_one(&input),
+        get_part_one(&input)?,
     );
 
     println!(
         "After the rearrangement procedure completes, what crate ends up on top of each stack? {}",
-        get_part_two(&input),
+        get_part_two(&input)?,
     );
+
+    Ok(())
 }

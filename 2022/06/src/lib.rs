a581a74527ab3c1eb0635fe70f120cc786865f79
PsypherPunk <psypherpunk@gmail.com>
2022-12-06 07:59:09 +0000

    refactor: fallible processing for 2022, day 6

--- 2022/06/src/lib.rs
+++ 2022/06/src/lib.rs
@@ -1,27 +1,28 @@
 use std::collections::HashSet;
 
-pub fn get_part_one(input: &str) -> usize {
-    let stream = input.trim().chars().collect::<Vec<_>>();
-
-    let (offset, _) = stream
-        .windows(4)
-        .enumerate()
-        .find(|(_, marker)| marker.iter().collect::<HashSet<_>>().len() == 4)
-        .unwrap();
-
-    offset + 4
+#[derive(Debug, PartialEq, Eq)]
+pub enum AdventOfCodeError {
+    InvalidStreamError(String),
 }
 
-pub fn get_part_two(input: &str) -> usize {
+fn find_marker(input: &str, length: usize) -> Result<usize, AdventOfCodeError> {
     let stream = input.trim().chars().collect::<Vec<_>>();
 
     let (offset, _) = stream
-        .windows(14)
+        .windows(length)
         .enumerate()
-        .find(|(_, marker)| marker.iter().collect::<HashSet<_>>().len() == 14)
-        .unwrap();
+        .find(|(_, marker)| marker.iter().collect::<HashSet<_>>().len() == length)
+        .ok_or_else(|| AdventOfCodeError::InvalidStreamError(input.to_owned()))?;
 
-    offset + 14
+    Ok(offset + length)
+}
+
+pub fn get_part_one(input: &str) -> Result<usize, AdventOfCodeError> {
+    find_marker(input, 4)
+}
+
+pub fn get_part_two(input: &str) -> Result<usize, AdventOfCodeError> {
+    find_marker(input, 14)
 }
 
 #[cfg(test)]
@@ -40,7 +41,7 @@ mod tests {
         7, 5, 6, 10, 11
     })]
     fn test_part_one(stream: &str, offset: usize) {
-        assert_eq!(offset, get_part_one(stream));
+        assert_eq!(Ok(offset), get_part_one(stream));
     }
 
     #[parameterized(stream = {
@@ -53,6 +54,6 @@ mod tests {
         19, 23, 23, 29, 26
     })]
     fn test_part_two(stream: &str, offset: usize) {
-        assert_eq!(offset, get_part_two(stream));
+        assert_eq!(Ok(offset), get_part_two(stream));
     }
 }

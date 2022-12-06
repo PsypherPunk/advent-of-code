dc71ba38057464127d4be0d93d51ef9645f4af7d
PsypherPunk <psypherpunk@gmail.com>
2022-12-06 07:59:09 +0000

    refactor: fallible processing for 2022, day 6

--- 2022/06/src/main.rs
+++ 2022/06/src/main.rs
@@ -2,16 +2,18 @@ use std::fs;
 
 use ::day06::*;
 
-fn main() {
+fn main() -> Result<(), AdventOfCodeError> {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!(
         "How many characters need to be processed before the first start-of-packet marker is detected? {}",
-        get_part_one(&input),
+        get_part_one(&input)?,
     );
 
     println!(
         "How many characters need to be processed before the first start-of-message marker is detected? {}",
-        get_part_two(&input),
+        get_part_two(&input)?,
     );
+
+    Ok(())
 }

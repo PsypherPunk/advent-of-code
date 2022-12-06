a105d1c6cde698c959a2fbc288b6357585c27e13
PsypherPunk <psypherpunk@gmail.com>
2022-12-06 07:47:13 +0000

    feat: 🌟 add 2022, day 6, part 2

--- 2022/06/src/lib.rs
+++ 2022/06/src/lib.rs
@@ -1,25 +1,27 @@
 use std::collections::HashSet;
 
 pub fn get_part_one(input: &str) -> usize {
-    let stream = input
-        .trim()
-        .chars()
-        .collect::<Vec<_>>();
+    let stream = input.trim().chars().collect::<Vec<_>>();
 
     let (offset, _) = stream
         .windows(4)
         .enumerate()
-        .find(|(_, marker)| {
-            marker.iter().collect::<HashSet<_>>().len() == 4
-        })
+        .find(|(_, marker)| marker.iter().collect::<HashSet<_>>().len() == 4)
         .unwrap();
 
     offset + 4
 }
 
 pub fn get_part_two(input: &str) -> usize {
+    let stream = input.trim().chars().collect::<Vec<_>>();
 
-    0
+    let (offset, _) = stream
+        .windows(14)
+        .enumerate()
+        .find(|(_, marker)| marker.iter().collect::<HashSet<_>>().len() == 14)
+        .unwrap();
+
+    offset + 14
 }
 
 #[cfg(test)]
@@ -28,21 +30,29 @@ mod tests {
 
     use parameterized::parameterized;
 
-    #[parameterized(stream = {                                        
+    #[parameterized(stream = {
         "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
         "bvwbjplbgvbhsrlpgdmjqwftvncz",
         "nppdvjthqldpwncqszvftbrmjlhg",
         "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
         "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
-    }, offset = {                                                            
+    }, offset = {
         7, 5, 6, 10, 11
     })]
     fn test_part_one(stream: &str, offset: usize) {
         assert_eq!(offset, get_part_one(stream));
     }
 
-    #[test]
-    fn test_part_two() {
-        assert_eq!(2, get_part_two(""));
+    #[parameterized(stream = {
+        "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+        "bvwbjplbgvbhsrlpgdmjqwftvncz",
+        "nppdvjthqldpwncqszvftbrmjlhg",
+        "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+        "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+    }, offset = {
+        19, 23, 23, 29, 26
+    })]
+    fn test_part_two(stream: &str, offset: usize) {
+        assert_eq!(offset, get_part_two(stream));
     }
 }

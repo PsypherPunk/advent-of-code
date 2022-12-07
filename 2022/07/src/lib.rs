56122c7be7d62abee79ff448bd311e591e187dc4
PsypherPunk <psypherpunk@gmail.com>
2022-12-07 18:11:34 +0000

    refactor: simplify 2022, day 7
    reduce the `filter_map()`/`fold()` combination to a single `try_fold()`.

--- 2022/07/src/lib.rs
+++ 2022/07/src/lib.rs
@@ -1,66 +1,93 @@
-use std::{collections::HashMap, path::PathBuf};
+#![deny(clippy::expect_used, clippy::unwrap_used)]
 
-fn get_sizes(input: &str) -> HashMap<PathBuf, usize> {
+use std::collections::HashMap;
+use std::num::ParseIntError;
+use std::path::PathBuf;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum AdventOfCodeError {
+    InvalidFileSystemError,
+    ParseIntError(ParseIntError),
+}
+
+fn get_sizes(input: &str) -> Result<HashMap<PathBuf, usize>, AdventOfCodeError> {
     let mut cwd = PathBuf::new();
 
     input
         .trim()
         .lines()
-        .filter_map(|line| {
-            let parts = line.split(' ').collect::<Vec<_>>();
-            match parts[0] {
-                "$" => match parts[1] {
-                    "cd" => match parts[2] {
-                        ".." => {
+        .try_fold(HashMap::new(), |mut acc, line| {
+            let mut parts = line.split(' ');
+
+            let (first, second, third) = (parts.next(), parts.next(), parts.next());
+
+            match first {
+                Some("$") => match second {
+                    Some("cd") => match third {
+                        Some("..") => {
                             cwd.pop();
-                            None
+                            Ok(acc)
                         }
-                        _ => {
-                            cwd.push(parts[2]);
-                            None
+                        Some(path) => {
+                            cwd.push(path);
+                            Ok(acc)
                         }
+                        _ => Ok(acc),
                     },
-                    _ => None,
+                    _ => Ok(acc),
                 },
-                "dir" => None,
-                _ => {
-                    cwd.push(parts[1]);
-                    let filesize = (cwd.clone(), parts[0].parse::<usize>().unwrap());
-                    cwd.pop();
+                Some("dir") => Ok(acc),
+                Some(size) => match second {
+                    Some(path) => {
+                        cwd.push(path);
 
-                    Some(filesize)
-                }
-            }
-        })
-        .fold(HashMap::new(), |mut acc, (mut path, size)| {
-            while path.pop() {
-                let entry = acc.entry(path.clone()).or_insert(0);
-                *entry += size;
-            }
+                        match size.parse::<usize>() {
+                            Ok(size) => {
+                                cwd.ancestors().skip(1).for_each(|ancestor| {
+                                    let entry = acc.entry(ancestor.to_path_buf()).or_insert(0);
+                                    *entry += size;
+                                });
 
-            acc
+                                cwd.pop();
+
+                                Ok(acc)
+                            }
+                            Err(e) => Err(AdventOfCodeError::ParseIntError(e)),
+                        }
+                    }
+                    None => Ok(acc),
+                },
+                None => Ok(acc),
+            }
         })
 }
 
-pub fn get_part_one(input: &str) -> usize {
-    let sizes = get_sizes(input);
+pub fn get_part_one(input: &str) -> Result<usize, AdventOfCodeError> {
+    let sizes = get_sizes(input)?;
 
-    sizes.values().filter(|&size| *size <= 100_000).sum()
+    let sum = sizes.values().filter(|&size| *size <= 100_000).sum();
+
+    Ok(sum)
 }
 
-pub fn get_part_two(input: &str) -> usize {
+pub fn get_part_two(input: &str) -> Result<usize, AdventOfCodeError> {
     let total_disk_space = 70_000_000;
     let required_disk_space = 30_000_000;
 
-    let sizes = get_sizes(input);
+    let sizes = get_sizes(input)?;
 
-    let unused_disk_space = total_disk_space - sizes.get(&PathBuf::from("/")).unwrap();
+    let unused_disk_space = total_disk_space
+        - sizes
+            .get(&PathBuf::from("/"))
+            .ok_or(AdventOfCodeError::InvalidFileSystemError)?;
 
-    *sizes
+    let delete = *sizes
         .values()
         .filter(|&size| unused_disk_space + *size >= required_disk_space)
         .min()
-        .unwrap()
+        .ok_or(AdventOfCodeError::InvalidFileSystemError)?;
+
+    Ok(delete)
 }
 
 #[cfg(test)]
@@ -94,11 +121,11 @@ $ ls
 
     #[test]
     fn test_part_one() {
-        assert_eq!(95_437, get_part_one(INPUT));
+        assert_eq!(Ok(95_437), get_part_one(INPUT));
     }
 
     #[test]
     fn test_part_two() {
-        assert_eq!(24_933_642, get_part_two(INPUT));
+        assert_eq!(Ok(24_933_642), get_part_two(INPUT));
     }
 }
--- 2022/07/src/main.rs
+++ 2022/07/src/main.rs
@@ -2,16 +2,18 @@ use std::fs;
 
 use ::day07::*;
 
-fn main() {
+fn main() -> Result<(), AdventOfCodeError> {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!(
         "What is the sum of the total sizes of those directories? {}",
-        get_part_one(&input),
+        get_part_one(&input)?,
     );
 
     println!(
         "What is the total size of that directory? {}",
-        get_part_two(&input),
+        get_part_two(&input)?,
     );
+
+    Ok(())
 }

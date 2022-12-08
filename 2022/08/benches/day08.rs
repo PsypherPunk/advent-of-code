3d05d2199cc51d4dab00c26ff144aeee247915c0
Roger G. Coram <roger@thedatashed.co.uk>
2022-12-08 18:31:15 +0000

    refactor: 🏇 benchmarks and performance tweaks
    allow early exiting of the checks for Part One.

---
+++ 2022/08/benches/day08.rs
@@ -0,0 +1,24 @@
+use std::{fs, str::FromStr};
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use day08::*;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+
+    c.bench_function("parse input", |b| {
+        b.iter(|| TallTrees::from_str(black_box(&input)).unwrap())
+    });
+
+    c.bench_function("part_one input", |b| {
+        b.iter(|| get_part_one(black_box(&input)))
+    });
+
+    c.bench_function("part_two input", |b| {
+        b.iter(|| get_part_two(black_box(&input)))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);

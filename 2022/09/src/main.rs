41db9cf1cd064ef6090b25839f90632a45b81731
PsypherPunk <psypherpunk@gmail.com>
2022-12-09 13:46:14 +0000

    feat: ⭐ add 2022, day 9, part 1

---
+++ 2022/09/src/main.rs
@@ -0,0 +1,14 @@
+use std::fs;
+
+use ::day09::*;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+
+    println!(
+        "How many positions does the tail of the rope visit at least once? {}",
+        get_part_one(&input),
+    );
+
+    println!("{}", get_part_two(&input),);
+}

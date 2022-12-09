41bf98a4185bf047e000c57ff1ac2d49355c0553
PsypherPunk <psypherpunk@gmail.com>
2022-12-09 20:51:49 +0000

    refactor: tidy up 2022, day 9

--- 2022/09/src/main.rs
+++ 2022/09/src/main.rs
@@ -2,16 +2,18 @@ use std::fs;
 
 use ::day09::*;
 
-fn main() {
+fn main() -> Result<(), AdventOfCodeError> {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!(
         "How many positions does the tail of the rope visit at least once? {}",
-        get_part_one(&input),
+        get_part_one(&input)?,
     );
 
     println!(
         "How many positions does the tail of the rope visit at least once? {}",
-        get_part_two(&input),
+        get_part_two(&input)?,
     );
+
+    Ok(())
 }

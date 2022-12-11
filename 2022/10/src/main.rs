c635d6ac4cdf6de1226a6a3a25dda40d8a077c60
PsypherPunk <psypherpunk@gmail.com>
2022-12-11 12:17:35 +0000

    feat: 🌟 add 2022, day 10, part 2

--- 2022/10/src/main.rs
+++ 2022/10/src/main.rs
@@ -2,13 +2,18 @@ use std::fs;
 
 use ::day10::*;
 
-fn main() {
+fn main() -> Result<(), AdventOfCodeError> {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!(
         "What is the sum of these six signal strengths? {}",
-        get_part_one(&input),
+        get_part_one(&input)?,
     );
 
-    println!("{}", get_part_two(&input),);
+    println!(
+        "What eight capital letters appear on your CRT?\n\n{}",
+        get_part_two(&input)?,
+    );
+
+    Ok(())
 }

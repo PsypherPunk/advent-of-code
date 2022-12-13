fe1d2c177877be06f7ca7a7c6a89b5a99ef268ea
PsypherPunk <psypherpunk@gmail.com>
2022-12-13 18:22:01 +0000

    refactor: fallible parsing for 2022, day 13

--- 2022/13/src/main.rs
+++ 2022/13/src/main.rs
@@ -2,16 +2,18 @@ use std::fs;
 
 use ::day13::*;
 
-fn main() {
+fn main() -> Result<(), AdventOfCodeError> {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!(
         "What is the sum of the indices of those pairs? {}",
-        get_part_one(&input),
+        get_part_one(&input)?,
     );
 
     println!(
         "What is the decoder key for the distress signal? {}",
-        get_part_two(&input),
+        get_part_two(&input)?,
     );
+
+    Ok(())
 }

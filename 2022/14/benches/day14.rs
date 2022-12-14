d68e27cde5d37fb27f4f82909319ed66f93070f6
PsypherPunk <psypherpunk@gmail.com>
2022-12-14 13:24:29 +0000

    feat: 🌟 add 2022, day 14, part 2

--- 2022/14/benches/day14.rs
+++ 2022/14/benches/day14.rs
@@ -7,6 +7,8 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 fn criterion_benchmark(c: &mut Criterion) {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
+    c.bench_function("get_scan input", |b| b.iter(|| get_scan(black_box(&input))));
+
     c.bench_function("part_one input", |b| {
         b.iter(|| get_part_one(black_box(&input)))
     });

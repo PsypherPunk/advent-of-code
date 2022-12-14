90eef10b790d8d57e2a9a0e42e1b7a6a860e7e38
PsypherPunk <psypherpunk@gmail.com>
2022-12-14 14:32:45 +0000

    refactor: impl FromStr for Scan

--- 2022/14/src/main.rs
+++ 2022/14/src/main.rs
@@ -2,16 +2,18 @@ use std::fs;
 
 use ::day14::*;
 
-fn main() {
+fn main() -> Result<(), AdventOfCodeError> {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
     println!(
         "How many units of sand come to rest before sand starts flowing into the abyss below? {}",
-        get_part_one(&input),
+        get_part_one(&input)?,
     );
 
     println!(
         "How many units of sand come to rest? {}",
-        get_part_two(&input),
+        get_part_two(&input)?,
     );
+
+    Ok(())
 }

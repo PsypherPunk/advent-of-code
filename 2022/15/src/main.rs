cd9f97a7b6e098542e74f548259eeb645b066a08
PsypherPunk <psypherpunk@gmail.com>
2022-12-15 19:19:31 +0000

    feat: 🌟 add 2022, day 15, part 2

--- 2022/15/src/main.rs
+++ 2022/15/src/main.rs
@@ -10,8 +10,5 @@ fn main() {
         get_part_one(&input, 2_000_000),
     );
 
-    println!(
-        "{}",
-        get_part_two(&input),
-    );
+    println!("What is its tuning frequency? {}", get_part_two(&input),);
 }

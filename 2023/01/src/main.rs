d0af89560c4c4760fd69c6dcc4805dcd055a15c3
PsypherPunk <psypherpunk@gmail.com>
2023-12-01 13:06:18 +0000

    feat: ⭐ add 2023, day 1, part 1

---
+++ 2023/01/src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+use ::day01::*;
+
+fn main() -> Result<(), String> {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+
+    println!(
+        "What is the sum of all of the calibration values? {}",
+        get_part_one(&input)?,
+    );
+
+    println!("{}", get_part_two(&input)?,);
+
+    Ok(())
+}

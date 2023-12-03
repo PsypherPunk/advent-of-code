fd256a7cc2fac4cfb1ddd8cad4543c9a150d1cec
PsypherPunk <psypherpunk@gmail.com>
2023-12-03 11:49:44 +0000

    feat: ⭐ add 2023, day 3, part 1

---
+++ 2023/03/src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+use ::day03::*;
+
+fn main() -> Result<(), String> {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+
+    println!(
+        "What is the sum of all of the part numbers in the engine schematic? {}",
+        get_part_one(&input)?,
+    );
+
+    println!("{}", get_part_two(&input)?,);
+
+    Ok(())
+}

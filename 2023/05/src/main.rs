0786ca92cc481e1261b9c2e28fd6899125c331be
PsypherPunk <psypherpunk@gmail.com>
2023-12-05 20:56:10 +0000

    feat: 🌟 add 2023, day 5, part 2

--- 2023/05/src/main.rs
+++ 2023/05/src/main.rs
@@ -10,7 +10,10 @@ fn main() -> Result<(), String> {
         get_part_one(&input)?,
     );
 
-    println!("{}", get_part_two(&input)?,);
+    println!(
+        "What is the lowest location number that corresponds to any of the initial seed numbers? {}",
+        get_part_two(&input)?,
+    );
 
     Ok(())
 }

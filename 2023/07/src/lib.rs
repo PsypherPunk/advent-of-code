864bafd907d163c7d9e1e0c9f323aaaee4a656a0
PsypherPunk <psypherpunk@gmail.com>
2023-12-07 17:39:26 +0000

    feat: 🌟 add 2023, day 7, part 2

--- 2023/07/src/lib.rs
+++ 2023/07/src/lib.rs
@@ -2,9 +2,15 @@ use std::cmp::Ordering;
 use std::collections::BTreeMap;
 use std::str::FromStr;
 
-#[allow(unused)]
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 struct Hand {
+    cards: Vec<u32>,
+    bid: usize,
+    r#type: Type,
+}
+
+#[derive(Debug, Eq, PartialEq)]
+struct JokerHand {
     cards: Vec<u32>,
     bid: usize,
     r#type: Type,
@@ -28,6 +34,60 @@ impl Ord for Type {
 }
 
 impl PartialOrd for Type {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self.r#type.cmp(&other.r#type) {
+            Ordering::Equal => {
+                match self
+                    .cards
+                    .iter()
+                    .zip(other.cards.iter())
+                    .find_map(|(a, b)| match a.cmp(b) {
+                        Ordering::Equal => None,
+                        o => Some(o),
+                    }) {
+                    Some(o) => o,
+                    _ => Ordering::Equal,
+                }
+            }
+            o => o,
+        }
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for JokerHand {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self.r#type.cmp(&other.r#type) {
+            Ordering::Equal => {
+                match self
+                    .cards
+                    .iter()
+                    .zip(other.cards.iter())
+                    .find_map(|(a, b)| match a.cmp(b) {
+                        Ordering::Equal => None,
+                        o => Some(o),
+                    }) {
+                    Some(o) => o,
+                    _ => Ordering::Equal,
+                }
+            }
+            o => o,
+        }
+    }
+}
+
+impl PartialOrd for JokerHand {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
@@ -75,7 +135,59 @@ impl FromStr for Hand {
             _ => unreachable!(),
         };
 
-        Ok(Hand { cards, bid, r#type })
+        Ok(Self { cards, bid, r#type })
+    }
+}
+
+impl FromStr for JokerHand {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (cards, bid) = s.trim().split_once(' ').ok_or(format!("bad line: {}", s))?;
+
+        let bid = bid.parse::<usize>().map_err(|e| e.to_string())?;
+
+        let cards = cards
+            .chars()
+            .filter_map(|c| match c {
+                'A' => Some(14),
+                'K' => Some(13),
+                'Q' => Some(12),
+                'J' => Some(1),
+                'T' => Some(10),
+                _ => c.to_digit(10),
+            })
+            .collect::<Vec<_>>();
+
+        let jokers = cards.iter().filter(|&card| *card == 1).count();
+        let counts =
+            cards
+                .iter()
+                .filter(|&card| *card != 1)
+                .fold(BTreeMap::new(), |mut counts, card| {
+                    *counts.entry(card).or_insert(0) += 1;
+
+                    counts
+                });
+        let mut counts = counts.into_values().collect::<Vec<_>>();
+        counts.sort_unstable();
+
+        let r#type = match counts.pop().unwrap_or(0) + jokers {
+            5 => Type::FiveOfAKind,
+            4 => Type::FourOfAKind,
+            3 => match counts.pop().ok_or("bad count".to_owned())? {
+                2 => Type::FullHouse,
+                _ => Type::ThreeOfAKind,
+            },
+            2 => match counts.pop().ok_or("bad count".to_owned())? {
+                2 => Type::TwoPair,
+                _ => Type::Pair,
+            },
+            1 => Type::HighCard,
+            _ => unreachable!(),
+        };
+
+        Ok(Self { cards, bid, r#type })
     }
 }
 
@@ -86,25 +198,7 @@ pub fn get_part_one(input: &str) -> Result<usize, String> {
         .map(Hand::from_str)
         .collect::<Result<Vec<_>, String>>()?;
 
-    hands.sort_unstable_by(|a, b| match a.r#type.cmp(&b.r#type) {
-        Ordering::Equal => {
-            match a
-                .cards
-                .iter()
-                .zip(b.cards.iter())
-                .find_map(|(a, b)| {
-                    match a.cmp(b) {
-                        Ordering::Equal => None,
-                        o => Some(o),
-                    }
-                })
-            {
-                Some(o) => o,
-                _ => Ordering::Equal,
-            }
-        }
-        o => o,
-    });
+    hands.sort_unstable();
 
     let winnings = hands
         .iter()
@@ -115,8 +209,22 @@ pub fn get_part_one(input: &str) -> Result<usize, String> {
     Ok(winnings)
 }
 
-pub fn get_part_two(_input: &str) -> Result<usize, String> {
-    Ok(0)
+pub fn get_part_two(input: &str) -> Result<usize, String> {
+    let mut hands = input
+        .trim()
+        .lines()
+        .map(JokerHand::from_str)
+        .collect::<Result<Vec<_>, String>>()?;
+
+    hands.sort_unstable();
+
+    let winnings = hands
+        .iter()
+        .enumerate()
+        .map(|(rank, hand)| (rank + 1) * hand.bid)
+        .sum();
+
+    Ok(winnings)
 }
 
 #[cfg(test)]
@@ -137,6 +245,6 @@ QQQJA 483
 
     #[test]
     fn test_part_two() {
-        assert_eq!(Ok(2), get_part_two(INPUT));
+        assert_eq!(Ok(5905), get_part_two(INPUT));
     }
 }
--- 2023/07/src/main.rs
+++ 2023/07/src/main.rs
@@ -5,15 +5,9 @@ use ::day07::*;
 fn main() -> Result<(), String> {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
-    println!(
-        "What are the total winnings? {}",
-        get_part_one(&input)?,
-    );
+    println!("What are the total winnings? {}", get_part_one(&input)?);
 
-    println!(
-        "{}",
-        get_part_two(&input)?,
-    );
+    println!("What are the new total winnings? {}", get_part_two(&input)?);
 
     Ok(())
 }

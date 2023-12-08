db46229e4be0dc595746b2fe2aef1a32ac134799
PsypherPunk <psypherpunk@gmail.com>
2023-12-08 15:27:05 +0000

    refactor: 🔨 simplify 2023, day 7

--- 2023/07/src/lib.rs
+++ 2023/07/src/lib.rs
@@ -1,16 +1,8 @@
 use std::cmp::Ordering;
 use std::collections::BTreeMap;
-use std::str::FromStr;
 
 #[derive(Debug, Eq, PartialEq)]
 struct Hand {
-    cards: Vec<u32>,
-    bid: usize,
-    r#type: Type,
-}
-
-#[derive(Debug, Eq, PartialEq)]
-struct JokerHand {
     cards: Vec<u32>,
     bid: usize,
     r#type: Type,
@@ -66,37 +58,8 @@ impl PartialOrd for Hand {
     }
 }
 
-impl Ord for JokerHand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match self.r#type.cmp(&other.r#type) {
-            Ordering::Equal => {
-                match self
-                    .cards
-                    .iter()
-                    .zip(other.cards.iter())
-                    .find_map(|(a, b)| match a.cmp(b) {
-                        Ordering::Equal => None,
-                        o => Some(o),
-                    }) {
-                    Some(o) => o,
-                    _ => Ordering::Equal,
-                }
-            }
-            o => o,
-        }
-    }
-}
-
-impl PartialOrd for JokerHand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl FromStr for Hand {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+impl Hand {
+    fn from_str(s: &str, jokers_wild: bool) -> Result<Self, String> {
         let (cards, bid) = s.trim().split_once(' ').ok_or(format!("bad line: {}", s))?;
 
         let bid = bid.parse::<usize>().map_err(|e| e.to_string())?;
@@ -107,59 +70,17 @@ impl FromStr for Hand {
                 'A' => Some(14),
                 'K' => Some(13),
                 'Q' => Some(12),
-                'J' => Some(11),
+                'J' => Some(if jokers_wild { 1 } else { 11 }),
                 'T' => Some(10),
                 _ => c.to_digit(10),
             })
             .collect::<Vec<_>>();
 
-        let counts = cards.iter().fold(BTreeMap::new(), |mut counts, card| {
-            *counts.entry(card).or_insert(0) += 1;
-
-            counts
-        });
-        let mut counts = counts.into_values().collect::<Vec<_>>();
-        counts.sort_unstable();
-        let r#type = match counts.pop().ok_or("bad counts".to_owned())? {
-            5 => Type::FiveOfAKind,
-            4 => Type::FourOfAKind,
-            3 => match counts.pop().ok_or("bad count".to_owned())? {
-                2 => Type::FullHouse,
-                _ => Type::ThreeOfAKind,
-            },
-            2 => match counts.pop().ok_or("bad count".to_owned())? {
-                2 => Type::TwoPair,
-                _ => Type::Pair,
-            },
-            1 => Type::HighCard,
-            _ => unreachable!(),
+        let jokers = if jokers_wild {
+            cards.iter().filter(|&card| *card == 1).count()
+        } else {
+            0
         };
-
-        Ok(Self { cards, bid, r#type })
-    }
-}
-
-impl FromStr for JokerHand {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (cards, bid) = s.trim().split_once(' ').ok_or(format!("bad line: {}", s))?;
-
-        let bid = bid.parse::<usize>().map_err(|e| e.to_string())?;
-
-        let cards = cards
-            .chars()
-            .filter_map(|c| match c {
-                'A' => Some(14),
-                'K' => Some(13),
-                'Q' => Some(12),
-                'J' => Some(1),
-                'T' => Some(10),
-                _ => c.to_digit(10),
-            })
-            .collect::<Vec<_>>();
-
-        let jokers = cards.iter().filter(|&card| *card == 1).count();
         let counts =
             cards
                 .iter()
@@ -195,7 +116,7 @@ pub fn get_part_one(input: &str) -> Result<usize, String> {
     let mut hands = input
         .trim()
         .lines()
-        .map(Hand::from_str)
+        .map(|line| Hand::from_str(line, false))
         .collect::<Result<Vec<_>, String>>()?;
 
     hands.sort_unstable();
@@ -213,7 +134,7 @@ pub fn get_part_two(input: &str) -> Result<usize, String> {
     let mut hands = input
         .trim()
         .lines()
-        .map(JokerHand::from_str)
+        .map(|line| Hand::from_str(line, true))
         .collect::<Result<Vec<_>, String>>()?;
 
     hands.sort_unstable();

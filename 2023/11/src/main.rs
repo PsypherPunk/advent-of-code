f858d5dd6dd9350aa063f4b19e157b85e847fe2b
PsypherPunk <psypherpunk@gmail.com>
2023-12-11 09:47:35 +0000

    feat: 🌟 add 2023, day 11, part 2

--- 2023/11/src/main.rs
+++ 2023/11/src/main.rs
@@ -10,7 +10,10 @@ fn main() -> Result<(), String> {
         get_part_one(&input)?,
     );
 
-    println!("{}", get_part_two(&input)?,);
+    println!(
+        "What is the sum of these lengths? {}",
+        get_part_two(&input)?,
+    );
 
     Ok(())
 }

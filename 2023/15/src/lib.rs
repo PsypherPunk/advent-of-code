cf839d23e507e9c547a9d9281158f103cd5e071b
PsypherPunk <psypherpunk@gmail.com>
2023-12-15 13:20:26 +0000

    feat: 🌟 add 2023, day 15, part 2

--- 2023/15/src/lib.rs
+++ 2023/15/src/lib.rs
@@ -1,3 +1,57 @@
+enum Operation {
+    Remove,
+    Insert(usize),
+}
+
+struct Step<'a> {
+    label: &'a str,
+    operation: Operation,
+    hash: usize,
+}
+
+peg::parser! {
+    pub grammar manual() for str {
+        rule with_slice<T>(r: rule<T>) -> (T, &'input str)
+            = value:&r() input:$(r()) { (value, input) }
+
+        rule _() = [' ' | '\n']*
+
+        rule integer() -> usize
+            = n:$(['0'..='9']+)
+                {? n.parse().or(Err("invalid integer")) }
+
+        rule label() -> &'input str
+            = l:$(['a'..='z']+)
+
+        rule insert() -> Step<'input>
+            = label:label()
+              "="
+              focal_length:integer()
+              {
+                Step {
+                    label,
+                    operation: Operation::Insert(focal_length),
+                    hash: hash(label),
+                }
+              }
+
+        rule remove() -> Step<'input>
+            = label:label()
+              "-"
+              {
+                Step {
+                    label,
+                    operation: Operation::Remove,
+                    hash: hash(label),
+                }
+              }
+
+        pub rule steps() -> Vec<Step<'input>>
+            = steps:(insert() / remove()) ++ ","
+
+    }
+}
+
 fn hash(string: &str) -> usize {
     string
         .as_bytes()
@@ -11,8 +65,50 @@ pub fn get_part_one(input: &str) -> Result<usize, String> {
     Ok(sum)
 }
 
-pub fn get_part_two(_input: &str) -> Result<usize, String> {
-    Ok(0)
+pub fn get_part_two(input: &str) -> Result<usize, String> {
+    let steps = manual::steps(input.trim()).map_err(|e| e.to_string())?;
+
+    let boxes = steps
+        .iter()
+        .try_fold(vec![vec![]; 256], |mut boxes, step| {
+            let box_: &mut Vec<&Step> = boxes
+                .get_mut(step.hash)
+                .ok_or(format!("no boxes at {}", step.hash))?;
+
+            match step.operation {
+                Operation::Remove => {
+                    if let Some(index) = box_.iter().position(|lens| lens.label == step.label) {
+                        box_.remove(index);
+                    }
+                }
+                Operation::Insert(_) => match box_.iter().position(|lens| lens.label == step.label)
+                {
+                    Some(index) => box_[index] = step,
+                    None => {
+                        box_.push(step);
+                    }
+                },
+            };
+
+            Ok::<Vec<Vec<_>>, String>(boxes)
+        })?;
+
+    let focusing_power = boxes
+        .iter()
+        .enumerate()
+        .filter(|(_, box_)| !box_.is_empty())
+        .map(|(box_number, box_)| {
+            box_.iter()
+                .enumerate()
+                .map(|(slot, step)| match step.operation {
+                    Operation::Insert(focal_length) => (box_number + 1) * (slot + 1) * focal_length,
+                    _ => unreachable!(),
+                })
+                .sum::<usize>()
+        })
+        .sum();
+
+    Ok(focusing_power)
 }
 
 #[cfg(test)]
@@ -33,6 +129,6 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        assert_eq!(Ok(2), get_part_two(INPUT));
+        assert_eq!(Ok(145), get_part_two(INPUT));
     }
 }
--- 2023/15/src/main.rs
+++ 2023/15/src/main.rs
@@ -5,13 +5,10 @@ use ::day15::*;
 fn main() -> Result<(), String> {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
 
-    println!(
-        "What is the sum of the results? {}",
-        get_part_one(&input)?,
-    );
+    println!("What is the sum of the results? {}", get_part_one(&input)?,);
 
     println!(
-        "{}",
+        "What is the focusing power of the resulting lens configuration? {}",
         get_part_two(&input)?,
     );
 

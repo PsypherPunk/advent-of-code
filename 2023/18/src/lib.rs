bf978d9c18e7ddd5608bca85e55225c99834fd06
PsypherPunk <psypherpunk@gmail.com>
2023-12-18 13:39:14 +0000

    feat: 🌟 add 2023, day 18, part 2

--- 2023/18/src/lib.rs
+++ 2023/18/src/lib.rs
@@ -30,28 +30,69 @@ pub fn get_part_one(input: &str) -> Result<isize, String> {
         })
         .collect::<Vec<_>>();
 
-    let positions = [vec![(0, 0)], positions].concat();
+    // TODO: `concat()` otherwise the perimeter misses the first step…?
+    let (area, perimeter) = [vec![(0, 0)], positions]
+        .concat()
+        .iter()
+        .tuple_windows()
+        .fold((0, 0), |(area, perimeter), ((ax, ay), (bx, by))| {
+            let area = area + (ax * by) - (ay * bx);
+            let perimeter = if ax == bx {
+                perimeter + ay.abs_diff(*by) as isize
+            } else {
+                perimeter + ax.abs_diff(*bx) as isize
+            };
 
-    let (area, perimeter) =
-        positions
-            .iter()
-            .tuple_windows()
-            .fold((0, 0), |(area, perimeter), ((ax, ay), (bx, by))| {
-                let area = area + (ax * by) - (ay * bx);
-                let perimeter = if ax == bx {
-                    perimeter + ay.abs_diff(*by) as isize
-                } else {
-                    perimeter + ax.abs_diff(*bx) as isize
-                };
-
-                (area, perimeter)
-            });
+            (area, perimeter)
+        });
 
     Ok(((area / 2) - (perimeter / 2) + 1) + perimeter)
 }
 
-pub fn get_part_two(_input: &str) -> Result<isize, String> {
-    Ok(0)
+pub fn get_part_two(input: &str) -> Result<isize, String> {
+    let dig_plan = input
+        .trim()
+        .lines()
+        .map(|line| {
+            let (_, hex) = line.split_once('#').ok_or(format!("bad line: {}", line))?;
+            let metres = isize::from_str_radix(&hex[..5], 16).map_err(|e| e.to_string())?;
+
+            Ok::<(_, _), String>((&hex[5..=5], metres))
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let positions = dig_plan
+        .iter()
+        .scan((0, 0), |position, (direction, metres)| {
+            *position = match *direction {
+                "3" => (position.0, position.1 - metres),
+                "1" => (position.0, position.1 + metres),
+                "2" => (position.0 - metres, position.1),
+                "0" => (position.0 + metres, position.1),
+                _ => unreachable!(),
+            };
+
+            Some((position.0, position.1))
+        })
+        .collect::<Vec<_>>();
+
+    // TODO: otherwise the perimeter misses the first step…?
+    let (area, perimeter) = [vec![(0, 0)], positions]
+        .concat()
+        .iter()
+        .tuple_windows()
+        .fold((0, 0), |(area, perimeter), ((ax, ay), (bx, by))| {
+            let area = area + (ax * by) - (ay * bx);
+            let perimeter = if ax == bx {
+                perimeter + ay.abs_diff(*by) as isize
+            } else {
+                perimeter + ax.abs_diff(*bx) as isize
+            };
+
+            (area, perimeter)
+        });
+
+    Ok(((area / 2) - (perimeter / 2) + 1) + perimeter)
 }
 
 #[cfg(test)]
@@ -81,6 +122,6 @@ U 2 (#7a21e3)
 
     #[test]
     fn test_part_two() {
-        assert_eq!(Ok(2), get_part_two(INPUT));
+        assert_eq!(Ok(952408144115), get_part_two(INPUT));
     }
 }

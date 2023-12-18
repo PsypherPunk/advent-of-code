01466c4d68da289b67adcfec3ac2185454c79cbf
PsypherPunk <psypherpunk@gmail.com>
2023-12-18 11:40:53 +0000

    feat: ⭐ add 2023, day 18, part 1

---
+++ 2023/18/src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+use ::day18::*;
+
+fn main() -> Result<(), String> {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+
+    println!(
+        "…how many cubic meters of lava could it hold? {}",
+        get_part_one(&input)?,
+    );
+
+    println!("{}", get_part_two(&input)?,);
+
+    Ok(())
+}

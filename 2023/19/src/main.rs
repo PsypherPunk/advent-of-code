79d55f0250afcd95aab792958caa649d1d2489c1
PsypherPunk <psypherpunk@gmail.com>
2023-12-19 11:00:00 +0000

    feat: ⭐ add 2023, day 19, part 1

---
+++ 2023/19/src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+use ::day19::*;
+
+fn main() -> Result<(), String> {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+
+    println!(
+        "…what do you get if you add together all of the rating numbers for all of the parts that ultimately get accepted? {}",
+        get_part_one(&input)?,
+    );
+
+    println!("{}", get_part_two(&input)?,);
+
+    Ok(())
+}

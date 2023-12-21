40938073c6b8e9c5c00c580b1b3b33ef8d3bf250
PsypherPunk <psypherpunk@gmail.com>
2023-12-21 11:15:05 +0000

    feat: ⭐ add 2023, day 21, part 1

---
+++ 2023/21/src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+use ::day21::*;
+
+fn main() -> Result<(), String> {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+
+    println!(
+        "…how many garden plots could the Elf reach in exactly 64 steps? {}",
+        get_part_one(&input, 64)?,
+    );
+
+    println!("{}", get_part_two(&input)?,);
+
+    Ok(())
+}

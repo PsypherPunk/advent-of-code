8be87418cba0d5d7e006d0dc349457586ac5d509
PsypherPunk <psypherpunk@gmail.com>
2023-12-22 15:16:41 +0000

    refactor: 🔨 update 2023, day 22

--- 2023/22/src/lib.rs
+++ 2023/22/src/lib.rs
@@ -35,6 +35,14 @@ impl Brick {
         self.x.0.max(other.x.0) <= self.x.1.min(other.x.1)
             && self.y.0.max(other.y.0) <= self.y.1.min(other.y.1)
     }
+
+    fn fall_to(&self, z: usize) -> Self {
+        Self {
+            x: self.x,
+            y: self.y,
+            z: (z, z + (self.z.1 - self.z.0)),
+        }
+    }
 }
 
 pub fn get_part_one(input: &str) -> Result<usize, String> {
@@ -46,40 +54,51 @@ pub fn get_part_one(input: &str) -> Result<usize, String> {
 
     bricks.sort_unstable_by_key(|brick| brick.z.0);
 
-    // TODO: ick.
-    for i in 0..bricks.len() {
-        let mut min_z = 1;
+    let bricks = bricks
+        .iter()
+        .enumerate()
+        .fold(vec![], |mut beneath, (i, brick)| {
+            let resting_z = beneath[..i].iter().fold(1, |min_z: usize, other| {
+                if brick.covers(other) {
+                    min_z.max(other.z.1 + 1)
+                } else {
+                    min_z
+                }
+            });
+            beneath.push(brick.fall_to(resting_z));
 
-        for j in 0..i {
-            if bricks[i].covers(&bricks[j]) {
-                min_z = min_z.max(bricks[j].z.1 + 1);
+            beneath
+        });
+    
+    let below =bricks
+        .iter()
+        .enumerate()
+        .map(|(i, brick)| {
+            bricks[..i]
+                .iter()
+                .enumerate()
+                .filter_map(|(j, other)| {
+                    if brick.covers(other) && brick.z.0 == other.z.1 + 1 {
+                        Some(j)
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let unsafe_ = below
+        .iter()
+        .filter_map(|bricks_below| {
+            match bricks_below.len() {
+                1 => bricks_below.first(),
+                _ => None,
             }
-        }
+        })
+        .collect::<HashSet<_>>();
 
-        let height = bricks[i].z.1 - bricks[i].z.0;
-        bricks[i].z.0 = min_z;
-        bricks[i].z.1 = min_z + height;
-    }
-
-    // TODO: double-ick.
-    let mut below = vec![Vec::new(); bricks.len()];
-    for i in 0..bricks.len() {
-        for j in 0..i {
-            if bricks[i].covers(&bricks[j]) && bricks[i].z.0 == bricks[j].z.1 + 1 {
-                below[i].push(j);
-            }
-        }
-    }
-
-    // TODO: bleurgh.
-    let mut safe = vec![true; below.len()];
-    for underneath in below {
-        if underneath.len() == 1 {
-            safe[underneath[0]] = false;
-        }
-    }
-
-    Ok(safe.iter().filter(|&brick| *brick).count())
+    Ok(bricks.len() - unsafe_.len())
 }
 
 pub fn get_part_two(input: &str) -> Result<usize, String> {
@@ -91,19 +110,21 @@ pub fn get_part_two(input: &str) -> Result<usize, String> {
 
     bricks.sort_unstable_by_key(|brick| brick.z.0);
 
-    for i in 0..bricks.len() {
-        let mut min_z = 1;
+    let bricks = bricks
+        .iter()
+        .enumerate()
+        .fold(vec![], |mut beneath, (i, brick)| {
+            let resting_z = beneath[..i].iter().fold(1, |min_z: usize, other| {
+                if brick.covers(other) {
+                    min_z.max(other.z.1 + 1)
+                } else {
+                    min_z
+                }
+            });
+            beneath.push(brick.fall_to(resting_z));
 
-        for j in 0..i {
-            if bricks[i].covers(&bricks[j]) {
-                min_z = min_z.max(bricks[j].z.1 + 1);
-            }
-        }
-
-        let height = bricks[i].z.1 - bricks[i].z.0;
-        bricks[i].z.0 = min_z;
-        bricks[i].z.1 = min_z + height;
-    }
+            beneath
+        });
 
     let mut above = vec![Vec::new(); bricks.len()];
     let mut below = vec![Vec::new(); bricks.len()];
@@ -127,8 +148,8 @@ pub fn get_part_two(input: &str) -> Result<usize, String> {
     let mut queue = VecDeque::new();
     let mut seen = HashSet::new();
 
-    for (start, &safe) in safe.iter().enumerate() {
-        if safe {
+    for (start, safe) in safe.iter().enumerate() {
+        if *safe {
             continue;
         }
 
@@ -137,8 +158,7 @@ pub fn get_part_two(input: &str) -> Result<usize, String> {
 
         while let Some(current) = queue.pop_front() {
             for &next in &above[current] {
-                if below[next].iter().all(|brick| seen.contains(brick)) && !seen.contains(&next) {
-                    seen.insert(next);
+                if below[next].iter().all(|brick| seen.contains(brick)) && seen.insert(next) {
                     chained += 1;
                     queue.push_back(next);
                 }

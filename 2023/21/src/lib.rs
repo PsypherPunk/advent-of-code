1cea4092e204818d05911e12f4fc8565f22752e7
PsypherPunk <psypherpunk@gmail.com>
2023-12-22 17:11:37 +0000

    perf: 🚀 speed up 2023, day 21

--- 2023/21/src/lib.rs
+++ 2023/21/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 #[allow(dead_code)]
 fn to_string(garden: &[Vec<u8>]) -> String {
@@ -9,7 +9,7 @@ fn to_string(garden: &[Vec<u8>]) -> String {
         .join("\n")
 }
 
-fn bfs(garden: &[Vec<u8>], start: (isize, isize), target: isize) -> isize {
+fn bfs(garden: &[Vec<u8>], start: (isize, isize), target: isize) -> HashSet<(isize, isize)> {
     let mut queue = VecDeque::from_iter([(start, 0)]);
     let mut seen = HashSet::new();
 
@@ -45,14 +45,45 @@ fn bfs(garden: &[Vec<u8>], start: (isize, isize), target: isize) -> isize {
         queue.extend(next);
     }
 
-    plots.len() as isize
+    plots
 }
 
-fn lagrange(a: isize, b: isize, c: isize) -> (isize, isize, isize) {
-    (a / 2 - b + c / 2, -3 * (a / 2) + 2 * b - c / 2, a)
+fn distances(garden: &[Vec<u8>], start: (isize, isize)) -> HashMap<(isize, isize), isize> {
+    let mut distances = HashMap::new();
+    let mut queue = VecDeque::new();
+
+    queue.push_back((start, 0));
+
+    let ylen = garden.len() as isize;
+    let xlen = garden[0].len() as isize;
+
+    while let Some(((x, y), steps)) = queue.pop_front() {
+        if distances.contains_key(&(x, y)) {
+            continue;
+        }
+
+        distances.insert((x, y), steps);
+
+        let next = [(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)]
+            .into_iter()
+            .filter_map(|(nx, ny)| {
+                let effective_y = ny.rem_euclid(ylen) as usize;
+                let effective_x = nx.rem_euclid(xlen) as usize;
+
+                match garden[effective_y][effective_x] {
+                    b'#' => None,
+                    _ => Some(((effective_x as isize, effective_y as isize), steps + 1)),
+                }
+            })
+            .collect::<Vec<_>>();
+
+        queue.extend(next);
+    }
+
+    distances
 }
 
-pub fn get_part_one(input: &str, target: isize) -> Result<isize, String> {
+pub fn get_part_one(input: &str, target: isize) -> Result<usize, String> {
     let mut start = (0, 0);
     let garden = input
         .trim()
@@ -73,7 +104,7 @@ pub fn get_part_one(input: &str, target: isize) -> Result<isize, String> {
         })
         .collect::<Vec<_>>();
 
-    Ok(bfs(&garden, start, target))
+    Ok(bfs(&garden, start, target).len())
 }
 
 pub fn get_part_two(input: &str, target: isize) -> Result<isize, String> {
@@ -97,17 +128,31 @@ pub fn get_part_two(input: &str, target: isize) -> Result<isize, String> {
         })
         .collect::<Vec<_>>();
 
+    let distances = distances(&garden, start);
+
+    let (odd, even, odd_edges, even_edges) = distances.iter().fold(
+        (0, 0, 0, 0),
+        |(odd, even, odd_edges, even_edges), (_, distance)| {
+            if *distance % 2 == 1 && *distance > 65 {
+                (odd + 1, even, odd_edges + 1, even_edges)
+            } else if *distance % 2 == 1 {
+                (odd + 1, even, odd_edges, even_edges)
+            } else if *distance % 2 == 0 && *distance > 65 {
+                (odd, even + 1, odd_edges, even_edges + 1)
+            } else {
+                (odd, even + 1, odd_edges, even_edges)
+            }
+        },
+    );
     let steps_to_bounday = ((garden.len() - 1) / 2) as isize;
     let len = garden.len() as isize;
-    let values = (
-        bfs(&garden, start, steps_to_bounday),
-        bfs(&garden, start, steps_to_bounday + len),
-        bfs(&garden, start, steps_to_bounday + len * 2),
-    );
-    let polynomial = lagrange(values.0, values.1, values.2);
-    let target = (target - steps_to_bounday) / len;
+    let count = (target - steps_to_bounday) / len;
+    let total_odd = odd * (count + 1) * (count + 1);
+    let total_even = even * (count * count);
+    let total_odd_edges = odd_edges * (count + 1);
+    let total_even_edges = count * even_edges;
 
-    Ok(polynomial.0 * target * target + polynomial.1 * target + polynomial.2)
+    Ok(total_odd + total_even - total_odd_edges + total_even_edges)
 }
 
 #[cfg(test)]

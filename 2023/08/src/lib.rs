fba88475a2da9e7b3abc640850f0d07cfddd498e
PsypherPunk <psypherpunk@gmail.com>
2024-01-09 15:56:28 +0000

    perf: 🚀 speed up 2023, day 8, part 2

--- 2023/08/src/lib.rs
+++ 2023/08/src/lib.rs
@@ -1,7 +1,17 @@
-use std::collections::BTreeMap;
+//! The input has a very specific structure: each node ending in `A`
+//! has a corresponding node ending in `Z`, forming a cycle.
+//! 
+//! The period of this cycle reaching the node ending in `Z` is the
+//! least-common-multiple of the length of both directions with the
+//! length of the cycle.
+//!
+//! A breadth-first-search from each start node finds the length of
+//! each cycle. The answer is the total length of the directions.
+use std::collections::{HashMap, HashSet, VecDeque};
 
-use rayon::iter::{ParallelBridge, ParallelIterator};
-
+/// Party one _should_ be a unique instance of the BFS for the nodes
+/// named `AAA` and `ZZZ`. However, this doesn't hold true for one of
+/// the test cases and has been left as the initial, naive approach.
 pub fn get_part_one(input: &str) -> Result<usize, String> {
     let mut lines = input.trim().lines();
 
@@ -15,7 +25,7 @@ pub fn get_part_one(input: &str) -> Result<usize, String> {
 
             (start, (left, right))
         })
-        .collect::<BTreeMap<&str, (&str, &str)>>();
+        .collect::<HashMap<_, _>>();
 
     let instructions = instructions.chars().cycle();
 
@@ -45,9 +55,9 @@ pub fn get_part_one(input: &str) -> Result<usize, String> {
 pub fn get_part_two(input: &str) -> Result<usize, String> {
     let mut lines = input.trim().lines();
 
-    let instructions = lines.next().ok_or(format!("bad input: {}", input))?;
+    let mut steps = lines.next().ok_or(format!("bad input: {}", input))?.len();
     let nodes = lines
-        .skip(1)
+        .skip(2)
         .map(|line| {
             let start = &line[..3];
             let left = &line[7..10];
@@ -55,36 +65,37 @@ pub fn get_part_two(input: &str) -> Result<usize, String> {
 
             (start, (left, right))
         })
-        .collect::<BTreeMap<&str, (&str, &str)>>();
+        .collect::<HashMap<_, _>>();
 
-    let steps = nodes
+    let mut queue = VecDeque::new();
+    let mut seen = HashSet::new();
+
+    nodes
         .keys()
-        .filter(|node| node.ends_with('A'))
-        .par_bridge()
-        .map(|ghost| {
-            let instructions = instructions.chars().cycle();
+        .filter(|key| key.ends_with('A'))
+        .for_each(|start| {
+            queue.push_back((start, 0));
+            seen.insert(start);
 
-            instructions
-                .scan(ghost, |node, instruction| match *node {
-                    e if e.ends_with('Z') => None,
-                    current => match nodes.get(current) {
-                        None => unreachable!(),
-                        Some((left, right)) => match instruction {
-                            'L' => {
-                                *node = left;
-                                Some(left)
-                            }
-                            'R' => {
-                                *node = right;
-                                Some(right)
-                            }
-                            _ => unreachable!(),
-                        },
-                    },
-                })
-                .count()
-        })
-        .reduce(|| 1, num_integer::lcm);
+            while let Some((node, cost)) = queue.pop_front() {
+                if node.ends_with('Z') {
+                    steps = num_integer::lcm(steps, cost);
+                    break;
+                }
+
+                if let Some((left, right)) = nodes.get(node) {
+                    if seen.insert(left) {
+                        queue.push_back((left, cost + 1));
+                    }
+                    if seen.insert(right) {
+                        queue.push_back((right, cost + 1));
+                    }
+                }
+            }
+
+            queue.clear();
+            seen.clear();
+        });
 
     Ok(steps)
 }

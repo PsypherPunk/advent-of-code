f99224723c4161053e576e351c27c33fcac62959
PsypherPunk <psypherpunk@gmail.com>
2024-12-02 13:37:42 +0000

    feat: 🌟 add 2024, day 2, part 2

--- 2024/02/main.go
+++ 2024/02/main.go
@@ -46,6 +46,20 @@ func isSafeDiff(reports []int) bool {
 	return true
 }
 
+func combinations(reports []int) [][]int {
+	combinations := [][]int{}
+
+	combinations = append(combinations, reports)
+
+	for i := 0; i < len(reports); i++ {
+		combination := append([]int{}, reports[:i]...)
+		combination = append(combination, reports[i+1:]...)
+		combinations = append(combinations, combination)
+	}
+
+	return combinations
+}
+
 func IsSafe(line string) bool {
 	fields := strings.Fields(line)
 	reports := make([]int, len(fields))
@@ -61,7 +75,24 @@ func IsSafe(line string) bool {
 	return (isIncreasing(reports) || isDecreasing(reports)) && isSafeDiff(reports)
 }
 
-func IsSafeButTolerant(report string) bool {
+func IsSafeButTolerant(line string) bool {
+	fields := strings.Fields(line)
+	reports := make([]int, len(fields))
+
+	for i, field := range fields {
+		report, err := strconv.Atoi(field)
+		if err != nil {
+			fmt.Printf("Error converting %s to int: %v\n", field, err)
+		}
+		reports[i] = report
+	}
+
+	for _, combination := range combinations(reports) {
+		if (isIncreasing(combination) || isDecreasing(combination)) && isSafeDiff(combination) {
+			return true
+		}
+	}
+
 	return false
 }
 
@@ -78,11 +109,19 @@ func PartOne(input string) int {
 }
 
 func PartTwo(input string) int {
-	return 0
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	safe := 0
+	for scanner.Scan() {
+		if IsSafeButTolerant(scanner.Text()) {
+			safe++
+		}
+	}
+
+	return safe
 }
 
 func main() {
 	fmt.Println("How many reports are safe?", PartOne(input))
 
-	fmt.Println("", PartTwo(input))
+	fmt.Println("How many reports are now safe?", PartTwo(input))
 }
--- 2024/02/main_test.go
+++ 2024/02/main_test.go
@@ -30,7 +30,14 @@ func Test_PartTwo(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected bool
-	}{}
+	}{
+		{"7 6 4 2 1", true},
+		{"1 2 7 8 9", false},
+		{"9 7 6 2 1", false},
+		{"1 3 2 4 5", true},
+		{"8 6 4 4 1", true},
+		{"1 3 6 7 9", true},
+	}
 
 	for _, tt := range tests {
 		t.Run(tt.input, func(t *testing.T) {

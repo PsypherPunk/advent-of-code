365b6c39cfe15977e145b1c8208cd044d2d26edb
PsypherPunk <psypherpunk@gmail.com>
2024-12-06 13:15:49 +0000

    feat: 🌟 add 2024, day 6, part 2

--- 2024/06/main_test.go
+++ 2024/06/main_test.go
@@ -34,7 +34,18 @@ func Test_PartTwo(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected int
-	}{}
+	}{
+		{`....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`, 6},
+	}
 
 	for _, tt := range tests {
 		t.Run(tt.input, func(t *testing.T) {

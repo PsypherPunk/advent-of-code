3931fa397f9f4ecc251179821edb251ebd31c200
PsypherPunk <psypherpunk@gmail.com>
2024-12-07 10:32:26 +0000

    feat: 🌟 add 2024, day 7, part 2

--- 2024/07/main.go
+++ 2024/07/main.go
@@ -23,6 +23,26 @@ func IsValid(testValue int, numbers []int, i int, currentValue int) bool {
 	return IsValid(testValue, numbers, i+1, currentValue+numbers[i]) || IsValid(testValue, numbers, i+1, currentValue*numbers[i])
 }
 
+func IsStillValid(testValue int, numbers []int, i int, currentValue int) bool {
+	if i >= len(numbers) {
+		return testValue == currentValue
+	}
+
+	if currentValue > testValue {
+		return false
+	}
+
+	nextValue, err := strconv.Atoi(fmt.Sprintf("%d%d", currentValue, numbers[i]))
+	if err != nil {
+		fmt.Println("invalid numbers", numbers)
+		return false
+	}
+
+	return IsStillValid(testValue, numbers, i+1, currentValue+numbers[i]) ||
+		IsStillValid(testValue, numbers, i+1, currentValue*numbers[i]) ||
+		IsStillValid(testValue, numbers, i+1, nextValue)
+}
+
 func PartOne(input string) int {
 	scanner := bufio.NewScanner(strings.NewReader(input))
 
@@ -55,11 +75,38 @@ func PartOne(input string) int {
 }
 
 func PartTwo(input string) int {
-	return 0
+	scanner := bufio.NewScanner(strings.NewReader(input))
+
+	totalCalibrationResult := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		testValueNumbers := strings.SplitN(line, ":", 2)
+		testValue, err := strconv.Atoi(testValueNumbers[0])
+		if err != nil {
+			fmt.Println("invalid line:", line, err)
+		}
+
+		stringNumbers := strings.Fields(testValueNumbers[1])
+		numbers := make([]int, len(stringNumbers), len(stringNumbers))
+		for i, stringNumber := range stringNumbers {
+			number, err := strconv.Atoi(stringNumber)
+			if err != nil {
+				fmt.Println("invalid line", line, err)
+			}
+			numbers[i] = number
+		}
+
+		if IsStillValid(testValue, numbers, 1, numbers[0]) {
+			totalCalibrationResult += testValue
+		}
+	}
+
+	return totalCalibrationResult
 }
 
 func main() {
 	fmt.Println("What is their total calibration result?", PartOne(input))
 
-	fmt.Println("", PartTwo(input))
+	fmt.Println("What is their total calibration result?", PartTwo(input))
 }

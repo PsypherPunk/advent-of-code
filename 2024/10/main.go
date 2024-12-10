aed41301686531531a1747487fc358eb33c8f169
PsypherPunk <psypherpunk@gmail.com>
2024-12-10 13:22:52 +0000

    feat: 🌟 add 2024, day 10, part 2

--- 2024/10/main.go
+++ 2024/10/main.go
@@ -11,7 +11,7 @@ import (
 //go:embed input.txt
 var input string
 
-func dfs(topographicMap map[image.Point]rune, position image.Point, seen map[image.Point]bool) (score int) {
+func dfsTrailheadScores(topographicMap map[image.Point]rune, position image.Point, seen map[image.Point]bool) (score int) {
 	if topographicMap[position] == '9' {
 		_, ok := seen[position]
 
@@ -27,7 +27,22 @@ func dfs(topographicMap map[image.Point]rune, position image.Point, seen map[ima
 	for _, step := range []image.Point{{0, -1}, {1, 0}, {0, 1}, {-1, 0}} {
 		next := position.Add(step)
 		if topographicMap[next] == topographicMap[position]+1 {
-			score += dfs(topographicMap, next, seen)
+			score += dfsTrailheadScores(topographicMap, next, seen)
+		}
+	}
+
+	return score
+}
+
+func dfsDistinctHikingTrails(topographicMap map[image.Point]rune, position image.Point) (score int) {
+	if topographicMap[position] == '9' {
+		return 1
+	}
+
+	for _, step := range []image.Point{{0, -1}, {1, 0}, {0, 1}, {-1, 0}} {
+		next := position.Add(step)
+		if topographicMap[next] == topographicMap[position]+1 {
+			score += dfsDistinctHikingTrails(topographicMap, next)
 		}
 	}
 
@@ -50,7 +65,7 @@ func PartOne(input string) int {
 	sumOfScores := 0
 	for position, height := range topographicMap {
 		if height == '0' {
-			sumOfScores += dfs(topographicMap, position, map[image.Point]bool{})
+			sumOfScores += dfsTrailheadScores(topographicMap, position, map[image.Point]bool{})
 		}
 	}
 
@@ -58,11 +73,30 @@ func PartOne(input string) int {
 }
 
 func PartTwo(input string) int {
-	return 0
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	topographicMap := make(map[image.Point]rune)
+
+	y := 0
+	for scanner.Scan() {
+		for x, r := range scanner.Text() {
+			topographicMap[image.Point{x, y}] = r
+		}
+
+		y++
+	}
+
+	sumOfScores := 0
+	for position, height := range topographicMap {
+		if height == '0' {
+			sumOfScores += dfsDistinctHikingTrails(topographicMap, position)
+		}
+	}
+
+	return sumOfScores
 }
 
 func main() {
 	fmt.Println("What is the sum of the scores of all trailheads on your topographic map?", PartOne(input))
 
-	fmt.Println("", PartTwo(input))
+	fmt.Println("What is the sum of the ratings of all trailheads?", PartTwo(input))
 }

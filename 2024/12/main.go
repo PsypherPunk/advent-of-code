269e94b2167ab59eaa1bd469807a39f0b237bd41
PsypherPunk <psypherpunk@gmail.com>
2024-12-12 14:22:56 +0000

    feat: 🌟 add 2024, day 12, part 2

--- 2024/12/main.go
+++ 2024/12/main.go
@@ -74,11 +74,77 @@ func PartOne(input string) int {
 }
 
 func PartTwo(input string) int {
-	return 0
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	garden := make(map[image.Point]rune)
+	notSeen := make(map[image.Point]bool)
+
+	y := 0
+	for scanner.Scan() {
+		for x, r := range scanner.Text() {
+			garden[image.Point{x, y}] = r
+			notSeen[image.Point{x, y}] = true
+		}
+		y++
+	}
+
+	directions := []image.Point{
+		{0, -1},
+		{1, 0},
+		{0, 1},
+		{-1, 0},
+	}
+
+	price := 0
+	for len(notSeen) > 0 {
+		var start image.Point
+		for k := range notSeen {
+			start = k
+			break
+		}
+		delete(notSeen, start)
+		plot := garden[start]
+
+		stack := []image.Point{start}
+		area, perimeter := 0, 0
+		sides := make(map[[2]image.Point]bool)
+
+		for len(stack) > 0 {
+			currentPlot := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			area++
+			perimeter += 4
+
+			for _, direction := range directions {
+				neighbourPlot := currentPlot.Add(direction)
+				if garden[neighbourPlot] != plot {
+					sides[[2]image.Point{currentPlot, direction}] = true
+					continue
+				}
+				perimeter--
+				if !notSeen[neighbourPlot] {
+					continue
+				}
+				delete(notSeen, neighbourPlot)
+				stack = append(stack, neighbourPlot)
+			}
+		}
+
+		adjustment := 0
+		for side := range sides {
+			plot, direction := side[0], side[1]
+			if sides[[2]image.Point{plot.Add(image.Point{X: direction.Y, Y: -direction.X}), direction}] {
+				adjustment--
+			}
+		}
+
+		price += area * (len(sides) + adjustment)
+	}
+
+	return price
 }
 
 func main() {
 	fmt.Println("What is the total price of fencing all regions on your map?", PartOne(input))
 
-	fmt.Println("", PartTwo(input))
+	fmt.Println("What is the new total price of fencing all regions on your map?", PartTwo(input))
 }
--- 2024/12/main_test.go
+++ 2024/12/main_test.go
@@ -14,6 +14,12 @@ BBCD
 BBCC
 EEEC
 `, 140},
+		{`OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO
+`, 772},
 		{`RRRRIICCFF
 RRRRIICCCF
 VVRRRCCFFF
@@ -40,7 +46,43 @@ func Test_PartTwo(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected int
-	}{}
+	}{
+		{`AAAA
+BBCD
+BBCC
+EEEC
+`, 80},
+		{`OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO
+`, 436},
+		{`EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE
+`, 236},
+		{`AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA
+`, 368},
+		{`RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE
+`, 1206},
+	}
 
 	for _, tt := range tests {
 		t.Run(tt.input, func(t *testing.T) {

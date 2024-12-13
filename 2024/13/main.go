87e241b6660abf50511d59701be204f53ef78715
PsypherPunk <psypherpunk@gmail.com>
2024-12-13 12:18:55 +0000

    feat: 🌟 add 2024, day 13, part 2

--- 2024/13/main.go
+++ 2024/13/main.go
@@ -45,11 +45,42 @@ func PartOne(input string) int {
 }
 
 func PartTwo(input string) int {
-	return 0
+	scanner := bufio.NewScanner(strings.NewReader(input))
+
+	var aX, aY, bX, bY, pX, pY int
+
+	totalTokens := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if len(line) == 0 {
+			continue
+		}
+
+		switch line[:8] {
+		case "Button A":
+			fmt.Sscanf(line, "Button A: X+%d, Y+%d", &aX, &aY)
+		case "Button B":
+			fmt.Sscanf(line, "Button B: X+%d, Y+%d", &bX, &bY)
+		case "Prize: X":
+			fmt.Sscanf(line, "Prize: X=%d, Y=%d", &pX, &pY)
+			pX, pY = pX+10000000000000, pY+10000000000000
+
+			// https://en.wikipedia.org/wiki/Cramer%27s_rule
+			D, Dx, Dy := aX*bY-bX*aY, pX*bY-bX*pY, aX*pY-pX*aY
+			if D != 0 && Dx == (Dx/D)*D && Dy == (Dy/D)*D {
+				totalTokens += (Dx/D)*3 + (Dy / D)
+			}
+
+			aX, aY, bX, bY, pX, pY = 0, 0, 0, 0, 0, 0
+		}
+	}
+
+	return totalTokens
 }
 
 func main() {
 	fmt.Println("What is the fewest tokens you would have to spend to win all possible prizes?", PartOne(input))
 
-	fmt.Println("", PartTwo(input))
+	fmt.Println("What is the fewest tokens you would have to spend to win all possible prizes?", PartTwo(input))
 }

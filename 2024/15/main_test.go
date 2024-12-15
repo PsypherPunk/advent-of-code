2053983bd8f3603915d5deabcd869ddde114d644
PsypherPunk <psypherpunk@gmail.com>
2024-12-15 20:22:05 +0000

    feat: ⭐ add 2024, day 15, part 1

---
+++ 2024/15/main_test.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_PartOne(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int
+	}{
+		{`##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+`, 10092},
+		{`########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<
+`, 2028},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			if actual := PartOne(tt.input); actual != tt.expected {
+				t.Errorf("PartOne() = %v, expected %v", actual, tt.expected)
+			}
+		})
+	}
+}
+
+func Test_PartTwo(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int
+	}{}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			if actual := PartTwo(tt.input); actual != tt.expected {
+				t.Errorf("PartTwo() = %v, expected %v", actual, tt.expected)
+			}
+		})
+	}
+}

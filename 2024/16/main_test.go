c2e54f115776e728a83ae3155f4eebb07b9ef153
PsypherPunk <psypherpunk@gmail.com>
2024-12-16 11:32:21 +0000

    feat: ⭐ add 2024, day 16, part 1

---
+++ 2024/16/main_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_PartOne(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int
+	}{
+		{`###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############
+`, 7036},
+		{`#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################
+`, 11048},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			if actual := PartOne(tt.input); actual != tt.expected {
+				t.Errorf("PartOne() = %v, expected %v", actual, tt.expected)
+			}
+		})
+	}
+}
+
+func Test_PartTwo(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int
+	}{}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			if actual := PartTwo(tt.input); actual != tt.expected {
+				t.Errorf("PartTwo() = %v, expected %v", actual, tt.expected)
+			}
+		})
+	}
+}

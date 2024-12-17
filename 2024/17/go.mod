370ed0ec41f5bc96a8a69b939552cbe30a6cdd3e
PsypherPunk <psypherpunk@gmail.com>
2024-12-17 10:11:17 +0000

    feat: ⭐ add 2024, day 17, part 1

---
+++ 2024/17/go.mod
@@ -0,0 +1,3 @@
+module psypherunk.io/aoc/2024/17
+
+go 1.23.3
---
+++ 2024/17/main_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_PartOne(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{`Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0
+`, "4,6,3,5,6,3,5,2,1,0"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			if actual := PartOne(tt.input); actual != tt.expected {
+				t.Errorf("PartOne() = %v, expected %v", actual, tt.expected)
+			}
+		})
+	}
+}
+
+func Test_PartTwo(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int
+	}{}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			if actual := PartTwo(tt.input); actual != tt.expected {
+				t.Errorf("PartTwo() = %v, expected %v", actual, tt.expected)
+			}
+		})
+	}
+}

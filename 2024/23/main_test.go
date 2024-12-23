e53d3f3ab119ceaef71d2fe3fe9d7c70c71be2d4
PsypherPunk <psypherpunk@gmail.com>
2024-12-23 11:01:06 +0000

    feat: ⭐ add 2024, day 23, part 1

---
+++ 2024/23/main_test.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_PartOne(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int
+	}{
+		{`kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn
+`, 7},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			if actual := PartOne(tt.input); actual != tt.expected {
+				t.Errorf("PartOne() = %v, expected %v", actual, tt.expected)
+			}
+		})
+	}
+}
+
+func Test_PartTwo(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int
+	}{}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			if actual := PartTwo(tt.input); actual != tt.expected {
+				t.Errorf("PartTwo() = %v, expected %v", actual, tt.expected)
+			}
+		})
+	}
+}

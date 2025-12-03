8142d01be67b90260bbe483a52724fbca6e4f7ce
PsypherPunk <psypherpunk@gmail.com>
2025-12-03 20:44:13 +0000

    feat: 🌟 add 2025, day 3, part 2

--- 2025/03/src/lib.rs
+++ 2025/03/src/lib.rs
@@ -1,24 +1,25 @@
 fn get_joltage(bank: &[u8], digits: usize) -> Result<Vec<u8>, String> {
     if digits == 1 {
-        let max = bank
-            .iter()
-            .max()
-            .ok_or_else(|| "bank is empty".to_string())?;
+        let max = bank.iter().max().ok_or("bank is empty")?;
         return Ok(vec![*max]);
     }
 
     let digit = bank[..bank.len() - digits + 1]
         .iter()
+        // .enumerate().max_by_key(|&(i, &b)| b)
+        // …doesn't necessarily get the first occurrence (i.e. wrong `i`)
+        // .enumerate().max_by(|(i1, &b1), (i2, &b2)| b1.cmp(&b2).then(i2.cmp(i1)))
+        // …is slower
         .max()
-        .ok_or_else(|| "bank is empty".to_string())?;
+        .ok_or("bank is empty")?;
+
+    let mut result = vec![*digit];
 
     let i = bank
         .iter()
         .position(|&b| b == *digit)
-        .ok_or_else(|| "digit not found in bank".to_string())?
+        .ok_or("digit not found in bank")?
         + 1;
-
-    let mut result = vec![*digit];
 
     result.extend(get_joltage(&bank[i..], digits - 1)?);
 
@@ -40,8 +41,19 @@ pub fn get_part_one(input: &str) -> Result<usize, String> {
     Ok(total_output_joltage.iter().sum())
 }
 
-pub fn get_part_two(_input: &str) -> Result<usize, String> {
-    Ok(0)
+pub fn get_part_two(input: &str) -> Result<usize, String> {
+    let total_output_joltage = input
+        .trim()
+        .lines()
+        .map(|line| {
+            let joltage = get_joltage(line.as_bytes(), 12)?;
+            Ok(joltage
+                .iter()
+                .fold(0, |acc, &b| acc * 10 + (b - b'0') as usize))
+        })
+        .collect::<Result<Vec<_>, String>>()?;
+
+    Ok(total_output_joltage.iter().sum())
 }
 
 #[cfg(test)]
@@ -61,6 +73,6 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        assert_eq!(Ok(2), get_part_two(INPUT));
+        assert_eq!(Ok(3121910778619), get_part_two(INPUT));
     }
 }
